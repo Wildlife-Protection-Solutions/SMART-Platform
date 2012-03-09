6afc1dec052c75e9e1d1397af6ca87fc69949fc2
Emily Gouge <egouge@refractions.net>
2012-03-09 00:21:17 +0000

    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@21 37136b1e-4cb1-4992-8394-573b48a9bdce

--- java/org.wcs.smart.patrol.test/src/org/wcs/smart/patrol/model/PatrolLegDayTest.java
+++ java/org.wcs.smart.patrol.test/src/org/wcs/smart/patrol/model/PatrolLegDayTest.java
@@ -7,6 +7,8 @@ import java.util.GregorianCalendar;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.wcs.smart.patrol.model.Patrol;
+import org.wcs.smart.patrol.model.PatrolLeg;
 
 public class PatrolLegDayTest {
 
