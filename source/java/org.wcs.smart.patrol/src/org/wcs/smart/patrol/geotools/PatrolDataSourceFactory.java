e944bd83aece0a617241a132f0b4b01596113990
Emily Gouge <egouge@refractions.net>
2012-03-13 18:06:51 +0000

    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@28 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ source/java/org.wcs.smart.patrol/src/org/wcs/smart/patrol/geotools/PatrolDataSourceFactory.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.patrol.geotools;
+
+import java.awt.RenderingHints.Key;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.Map;
+
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFactorySpi;
+import org.hibernate.Session;
+import org.wcs.smart.hibernate.HibernateManager;
+import org.wcs.smart.patrol.model.Patrol;
+
+/**
+ * Smart patrol data source factor.  This is a read only data source.
+ * Sources waypoints and tracks from a patrol.
+ * 
+ * @author Emily
+ * @since 1.0.0
+ */
+public class PatrolDataSourceFactory implements DataStoreFactorySpi{
+
+	public static final Param PATROL_UUID = new Param("patroluuid", byte[].class, "Patrol", true); 
+	  
+	/* (non-Javadoc)
+	 * @see org.geotools.data.DataAccessFactory#canProcess(java.util.Map)
+	 */
+	@Override
+	public boolean canProcess(Map<String, Serializable> params) {
+		if (params.containsKey(PATROL_UUID.key)){
+			return true;
+		}
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.geotools.data.DataAccessFactory#getDescription()
+	 */
+	@Override
+	public String getDescription() {
+		return "Smart patrol layers";
+	}
+
+	/* (non-Javadoc)
+	 * @see org.geotools.data.DataAccessFactory#getDisplayName()
+	 */
+	@Override
+	public String getDisplayName() {
+		return "Smart Patrol";
+	}
+
+	/* (non-Javadoc)
+	 * @see org.geotools.data.DataAccessFactory#getParametersInfo()
+	 */
+	@Override
+	public Param[] getParametersInfo() {
+		return new Param[]{PATROL_UUID };
+	}
+
+	/**
+	 * @see org.geotools.data.DataAccessFactory#isAvailable()
+	 */
+	@Override
+	public boolean isAvailable() {
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.geotools.factory.Factory#getImplementationHints()
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public Map<Key, ?> getImplementationHints() {
+		return Collections.EMPTY_MAP;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.geotools.data.DataStoreFactorySpi#createDataStore(java.util.Map)
+	 */
+	@Override
+	public DataStore createDataStore(Map<String, Serializable> params)
+			throws IOException {
+		
+		Session session = HibernateManager.openSession();
+		Patrol patrol = null;
+		try{
+			patrol = (Patrol)session.load(Patrol.class, ((byte[])params.get(PATROL_UUID.key)));	
+		
+		}finally{
+			session.close();
+		}
+		if (patrol == null ){
+			throw new IOException("Unable to read patrol data source.");
+		}
+		return new PatrolDataSource(patrol);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.geotools.data.DataStoreFactorySpi#createNewDataStore(java.util.Map)
+	 */
+	@Override
+	public DataStore createNewDataStore(Map<String, Serializable> arg0)
+			throws IOException {
+		throw new UnsupportedOperationException("This is a read-only data store.");
+	}
+
+}
