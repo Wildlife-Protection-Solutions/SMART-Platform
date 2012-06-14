b67c1efb5db779c6d252ae835319490f26c13f2a
Jeff Lounsbury <jeffloun@refractions.net>
2012-06-14 16:54:41 +0000

    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@147 37136b1e-4cb1-4992-8394-573b48a9bdce

--- java/org.wcs.smart.util/src/main/resources/org/wcs/smart/db/smart-tables-query.sql
+++ java/org.wcs.smart.util/src/main/resources/org/wcs/smart/db/smart-tables-query.sql
@@ -26,6 +26,21 @@ CREATE TABLE smart.waypoint_query
 	PRIMARY KEY (UUID)
 );
 
+CREATE TABLE smart.patrol_query
+(
+	UUID CHAR(16) FOR BIT DATA NOT NULL,
+	CREATOR_UUID CHAR(16) FOR BIT DATA  NOT NULL,
+	NAME VARCHAR(1024) NOT NULL,
+	QUERY_FILTER VARCHAR(32672),
+	CA_FILTER VARCHAR(32672),
+	CA_UUID CHAR(16) FOR BIT DATA NOT NULL,
+	FOLDER_UUID CHAR(16) FOR BIT DATA,
+	COLUMN_FILTER VARCHAR(32672),
+	SHARED BOOLEAN DEFAULT false NOT NULL,
+	ID VARCHAR(6) NOT NULL,
+	PRIMARY KEY (UUID)
+);
+
 
 CREATE TABLE smart.summary_query
 (
@@ -91,6 +106,30 @@ ALTER TABLE smart.waypoint_query
 	ON UPDATE RESTRICT
 	ON DELETE RESTRICT
 ;
+
+ALTER TABLE smart.patrol_query
+	ADD constraint patrol_query_creator_uuid_fk FOREIGN KEY (CREATOR_UUID)
+	REFERENCES smart.employee (UUID)
+	ON UPDATE RESTRICT
+	ON DELETE RESTRICT
+;
+
+
+ALTER TABLE smart.patrol_query
+	ADD constraint patrol_query_ca_uuid_fk FOREIGN KEY (CA_UUID)
+	REFERENCES smart.conservation_area (UUID)
+	ON UPDATE RESTRICT
+	ON DELETE RESTRICT
+;
+
+
+ALTER TABLE smart.patrol_query
+	ADD constraint patrol_query_folder_uuid_fk FOREIGN KEY (FOLDER_UUID)
+	REFERENCES smart.query_folder (uuid)
+	ON UPDATE RESTRICT
+	ON DELETE RESTRICT
+;
+
 
 
 
