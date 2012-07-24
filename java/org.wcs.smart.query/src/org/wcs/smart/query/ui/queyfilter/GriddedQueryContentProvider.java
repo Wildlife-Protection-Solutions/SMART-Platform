1be2e22ed10654e2356293e798402a1381685903
Jeff Lounsbury <jeffloun@refractions.net>
2012-07-24 19:48:05 +0000

    more grid stuff...
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@216 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ java/org.wcs.smart.query/src/org/wcs/smart/query/ui/queyfilter/GriddedQueryContentProvider.java
@@ -0,0 +1,556 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.query.ui.queyfilter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.graphics.Image;
+import org.hibernate.Session;
+import org.wcs.smart.ca.datamodel.Attribute;
+import org.wcs.smart.ca.datamodel.Attribute.AttributeType;
+import org.wcs.smart.ca.datamodel.AttributeTreeNode;
+import org.wcs.smart.ca.datamodel.Category;
+import org.wcs.smart.ca.datamodel.CategoryAttribute;
+import org.wcs.smart.ca.datamodel.DataModel;
+import org.wcs.smart.ca.datamodel.DmObject;
+import org.wcs.smart.hibernate.HibernateManager;
+import org.wcs.smart.patrol.SmartPatrolPlugIn;
+import org.wcs.smart.query.QueryPlugIn;
+import org.wcs.smart.query.parser.internal.PatrolQueryOptions.DateGroupByOption;
+import org.wcs.smart.query.parser.internal.PatrolQueryOptions.PatrolQueryOption;
+import org.wcs.smart.query.parser.internal.PatrolQueryOptions.PatrolValueOption;
+import org.wcs.smart.ui.properties.DataModelContentProvider;
+import org.wcs.smart.ui.properties.DataModelLabelProvider;
+
+/**
+ * Content provider for gridded query options.  Includes
+ * value and group by options.
+ * 
+ * @author egouge
+ * @since 1.0.0
+ */
+public class GriddedQueryContentProvider  implements ITreeContentProvider {
+
+	//data model 
+	private DataModel dataModel = null;
+	private DataModelContentProvider provider;
+	
+	//root nodes
+	private RootNode valueNode = new RootNode(NodeType.VALUE_NODE);
+	private RootNode groupByNode = new RootNode(NodeType.GROUP_BY_NODE);
+	//patrol nodes
+	private RootNode patrolValueNode = new RootNode(NodeType.PATROL_VALUES);
+	private RootNode patrolGroupByNode = new RootNode(NodeType.PATROL_GROUPBYS);
+	
+	//date group by node 
+	private RootNode dateGroupByNode = new RootNode(NodeType.PATROL_DATE_GROUPBYS);
+
+	//datamodel nodes
+	private RootNode dataModelValueNode = new RootNode(NodeType.DATAMODEL_VALUES);
+	private RootNode dataModelGroupByNode = new RootNode(NodeType.DATAMODEL_GROUPBYS);
+	
+	private RootNode dataModelValueCategory = new RootNode(NodeType.DATAMODEL_VALUE_CATEGORY);
+	private RootNode dataModelValueAttribute = new RootNode(NodeType.DATAMODEL_VALUE_ATTRIBUTES);
+
+	private RootNode dataModelGroupByCategory = new RootNode(NodeType.DATAMODEL_GROUPBY_CATEGORY);
+	private RootNode dataModelGroupByAttribute = new RootNode(NodeType.DATAMODEL_GROUPBY_ATTRIBUTES);
+	
+	private PatrolValueOption[] patrolValueOptions = null;
+	private PatrolQueryOption[] patrolGroupByOption = null;
+	private DateGroupByOption[] dateGroupByOptions = null;
+	
+	
+	/**
+	 * Data model children items
+	 */
+	enum DataModelItem{
+		CATEGORIES("Categories"),
+		ATTRIBUTES("Attributes");
+		
+		String guiName;
+		
+		DataModelItem(String guiName){
+			this.guiName = guiName;
+		}
+	}
+	
+	
+	
+	/**
+	 * Root node children
+	 */
+	enum NodeType  {
+		VALUE_NODE("Value Options"),
+		GROUP_BY_NODE("Group By Options"),
+		PATROL_VALUES("Patrol Values"),
+		PATROL_GROUPBYS("Patrol Group Bys"),
+		PATROL_DATE_GROUPBYS("Date"),
+		DATAMODEL_VALUES("Data Model Values"),
+		DATAMODEL_GROUPBYS("Data Model Group Bys"),
+		DATAMODEL_VALUE_CATEGORY("Categories & Attribute"),
+		DATAMODEL_VALUE_ATTRIBUTES("Attributes"),
+		DATAMODEL_GROUPBY_CATEGORY("Categories & Attribute"),
+		DATAMODEL_GROUPBY_ATTRIBUTES("Attributes");		
+		
+		private String name;
+		
+		private NodeType( String name){
+			this.name = name;
+		}
+	}
+	
+	/**
+	 * Creates a new content provider 
+	 */
+	public GriddedQueryContentProvider(){
+		provider = new DataModelContentProvider(false, true, true);
+		
+	}
+
+	/**
+	 * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+	 */
+	@Override
+	public void dispose() {
+		provider.dispose();
+	}
+
+	/**
+	 * 
+	 * @param newInput must be a map that contains the keys
+	 * RootNodeType.DATA_MODEL_ITEM whose value is the current data model
+	 * and 
+	 * RootNodeType.PATROL_VALUES
+	 * RootNodeType.PATROL_GROUPBYS
+	 * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		if (newInput == null){
+			patrolValueOptions = null;
+			patrolGroupByOption = null;
+			provider.inputChanged(viewer, oldInput, null);
+		}else if (newInput instanceof String){
+			patrolValueOptions = null;
+			patrolGroupByOption = null;
+			dataModel = null;
+			dateGroupByOptions = null;
+		}else{
+			if (newInput != null && !(newInput instanceof Map)){
+				throw new IllegalArgumentException("new input must be map");
+			}
+			Map<?, ?> in = (Map<?, ?>)newInput;
+			this.dataModel = (DataModel)in.get(NodeType.GROUP_BY_NODE);
+			patrolValueOptions = (PatrolValueOption[])in.get(NodeType.PATROL_VALUES);
+			patrolGroupByOption = (PatrolQueryOption[])in.get(NodeType.PATROL_GROUPBYS);
+			dateGroupByOptions = (DateGroupByOption[]) in.get(GriddedQueryContentProvider.NodeType.PATROL_DATE_GROUPBYS);
+			provider.inputChanged(viewer, oldInput, this.dataModel);	
+		}
+	}
+
+	/**
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getElements(java.lang.Object)
+	 */
+	@Override
+	public Object[] getElements(Object inputElement) {
+		if (patrolValueOptions == null &&
+			patrolGroupByOption == null &&
+			dataModel == null &&
+			dateGroupByOptions == null){
+			return new String[]{"Loading"};
+		}
+		
+		return new Object[]{groupByNode, valueNode};
+	}
+
+	/**
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getChildren(java.lang.Object)
+	 */
+	@Override
+	public Object[] getChildren(Object parentElement) {
+		if (parentElement instanceof RootNode){
+			return ((RootNode)parentElement).getChildren();
+		//}else if (parentElement instanceof AREA FITLER){
+		}else if (parentElement instanceof DataModelItem){
+			if (parentElement == DataModelItem.CATEGORIES){
+					return provider.getChildren(provider.getElements(null)[0]);	
+			}else if (parentElement == DataModelItem.ATTRIBUTES){
+//				DataModel dm = provider.
+				List<Attribute> atts = new ArrayList<Attribute>();
+				for (Attribute a : this.dataModel.getAttributes()){
+					//TODO: check categories and only include attributes with at least one active 
+					//category
+					atts.add(a);
+				}
+				return atts.toArray(new Attribute[atts.size()]);
+				
+			}
+			return null;
+		}else if (parentElement instanceof SummaryDmObject){
+			SummaryDmObject parent = ((SummaryDmObject)parentElement);
+			return getChildren(parent);
+			
+		}
+		return null;
+	}
+
+	private Object[] getAttributeTreeChildren(final SummaryDmObject parent){
+	
+		final List<AttributeTreeNode> kids = new ArrayList<AttributeTreeNode>();
+		Job j = new Job("loading tree children"){
+
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				Session s = HibernateManager.openSession();
+				s.beginTransaction();
+				try{
+					
+					List<AttributeTreeNode> nodes = null;
+					if (parent.getObject() instanceof Attribute){
+						s.update(parent.getObject());
+						nodes = ((Attribute)parent.getObject()).getTree() ;
+						Collections.sort(nodes, new AttributeTreeNode.NodeComparator());
+					}else if (parent.getObject() instanceof CategoryAttribute ){
+						s.update(((CategoryAttribute)parent.getObject()).getAttribute() );
+						nodes = ((CategoryAttribute)parent.getObject()).getAttribute().getTree() ;
+						Collections.sort(nodes, new AttributeTreeNode.NodeComparator());
+					}else if (parent.getObject() instanceof AttributeTreeNode){
+						s.update(parent.getObject());
+						nodes =  ((AttributeTreeNode)parent.getObject()).getChildren() ;
+					}
+					for (AttributeTreeNode node : nodes){
+						if (node.getIsActive()){
+							kids.add( node );								
+						}
+					}
+					s.getTransaction().rollback();
+				}catch (Exception ex){
+					QueryPlugIn.log("Could not load tree children:" + ex.getMessage(), ex);
+				}finally{
+					s.close();
+				}
+				return Status.OK_STATUS;
+			}
+			
+		};
+		j.schedule();
+		try{
+			j.join();
+		}catch (Exception ex){
+			QueryPlugIn.log("Could not load tree children:" + ex.getMessage(), ex);
+			return null;
+		}
+		
+		if (kids == null || kids.size() == 0){
+			return null;
+		}
+		Object[] results = new Object[kids.size()];
+		int index = 0;
+		Object obj2 = parent.getObject2();
+		if (parent.getObject() instanceof CategoryAttribute){
+			obj2 = ((CategoryAttribute) parent.getObject()).getCategory();
+		}
+		
+		for (AttributeTreeNode kid : kids){
+			results[index++] = new SummaryDmObject(kid, obj2, parent.isValue());
+		}
+		return results;
+	}
+	
+	
+	public Object[] getChildren(SummaryDmObject parent){
+
+		if (parent.getObject() instanceof Attribute && 
+				((Attribute)parent.getObject()).getType() == AttributeType.TREE &&
+				!parent.isValue()
+				){
+			return getAttributeTreeChildren(parent);
+		}else if (parent.getObject() instanceof CategoryAttribute && 
+			((CategoryAttribute)parent.getObject()).getAttribute().getType() == AttributeType.TREE &&
+			!parent.isValue()){
+			
+			return getAttributeTreeChildren(parent);
+		}else if (parent.getObject() instanceof AttributeTreeNode && !parent.isValue()){
+			return getAttributeTreeChildren(parent);
+		}
+		
+		Object[] kids = provider.getChildren(  parent.getObject() );
+		
+		if (kids == null){ return null; }
+		
+		Object[] results = new Object[kids.length];
+		
+		int cnt = 0;
+		
+		for (int i = 0; i < kids.length; i ++){
+			boolean add = false;
+			if (parent.isValue()){
+				if (kids[i] instanceof Attribute){
+					if (( (Attribute)kids[i]).getType() == AttributeType.NUMERIC){
+						add = true;
+					}
+				}else if (kids[i] instanceof CategoryAttribute){
+					if (( (CategoryAttribute)kids[i]).getAttribute().getType() == AttributeType.NUMERIC){
+						add = true;
+					}
+				}else if (kids[i] instanceof Category){
+					add = true;
+				}
+			}else{
+				if (kids[i] instanceof Attribute){
+					if (( (Attribute)kids[i]).getType() == AttributeType.LIST ||
+							( (Attribute)kids[i]).getType() == AttributeType.TREE){
+						add = true;
+					}
+				}else if (kids[i] instanceof CategoryAttribute){
+					if (( (CategoryAttribute)kids[i]).getAttribute().getType() == AttributeType.LIST ||
+							( (CategoryAttribute)kids[i]).getAttribute().getType() == AttributeType.TREE){
+						add = true;
+					}
+				}else if (kids[i] instanceof Category){
+					add = true;
+				}
+			}
+			if (add){
+				results[cnt++] = new SummaryDmObject(kids[i], parent.isValue());
+			}
+		}
+		//assume data model
+		if (cnt == 0){
+			return null;
+		}else{
+			return Arrays.copyOf(results, cnt);
+		}
+	}
+	/**
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object)
+	 */
+	@Override
+	public Object getParent(Object element) {
+		if (element instanceof RootNode){
+			switch (((RootNode)element).type){
+				case VALUE_NODE:	
+				case GROUP_BY_NODE:
+					return null;
+					
+				case PATROL_DATE_GROUPBYS:
+					return patrolGroupByNode;
+				case PATROL_GROUPBYS:
+				case DATAMODEL_GROUPBYS:
+					return groupByNode;
+				case DATAMODEL_VALUES:
+				case PATROL_VALUES:
+					return valueNode;
+			}
+			return null;
+		}else if (element instanceof PatrolQueryOption){
+			return patrolGroupByNode;
+		}else if (element instanceof PatrolValueOption){
+			return patrolValueNode;
+		//}else if (parentElement instanceof AREA FITLER){
+		}else if (element instanceof DataModelItem){
+			Object parent = getParent(element);
+			if (parent instanceof RootNode){
+				if (((RootNode)parent).type == NodeType.VALUE_NODE){
+					return dataModelValueNode;
+				}else{
+					return dataModelGroupByNode;
+				}
+			}
+			return null;
+		}else if (element instanceof SummaryDmObject){
+			//assume data model
+			return provider.getParent( ((SummaryDmObject)element).getObject() );	
+		}else{
+			return provider.getParent(element);
+		}
+
+	}
+
+	/**
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
+	 */
+	@Override
+	public boolean hasChildren(Object element) {
+		if (element instanceof RootNode){
+			return true;
+		}else if (element instanceof PatrolQueryOption){
+			return false;
+		}else if (element instanceof PatrolValueOption){
+			return false;
+		}else if (element instanceof DataModelItem){
+			return false;
+		//}else if (parentElement instanceof AREA FITLER){
+		}else if (element instanceof SummaryDmObject){
+			if (!((SummaryDmObject) element).isValue()){
+				if (((SummaryDmObject) element).getObject() instanceof AttributeTreeNode){
+					return ((AttributeTreeNode)((SummaryDmObject) element).getObject()).getChildren().size() > 0;
+				}else if (((SummaryDmObject) element).getObject() instanceof Attribute){
+					if (((Attribute)((SummaryDmObject) element).getObject()).getType() == AttributeType.TREE){
+						return true;
+					}else{
+						return false;
+					}
+				}
+			}
+			Object[] kids = getChildren(element);
+			if (kids == null || kids.length == 0){
+				return false;
+			}
+			return true;
+			//return provider.hasChildren(((SummaryDmObject) element).getObject());		
+		}else{
+			//assume data model
+			return provider.hasChildren(element);
+			
+		}
+	}
+
+	
+	/*
+	 * A root node of the content provider
+	 */
+	class RootNode{
+		private NodeType type;
+		
+		/**
+		 * Creates a new root node of a give type
+		 * @param type
+		 */
+		public RootNode(NodeType type){
+			this.type = type;
+		}
+		
+		/**
+		 * @return the children of the given root node
+		 */
+		public Object[] getChildren(){
+			if (type == NodeType.VALUE_NODE){
+				return new Object[]{patrolValueNode, dataModelValueNode};
+			}else if (type == NodeType.PATROL_VALUES){
+				return patrolValueOptions;
+			}else if (type == NodeType.PATROL_GROUPBYS){
+				Object[] kids = new Object[patrolGroupByOption.length + 1];
+				for (int i = 0; i < patrolGroupByOption.length; i++){
+					kids[i] = patrolGroupByOption[i];
+				}
+				kids[kids.length-1] = dateGroupByNode;
+				return kids;
+			}else if (type == NodeType.GROUP_BY_NODE){				
+				return new Object[]{patrolGroupByNode, dataModelGroupByNode};
+			}else if (type == NodeType.PATROL_DATE_GROUPBYS){
+				return dateGroupByOptions;
+			}else if (type == NodeType.DATAMODEL_VALUES){
+				return new Object[]{dataModelValueCategory, dataModelValueAttribute};
+			}else if (type == NodeType.DATAMODEL_VALUE_CATEGORY){
+				Object[] kids = provider.getChildren(provider.getElements(null)[0]);
+				Object[] results = new Object[kids.length];
+				for (int i = 0; i < kids.length; i ++){
+					results[i] = new SummaryDmObject((DmObject)kids[i], true);
+				}
+				//assume data model
+				return results;
+			}else if (type == NodeType.DATAMODEL_VALUE_ATTRIBUTES){
+				Collection<Attribute> atts = dataModel.getAttributes();
+				Object[] results = new Object[atts.size()];
+				int cnt = 0;
+				for (Attribute att: atts){
+					if (att.getType() == AttributeType.NUMERIC){
+						results[cnt++] = new SummaryDmObject(att, true);
+					}
+				}
+				return Arrays.copyOf(results, cnt);
+				
+			}else if (type == NodeType.DATAMODEL_GROUPBYS){
+				return new Object[]{dataModelGroupByCategory, dataModelGroupByAttribute};
+			}else if (type == NodeType.DATAMODEL_GROUPBY_CATEGORY){
+				Object[] kids = provider.getChildren(provider.getElements(null)[0]);
+				Object[] results = new Object[kids.length];
+				for (int i = 0; i < kids.length; i ++){
+					results[i] = new SummaryDmObject((DmObject)kids[i], false);
+				}
+				//assume data model
+				return results;
+			}else if (type == NodeType.DATAMODEL_GROUPBY_ATTRIBUTES){
+				Collection<Attribute> atts = dataModel.getAttributes();
+				Object[] results = new Object[atts.size()];
+				int cnt = 0;
+				for (Attribute att: atts){
+					if (att.getType() == AttributeType.LIST |
+						att.getType() == AttributeType.TREE){
+						results[cnt++] = new SummaryDmObject(att, false);
+					}
+				}
+				return Arrays.copyOf(results, cnt);
+				
+			}
+			return null;
+		}
+		
+		/**
+		 * @return root node name
+		 */
+		public String getName(){
+			return type.name;
+		}
+		
+		/**
+		 * @return root node image
+		 */
+		public Image getImage(){
+			if (type == NodeType.VALUE_NODE){
+				return JFaceResources.getImageRegistry().get(QueryPlugIn.VALUE_ICON);
+			}else if (type == NodeType.GROUP_BY_NODE){
+				 return JFaceResources.getImageRegistry().get(QueryPlugIn.GROUPBY_ICON);
+			}else if (type == NodeType.PATROL_DATE_GROUPBYS){
+				return JFaceResources.getImageRegistry().get(QueryPlugIn.CALENDAR_ICON);
+			}else if (type == NodeType.DATAMODEL_VALUES ||
+					type == NodeType.DATAMODEL_GROUPBYS){
+				return JFaceResources.getImageRegistry().get(DataModelLabelProvider.DATA_MODEL_ICON);
+			}else if (type == NodeType.PATROL_GROUPBYS ||
+					type == NodeType.PATROL_VALUES){
+				return JFaceResources.getImageRegistry().get(SmartPatrolPlugIn.PATROL_ICON);
+			}else if (type == NodeType.DATAMODEL_VALUE_ATTRIBUTES){
+				return JFaceResources.getImageRegistry().get(DataModelLabelProvider.ATTRIBUTE_NUMBER_ICON);
+			}else if (type == NodeType.DATAMODEL_VALUE_CATEGORY){
+				return JFaceResources.getImageRegistry().get(DataModelLabelProvider.CATEGORY_ICON);
+			}else if (type == NodeType.DATAMODEL_GROUPBY_ATTRIBUTES){
+				return JFaceResources.getImageRegistry().get(DataModelLabelProvider.ATTRIBUTE_LIST_ICON);
+			}else if (type == NodeType.DATAMODEL_GROUPBY_CATEGORY){
+				return JFaceResources.getImageRegistry().get(DataModelLabelProvider.CATEGORY_ICON);
+			}
+			return null;
+		}
+	}
+}
---
+++ java/org.wcs.smart.query/src/org/wcs/smart/query/ui/queyfilter/GriddedQueryLabelProvider.java
@@ -0,0 +1,59 @@
+package org.wcs.smart.query.ui.queyfilter;
+
+import org.eclipse.swt.graphics.Image;
+import org.wcs.smart.ca.datamodel.Attribute;
+import org.wcs.smart.ca.datamodel.AttributeTreeNode;
+import org.wcs.smart.ca.datamodel.Category;
+import org.wcs.smart.ca.datamodel.CategoryAttribute;
+import org.wcs.smart.query.parser.internal.PatrolQueryOptions;
+import org.wcs.smart.query.parser.internal.PatrolQueryOptions.DateGroupByOption;
+import org.wcs.smart.query.parser.internal.PatrolQueryOptions.PatrolQueryOption;
+import org.wcs.smart.query.parser.internal.PatrolQueryOptions.PatrolValueOption;
+
+public class GriddedQueryLabelProvider extends QueryFilterLabelProvider {
+
+	@Override
+	public Image getImage(Object element) {
+		if (element instanceof GriddedQueryContentProvider.RootNode) {
+			return ((GriddedQueryContentProvider.RootNode) element).getImage();
+		}else if (element instanceof DateGroupByOption){
+			return PatrolQueryOptions.getImage((DateGroupByOption)element);
+		}else if (element instanceof SummaryDmObject){
+			return super.getImage(((SummaryDmObject) element).getObject());
+		}else if (element instanceof PatrolValueOption){
+			return ((PatrolValueOption) element).getIcon();
+		}
+		return super.getImage(element);
+	}
+
+	@Override
+	public String getText(Object element) {
+		if (element instanceof PatrolValueOption) {
+			return ((PatrolValueOption) element).getGuiName();
+		} else if (element instanceof PatrolQueryOption) {
+			return ((PatrolQueryOption) element).getGuiName();
+		} else if (element instanceof GriddedQueryContentProvider.RootNode) {
+			return ((GriddedQueryContentProvider.RootNode) element).getName();
+		} else if (element instanceof DateGroupByOption) {
+			return ((DateGroupByOption) element).getGuiName();
+		} else if (element instanceof SummaryDmObject){
+			SummaryDmObject obj = (SummaryDmObject)element;
+			if (obj.getObject() instanceof Attribute){
+				return ((Attribute)obj.getObject()).getName();
+			}else if (obj.getObject() instanceof AttributeTreeNode){
+				return ((AttributeTreeNode)obj.getObject()).getName();
+			}else if (obj.getObject() instanceof Category){
+				if (obj.isValue()){
+					return "Count '" + ((Category)obj.getObject()).getName() + "' ";
+				}else{
+					return ((Category)obj.getObject()).getName();
+				}
+			}else if (obj.getObject() instanceof CategoryAttribute){
+				return ((CategoryAttribute)obj.getObject()).getAttribute().getName();
+			}
+			
+		}
+		return super.getText(element);
+
+	}
+}
