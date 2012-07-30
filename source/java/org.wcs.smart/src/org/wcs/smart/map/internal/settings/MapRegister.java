322755b8fa3e846d76257d2d5284138c86b8401a
Mauricio Pazos <mpazos@refractions.net>
2012-07-30 21:41:32 +0000

    ticket-196 - map custom setting - in progress
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@237 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ source/java/org.wcs.smart/src/org/wcs/smart/map/internal/settings/MapRegister.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.map.internal.settings;
+
+import java.net.URI;
+import java.util.List;
+
+import org.geotools.brewer.color.BrewerPalette;
+
+/**
+ * Maintains the custom setting done in the map object
+ * 
+ * @author Mauricio Pazos
+ *
+ */
+final class MapRegister {
+
+	private final BrewerPalette colorPalette;
+	private final List<LayerRegister> layerList;
+	private final URI id;
+	private final String name;
+	private final String envelop;
+
+	public MapRegister(
+			final URI id, final String sName, final BrewerPalette colorPalette,
+			final String envelop,
+			final List<LayerRegister> layerRegisterList) {
+
+		this.id = id;
+		this.name = sName;
+		this.colorPalette = colorPalette;
+		this.envelop = envelop;
+
+		this.layerList = layerRegisterList;
+	}
+
+	
+	public URI getId() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public BrewerPalette getColorPalette() {
+		return colorPalette;
+	}
+
+	public List<LayerRegister> getLayerList() {
+		return layerList;
+	}
+
+
+	public String getEnvelop() {
+		return envelop;
+	}
+
+
+	@Override
+	public String toString() {
+		return "MapRegister [colorPalette=" + colorPalette + ", layerList="
+				+ layerList + ", id=" + id + ", name=" + name + ", envelop="
+				+ envelop + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MapRegister other = (MapRegister) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+}
---
+++ source/java/org.wcs.smart/src/org/wcs/smart/map/internal/settings/StyleRegister.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.map.internal.settings;
+
+import net.refractions.udig.project.internal.StyleEntry;
+
+
+/**
+ * Maintains the custom style. Its values are retrieved from {@link StyleEntry} object. 
+ * 
+ * @author Mauricio Pazos
+ *
+ */
+final class StyleRegister {
+
+	private final String id; 
+	private final String memento;
+
+	public StyleRegister(String id, String styleMemento) {
+		this.id = id;
+		this.memento = styleMemento;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+	
+	public String getMemento() {
+		return memento;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StyleRegister other = (StyleRegister) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "StyleRegister [id=" + id + ", memento=" + memento + "]";
+	}
+	
+	
+}
