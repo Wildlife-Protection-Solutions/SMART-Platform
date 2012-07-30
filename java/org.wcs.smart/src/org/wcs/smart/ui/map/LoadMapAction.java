d8892857d9a1ffef0ba6393bc2fe98683dc21f26
Mauricio Pazos <mpazos@refractions.net>
2012-07-30 21:42:37 +0000

    ticket-196 - save action in toolbar
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@238 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ java/org.wcs.smart/src/org/wcs/smart/ui/map/LoadMapAction.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package org.wcs.smart.ui.map;
+
+import net.refractions.udig.project.internal.Map;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.wcs.smart.hibernate.SmartDB;
+import org.wcs.smart.map.internal.settings.MapSettings;
+
+/**
+ * FIXME WARNING only for test purpose. This class should be removed
+ * 
+ * <p>
+ * This action was added in order to avoid reinit the application (in development environment).
+ * It shouldn't be part of the final product.  
+ * </p>
+ * 
+ * @author Mauricio Pazos
+ *
+ */
+public class LoadMapAction implements IViewActionDelegate {
+
+	private MapView view;
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+	 */
+	@Override
+	public void run(IAction action) {
+		
+
+		Map map = this.view.getMap();
+		if(map == null) return;
+		
+		MapSettings settings = MapSettings.getInstance(SmartDB.getCurrentEmployee()); 
+		settings.applyTo(map);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+	 */
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IViewActionDelegate#init(org.eclipse.ui.IViewPart)
+	 */
+	@Override
+	public void init(IViewPart view) {
+		this.view = (MapView)view;
+
+	}
+
+}
