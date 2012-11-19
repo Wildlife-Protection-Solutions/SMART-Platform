8642460b87048b3980d943c5354235e285898db3
Emily Gouge <egouge@refractions.net>
2012-11-19 23:00:24 +0000

    additions for deleting employees
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@469 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ source/java/org.wcs.smart.query/src/org/wcs/smart/query/internal/advisor/EmployeeDeleteAdvisor.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.query.internal.advisor;
+
+import org.hibernate.Session;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.wcs.smart.ca.Employee;
+import org.wcs.smart.ca.advisors.IDeleteAdvisor;
+import org.wcs.smart.query.model.Query.QueryType;
+import org.wcs.smart.query.model.QueryFolder;
+
+/**
+ * Delete advisor for determining if an
+ * employee can be deleted.  
+ * Validates if the employee owns any queries or
+ * query folders;
+ * 
+ * @author Emily
+ *
+ */
+public class EmployeeDeleteAdvisor  implements IDeleteAdvisor {
+
+	/**
+	 * @see org.wcs.smart.ca.advisors.IDeleteAdvisor#canDelete(java.lang.Object, org.hibernate.Session)
+	 */
+	@Override
+	public String canDelete(Object object, Session session) {
+		if (!(object instanceof Employee)){
+			return "Object not of type Employee. Can not delete.";
+		}
+		Employee e = (Employee)object;
+		if (e.getUuid() == null){
+			return null;
+		}
+		for (int i = 0; i < QueryType.values().length; i++){
+			QueryType qt = QueryType.values()[i];
+			Long cnt = (Long) session.createCriteria(qt.getHibernateClass()).add(Restrictions.eq("owner", e)).setProjection(Projections.rowCount()).uniqueResult();
+			if (cnt > 0){
+				return "The employee " + e.getLabel() + " owns " + cnt + " saved " + qt.getUiName() + " queries.  These queries removed before the employee can be remove.";
+			}
+		}
+	
+		Long cnt = (Long) session.createCriteria(QueryFolder.class).add(Restrictions.eq("employee", e)).setProjection(Projections.rowCount()).uniqueResult();
+		if (cnt > 0){
+			return "The employee " + e.getLabel() + " has " + cnt + " query folders.  These folders must be deleted before the employee can be removed.";
+		}
+		return null;
+	}
+
+}
