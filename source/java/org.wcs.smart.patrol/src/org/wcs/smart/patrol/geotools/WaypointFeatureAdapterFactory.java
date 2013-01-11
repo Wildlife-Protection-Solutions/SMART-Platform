e41cc03212cc2c6bd1f815d2db17408c1d719130
Emily Gouge <egouge@refractions.net>
2013-01-11 19:57:16 +0000

    updates to WaypointInfoView & adding adapters to work with patrol map window
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@590 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ source/java/org.wcs.smart.patrol/src/org/wcs/smart/patrol/geotools/WaypointFeatureAdapterFactory.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.patrol.geotools;
+
+import org.eclipse.core.runtime.IAdapterFactory;
+import org.hibernate.Session;
+import org.opengis.feature.simple.SimpleFeature;
+import org.wcs.smart.hibernate.HibernateManager;
+import org.wcs.smart.patrol.SmartPatrolPlugIn;
+import org.wcs.smart.patrol.model.Waypoint;
+import org.wcs.smart.util.SmartUtils;
+
+/**
+ * Adpater factory for adapting SimpleFeature with WaypointFeatureType
+ * to Waypoints.
+ * 
+ * @author egouge
+ *
+ */
+public class WaypointFeatureAdapterFactory implements IAdapterFactory {
+
+	@Override
+	public Object getAdapter(Object adaptableObject, Class adapterType) {
+		if (adapterType == Waypoint.class) {
+			
+			if (adaptableObject instanceof SimpleFeature){
+				SimpleFeature sf = (SimpleFeature)adaptableObject;
+				if (sf.getFeatureType().getTypeName().equals(PatrolDataSource.WAYPOINT_TYPE)){
+					String key = sf.getID();
+					String uuids = key.substring(key.lastIndexOf('.') + 1);
+					byte[] wpuuid;
+					try {
+						wpuuid = SmartUtils.decodeHex(uuids);
+					} catch (Exception e) {
+						SmartPatrolPlugIn
+							.log("Could not determine waypoint for uuid " + uuids, e); //$NON-NLS-1$
+						return null;
+					}
+					Session s = HibernateManager.openSession();
+					s.getTransaction().begin();
+					try {
+						Waypoint wp = (Waypoint) s.get(Waypoint.class, wpuuid);
+						return wp;
+					} finally {
+						s.getTransaction().rollback();
+						s.close();
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Class[] getAdapterList() {
+		return new Class[]{Waypoint.class};
+	}
+
+}
