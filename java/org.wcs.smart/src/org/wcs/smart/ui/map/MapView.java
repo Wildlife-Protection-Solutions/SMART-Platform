814ebacb88b6fd6f0fb2b2ccf8b15be4241c1c32
Evgeniy Litvin <elitvin@refractions.net>
2013-01-16 19:11:00 +0000

    Refactoring: create generic component for map info area NOTE: Also fixed following bug - Wrong shell used in IViewportModelListener (might cause org.eclipse.swt.SWTException: Invalid thread access)
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@608 37136b1e-4cb1-4992-8394-573b48a9bdce

--- java/org.wcs.smart/src/org/wcs/smart/ui/map/MapView.java
+++ java/org.wcs.smart/src/org/wcs/smart/ui/map/MapView.java
@@ -14,46 +14,30 @@
  */
 package org.wcs.smart.ui.map;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-
 import net.refractions.udig.project.internal.Map;
 import net.refractions.udig.project.internal.ProjectFactory;
-import net.refractions.udig.project.internal.commands.ChangeCRSCommand;
 import net.refractions.udig.project.internal.render.ViewportModel;
-import net.refractions.udig.project.render.IViewportModelListener;
-import net.refractions.udig.project.render.ViewportModelEvent;
 import net.refractions.udig.project.ui.ApplicationGIS;
 import net.refractions.udig.project.ui.internal.MapPart;
 import net.refractions.udig.project.ui.internal.tool.display.ToolManager;
 import net.refractions.udig.project.ui.internal.tool.display.ToolProxy;
-import net.refractions.udig.project.ui.render.displayAdapter.MapMouseEvent;
-import net.refractions.udig.project.ui.render.displayAdapter.MapMouseMotionListener;
 import net.refractions.udig.project.ui.tool.IMapEditorSelectionProvider;
 import net.refractions.udig.project.ui.tool.IToolManager;
 import net.refractions.udig.project.ui.viewers.MapViewer;
 
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.action.IStatusLineManager;
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IPartListener2;
 import org.eclipse.ui.IWorkbenchPartReference;
 import org.eclipse.ui.part.ViewPart;
-import org.wcs.smart.SmartPlugIn;
 import org.wcs.smart.ca.Area;
 import org.wcs.smart.internal.Messages;
 import org.wcs.smart.map.internal.ZoomHandler;
-
-import com.vividsolutions.jts.geom.Coordinate;
 
 /**
  * A map view.
@@ -101,7 +85,6 @@ public class MapView extends ViewPart implements MapPart, IAdaptable {
 
     };
     
-	private Label lblCoordinates;
     public MapView() {
         super();
     }
@@ -135,91 +118,9 @@ public class MapView extends ViewPart implements MapPart, IAdaptable {
         map.getViewportModelInternal().setCRS(ViewportModel.BAD_DEFAULT);
 		map.getViewportModelInternal().setCRS(Area.AREA_CRS);
 		
-        Composite infoArea = new Composite(parent, SWT.NONE);
-        GridLayout gl = new GridLayout(5, false);
-        gl.marginTop = gl.marginBottom = gl.marginHeight= 0;
-        infoArea.setLayout(gl);
-        infoArea.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false));
-        lblCoordinates = new Label(infoArea, SWT.NONE);
-        lblCoordinates.setText(SmartMapEditorPart.COORDINATE_LABEL);
-        lblCoordinates.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
-        lblCoordinates.setAlignment(SWT.RIGHT);
-        
-        Label lblSeparator = new Label(infoArea, SWT.SEPARATOR | SWT.VERTICAL);
-        GridData gd = new GridData(SWT.CENTER, SWT.CENTER, false, false);
-        gd.heightHint = lblCoordinates.computeSize(SWT.DEFAULT, SWT.DEFAULT).y;
-        lblSeparator.setLayoutData(gd);
-        
-        ScaleRatioComposite scale = new ScaleRatioComposite(infoArea, getMap());
-        scale.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false));
-        
-        
-        lblSeparator = new Label(infoArea, SWT.SEPARATOR | SWT.VERTICAL);
-        gd = new GridData(SWT.CENTER, SWT.CENTER, false, false);
-        gd.heightHint = lblCoordinates.computeSize(SWT.DEFAULT, SWT.DEFAULT).y;
-        lblSeparator.setLayoutData(gd);
-        
-        final Button lblSRID = new Button(infoArea, SWT.NONE);
-        lblSRID.setText(map.getViewportModel().getCRS().getName().getCode());
-        lblSRID.setLayoutData(new GridData(SWT.RIGHT, SWT.FILL, false, false));
-        lblSRID.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				ProjectionDialog pd = new ProjectionDialog(getSite().getShell());
-				if (pd.open() == IDialogConstants.OK_ID){
-					try{
-						ChangeCRSCommand command = new ChangeCRSCommand(pd.getSelection().getCrs());
-						map.sendCommandASync(command);
-					}catch (Exception ex){
-						SmartPlugIn.displayLog(getSite().getShell(), SmartMapEditorPart.ERROR_SETTING_MAP_PROJECTION + ex.getLocalizedMessage(), ex);
-					}	
-				}
-			}
-		});
-        
-        map.getViewportModel().addViewportModelListener(new IViewportModelListener() {
-			@Override
-			public void changed(ViewportModelEvent event) {
-				if(event.getType() == ViewportModelEvent.EventType.CRS){
-					getSite().getShell().getDisplay().asyncExec(new Runnable(){
-						@Override
-						public void run() {
-							lblSRID.setText(map.getViewportModel().getCRS().getName().getCode());
-							lblSRID.getParent().layout();
-						}});
-				}
-				
-			}
-		});
-      
-        
-        mapviewer.getViewport().addMouseMotionListener(new MapMouseMotionListener() {
-			
-			@Override
-			public void mouseMoved(MapMouseEvent event) {
-				event.getPoint();
-				Coordinate c = map.getViewportModelInternal().pixelToWorld(event.x, event.y);
-				lblCoordinates.setText(format(c.x) + SmartMapEditorPart.COORDINATE_XYSEPARATOR + format(c.y));
-				//see CursorPosition Tool
-			}
-			
-			private String format(double d){
-				 DecimalFormat format = (DecimalFormat) NumberFormat.getNumberInstance();
-		         format.setMaximumFractionDigits(4);
-		         format.setMinimumIntegerDigits(1);
-		         format.setGroupingUsed(false);
-		         String string = format.format(d);
-		         return string;
-			}
-			@Override
-			public void mouseHovered(MapMouseEvent event) {
-			}
-			
-			@Override
-			public void mouseDragged(MapMouseEvent event) {
-			}
-		});        
-        getSite().getWorkbenchWindow().getPartService().addPartListener(partlistener);
+		new MapInfoAreaComposite(parent, SWT.NONE, mapviewer);
+
+		getSite().getWorkbenchWindow().getPartService().addPartListener(partlistener);
         setTool(ZoomHandler.ZoomToolId);
     }
 
