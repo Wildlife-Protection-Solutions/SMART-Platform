665bb60040d5da99e407e678e0a1adba83fbb9c2
Evgeniy Litvin <elitvin@refractions.net>
2013-01-25 17:15:44 +0000

    Intelligence Filter Dialog implementation
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@667 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ java/org.wcs.smart.intelligence/src/org/wcs/smart/intelligence/ui/IntelligenceFilterDialog.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.intelligence.ui;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.wcs.smart.common.filter.DateFilterComposite;
+import org.wcs.smart.common.filter.SmartFilterDialog;
+import org.wcs.smart.common.filter.StringFilterComposite;
+
+/**
+ * Filter dialog for filtering intelligences displayed in the intelligence list view.
+ * 
+ * @author elitvin
+ * @since 1.0.0
+ */
+public class IntelligenceFilterDialog extends SmartFilterDialog {
+
+	private IntelligenceViewFilter currentFilter;
+	private IntelligenceListView view;
+	
+	private DateFilterComposite receivedDateCmp;
+	private DateFilterComposite relevantDateCmp;
+	private StringFilterComposite nameCmp;
+
+	public IntelligenceFilterDialog(Shell parentShell, IntelligenceListView view) {
+		super(parentShell);
+		this.view = view;
+		currentFilter = view.getFilter();
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 */
+	protected Control createDialogArea(Composite parent) {
+		final Composite filter = (Composite)super.createDialogArea(parent);
+		setMessage("Filter intelligences shown in the Intelligence List View.");
+		
+		Composite composite = new Composite((Composite) filter, SWT.NONE);
+		composite.setLayout(new GridLayout(1, false));
+		composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+
+		Composite receivedGrp = createGroupComposite("Received Date", composite);
+		receivedDateCmp = new DateFilterComposite(receivedGrp, SWT.NONE, this);
+		
+		Composite relevantGrp = createGroupComposite("Relevant Date(s)", composite);
+		relevantDateCmp = new DateFilterComposite(relevantGrp, SWT.NONE, this);
+
+		Composite nameGrp = createGroupComposite("Short Name", composite);
+		nameCmp = new StringFilterComposite(nameGrp, SWT.NONE);
+		nameCmp.setValueLabel("Short Name");
+		
+		updateControlsValues();
+		
+		return filter;
+	}
+
+	@Override
+	protected void configureShell(Shell shell) {
+		super.configureShell(shell);
+		shell.setText("Intelligence View Filter");
+	}
+	
+	@Override
+	protected void updateFilterModel() {
+		currentFilter.setReceivedDateFilter(receivedDateCmp.getDateFilterForModel(),
+				receivedDateCmp.getStartDateForModel(), receivedDateCmp.getEndDateForModel());
+
+		currentFilter.setRelevantDateFilter(relevantDateCmp.getDateFilterForModel(),
+				relevantDateCmp.getStartDateForModel(), relevantDateCmp.getEndDateForModel());
+		
+		currentFilter.setNameFilter(nameCmp.getComparisonForModel(), nameCmp.getFilterValueForModel());
+	}
+
+	@Override
+	protected void updateControlsValues() {
+		receivedDateCmp.applyState(currentFilter.getReceivedDateFilter(),
+				currentFilter.getReceivedDateStart(), currentFilter.getReceivedDateEnd());
+
+		relevantDateCmp.applyState(currentFilter.getRelevantDateFilter(),
+				currentFilter.getRelevantDateStart(), currentFilter.getRelevantDateEnd());
+		
+		nameCmp.applyState(currentFilter.getNameComparison(), currentFilter.getName());
+	}
+
+	@Override
+	protected void applyFilterToView() {
+		this.view.updateContent();
+	}
+
+	@Override
+	protected void resetFilterModel() {
+		currentFilter.resetDefaults();
+	}
+
+}
