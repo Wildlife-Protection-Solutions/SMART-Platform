8c53a2c3d006b30e29c6ffd5d50a26da353b1fbe
Emily Gouge <egouge@refractions.net>
2013-02-05 19:00:23 +0000

    fix for not committing changes to ca
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@750 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart.patrol/src/org/wcs/smart/patrol/internal/ui/views/PatrolFilterDialog.java
+++ source/java/org.wcs.smart.patrol/src/org/wcs/smart/patrol/internal/ui/views/PatrolFilterDialog.java
@@ -156,7 +156,7 @@ public class PatrolFilterDialog extends SmartFilterDialog {
 			
 			updateControlsValues();
 		} finally {
-			session.getTransaction().commit();
+			session.getTransaction().rollback();
 			session.close();
 		}
 		
