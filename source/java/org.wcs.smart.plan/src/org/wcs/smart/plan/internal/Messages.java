96d37d53d9130024f74ed832febfeb5c6fa10d25
Evgeniy Litvin <elitvin@refractions.net>
2013-02-15 01:12:24 +0000

    Localization for Plan and PlanEditor
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@838 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart.plan/src/org/wcs/smart/plan/internal/Messages.java
+++ source/java/org.wcs.smart.plan/src/org/wcs/smart/plan/internal/Messages.java
@@ -4,6 +4,33 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.wcs.smart.plan.internal.messages"; //$NON-NLS-1$
+	public static String PlanEditor_CannotOpenFile_Error;
+	public static String PlanEditor_ChildPlanTarges_Label;
+	public static String PlanEditor_Description_Label;
+	public static String PlanEditor_Edit_Link_Label;
+	public static String PlanEditor_EndDate_Label;
+	public static String PlanEditor_Loading_Label;
+	public static String PlanEditor_LoadPatrolsJob_Title;
+	public static String PlanEditor_Name_Label;
+	public static String PlanEditor_None_Label;
+	public static String PlanEditor_None_Text;
+	public static String PlanEditor_ParentPlan_Label;
+	public static String PlanEditor_Patrols_Label;
+	public static String PlanEditor_PlanId_Label;
+	public static String PlanEditor_PlanTarges_Label;
+	public static String PlanEditor_Refresh_Link_Label;
+	public static String PlanEditor_RefreshJob_Title;
+	public static String PlanEditor_StartDate_Label;
+	public static String PlanEditor_Station_Label;
+	public static String PlanEditor_Summary_Label;
+	public static String PlanEditor_Targets_Label;
+	public static String PlanEditor_Team_Label;
+	public static String PlanEditor_Type_Label;
+	public static String PlanEditor_UnavailableEmployees_Label;
+	public static String PlanType_ConservationArea;
+	public static String PlanType_Patrol;
+	public static String PlanType_Station;
+	public static String PlanType_Team;
 	public static String SpatialPlanTarget_CategoryName;
 	public static String SpatialPlanTarget_Points_Label;
 	public static String SpatialPlanTargetPropertyPage_Description_Label;
