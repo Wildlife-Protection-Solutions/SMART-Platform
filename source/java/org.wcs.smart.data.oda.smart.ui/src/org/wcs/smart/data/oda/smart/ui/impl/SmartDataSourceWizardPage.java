ee75132647b1e6ab586b75b72f740476fff024f9
Emily Gouge <egouge@refractions.net>
2013-02-21 20:11:40 +0000

    some cleanup in order to fix oda bug, still not fixed
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@919 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ source/java/org.wcs.smart.data.oda.smart.ui/src/org/wcs/smart/data/oda/smart/ui/impl/SmartDataSourceWizardPage.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.data.oda.smart.ui.impl;
+
+import java.util.Properties;
+
+import org.eclipse.datatools.connectivity.oda.design.ui.pages.impl.DefaultDataSourceWizardPage;
+import org.eclipse.datatools.connectivity.oda.design.ui.wizards.DataSourceWizardPage;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+/**
+ * A new wizard page for the smart data source.
+ * @author Emily
+ *
+ */
+public class SmartDataSourceWizardPage extends DefaultDataSourceWizardPage {
+
+	Properties p = new Properties();
+	public SmartDataSourceWizardPage(String pageName) {
+		super(pageName);
+	}
+
+	@Override
+	public Properties collectCustomProperties() {
+		return p;
+	}
+
+	@Override
+	public void createPageCustomControl(Composite arg0) {
+		Label lbl = new Label(arg0, SWT.NONE);
+		lbl.setText("The SMART datasource is not configurable.");
+		setTitle("SMART Data Source");
+	}
+
+	@Override
+	public void setInitialProperties(Properties prop) {
+		this.p = prop;
+
+	}
+
+}
