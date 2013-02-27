b5c2e08a78875fa93c8746abc93d9cc97e94e31f
Evgeniy Litvin <elitvin@refractions.net>
2013-02-27 18:12:09 +0000

    Update script to support "Distance For Completion" for SpacialPlanTarget
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@941 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart.util/src/main/resources/org/wcs/smart/db/updates/version_1.1.sql
+++ source/java/org.wcs.smart.util/src/main/resources/org/wcs/smart/db/updates/version_1.1.sql
@@ -1,2 +1,6 @@
 -- Database Updates For Version 1.1
 update smart.db_version set version = '1.1';
+
+-- "Add Distance To Complete"("Success Distance") for Plan Terget and set default value for previously added spacial targets to 250 --
+ALTER TABLE smart.plan_target ADD success_distance INTEGER;
+UPDATE smart.plan_target SET success_distance=250 WHERE category='SPATIAL';
