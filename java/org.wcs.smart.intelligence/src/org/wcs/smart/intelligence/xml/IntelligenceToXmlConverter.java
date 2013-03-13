76a5cef7485037e3df22d530d4e0b992faa7a6e3
Evgeniy Litvin <elitvin@refractions.net>
2013-03-13 20:44:55 +0000

    Intelligence XML export functionality
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@1005 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ java/org.wcs.smart.intelligence/src/org/wcs/smart/intelligence/xml/IntelligenceToXmlConverter.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.intelligence.xml;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeConstants;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.wcs.smart.ca.Label;
+import org.wcs.smart.intelligence.model.Intelligence;
+import org.wcs.smart.intelligence.model.IntelligenceAttachment;
+import org.wcs.smart.intelligence.model.IntelligencePoint;
+import org.wcs.smart.intelligence.xml.model.IntelligenceType;
+import org.wcs.smart.intelligence.xml.model.LabelType;
+import org.wcs.smart.intelligence.xml.model.PointType;
+
+/**
+ * Class responsible for converting intelligence object to XML.
+ * 
+ * @author elitvin
+ * @since 1.0.0
+ */
+public class IntelligenceToXmlConverter {
+
+	public static IntelligenceType toXml(Intelligence i) throws DatatypeConfigurationException {
+		IntelligenceType xml = new IntelligenceType();
+		
+		/* names */
+		for(Label label : i.getNames()) {
+			xml.getName().add(toXmlLabel(label));
+		}
+
+		/* dates */
+		xml.setReceivedDate(toXmlDate(i.getReceivedDate()));
+		xml.setFromDate(toXmlDate(i.getFromDate()));
+		xml.setToDate(toXmlDate(i.getToDate()));
+
+		/* source */
+		xml.setSource(i.getSource().name());
+
+		/* source */
+		xml.setDescription(i.getDescription());
+
+		/* points */
+		for(IntelligencePoint p : i.getPoints()) {
+			xml.getPoints().add(toXmlPoint(p));
+		}
+
+		/* attachments */
+		for(IntelligenceAttachment a : i.getAttachments()) {
+			xml.getAttachments().add(a.getFilename());
+		}
+		
+		return xml;
+	}
+
+	private static LabelType toXmlLabel(Label label){
+		if (label == null) {
+			return null;
+		}
+		LabelType lbl = new LabelType();
+		lbl.setLanguageCode(label.getLanguage().getCode());
+		lbl.setValue(label.getValue());
+		return lbl;
+	}
+
+	private static PointType toXmlPoint(IntelligencePoint p) {
+		if (p == null) {
+			return null;
+		}
+		PointType pt = new PointType();
+		pt.setX(p.getX());
+		pt.setY(p.getY());
+		return pt;
+	}
+	
+	/**
+	 * Converts a date to an xml date.  Sets the
+	 * XML timezone field to undefined so timezone information
+	 * is not included in output.
+	 * 
+	 * @param d
+	 * @return
+	 * @throws DatatypeConfigurationException
+	 */
+	private static XMLGregorianCalendar toXmlDate(Date d) throws DatatypeConfigurationException {
+		if (d == null) {
+			return null;
+		}
+		GregorianCalendar cal = (GregorianCalendar) GregorianCalendar.getInstance();
+		cal.setTime(d);
+		XMLGregorianCalendar xgc = DatatypeFactory.newInstance().newXMLGregorianCalendar(cal);
+		xgc.setTimezone(DatatypeConstants.FIELD_UNDEFINED);
+		xgc.setMillisecond(DatatypeConstants.FIELD_UNDEFINED);
+		xgc.setHour(DatatypeConstants.FIELD_UNDEFINED);
+		xgc.setMinute(DatatypeConstants.FIELD_UNDEFINED);
+		xgc.setSecond(DatatypeConstants.FIELD_UNDEFINED);
+		
+		return xgc;
+	}
+	
+}
