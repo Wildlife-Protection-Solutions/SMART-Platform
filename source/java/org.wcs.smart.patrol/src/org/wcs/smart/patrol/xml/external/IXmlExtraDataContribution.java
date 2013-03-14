cb477c239514bd38fff99d7bb12131f0a499d15e
Evgeniy Litvin <elitvin@refractions.net>
2013-03-14 22:16:54 +0000

    Extension Point for XML Export/Import extra-data contribution
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@1012 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ source/java/org.wcs.smart.patrol/src/org/wcs/smart/patrol/xml/external/IXmlExtraDataContribution.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.patrol.xml.external;
+
+import java.util.List;
+
+import org.wcs.smart.patrol.model.Patrol;
+import org.wcs.smart.patrol.xml.model.ExtraDataType;
+
+/**
+ * Contribution for the Patrol Export/Import functionality to handle
+ * "extra-data" section of XML file. Contributions are provided via
+ * extension point.
+ * 
+ * @author elitvin
+ * @since 1.0.0
+ */
+public interface IXmlExtraDataContribution {
+
+	/**
+	 * Extension id
+	 */
+	public static final String EXTENSION_ID = "org.wcs.smart.patrol.xml"; //$NON-NLS-1$
+
+	public List<ExtraDataType> exportData(Patrol patrol) throws Exception;
+	
+}
