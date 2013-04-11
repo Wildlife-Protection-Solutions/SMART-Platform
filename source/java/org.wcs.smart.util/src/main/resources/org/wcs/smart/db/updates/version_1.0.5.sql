bb45a4831ce28565e7f0ed4471806bc0b423ea3f
Emily Gouge <egouge@refractions.net>
2013-04-11 18:17:38 +0000

    rename next build to 1.0.5
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@1104 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart.util/src/main/resources/org/wcs/smart/db/updates/version_1.0.5.sql
+++ source/java/org.wcs.smart.util/src/main/resources/org/wcs/smart/db/updates/version_1.0.5.sql
@@ -1,5 +1,5 @@
 -- Database Updates For Version 1.1
-update smart.db_version set version = '1.1';
+update smart.db_version set version = '1.0.5';
 
 -- Add "Distance For Completion"("Success Distance") for Plan Target and set default value for previously added spacial targets to 250 --
 ALTER TABLE smart.plan_target ADD success_distance INTEGER;
