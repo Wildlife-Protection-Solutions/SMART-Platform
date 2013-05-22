76c6d517a84720dba1f82857c81d8990f7dc21a9
Emily Gouge <egouge@refractions.net>
2013-05-22 23:18:45 +0000

    fixes to intelligence export dialog
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@1260 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart.intelligence/src/org/wcs/smart/intelligence/xml/export/MultiIntelligenceExportDialog.java
+++ source/java/org.wcs.smart.intelligence/src/org/wcs/smart/intelligence/xml/export/MultiIntelligenceExportDialog.java
@@ -21,6 +21,8 @@
  */
 package org.wcs.smart.intelligence.xml.export;
 
+import java.io.File;
+import java.text.MessageFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -30,6 +32,7 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.DialogSettings;
 import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
@@ -42,6 +45,8 @@ import org.wcs.smart.intelligence.model.Intelligence;
 import org.wcs.smart.intelligence.ui.IIntelligenceFilteringView;
 import org.wcs.smart.intelligence.ui.IntelligenceFilterDialog;
 import org.wcs.smart.intelligence.ui.IntelligenceViewFilter;
+import org.wcs.smart.patrol.SmartPatrolPlugIn;
+import org.wcs.smart.util.SmartUtils;
 
 /**
  * Dialog to allow users to export multiple intelligences at once.
@@ -72,18 +77,44 @@ public class MultiIntelligenceExportDialog extends XmlMultiExportDialog implemen
 
 	@Override
 	protected Control createDialogArea(Composite parent) {
-		setTitle(Messages.MultiIntelligenceExportDialog_PageTitle);
+		setTitle(Messages.MultiIntelligenceExportDialog_PageTitle1);
 		setMessage(Messages.MultiIntelligenceExportDialog_Message);
-		getShell().setText(Messages.MultiIntelligenceExportDialog_Title);
+		getShell().setText(Messages.MultiIntelligenceExportDialog_Title1);
 		return super.createDialogArea(parent);
 	}
 	/**
 	 * @see org.eclipse.jface.dialogs.Dialog#buttonPressed(int)
 	 */
 	protected void buttonPressed(int buttonId) {
+		if (!validate()){
+			return;
+		}
 		super.buttonPressed(buttonId);
 		dialogSettings.put(OUTPUT_DIR, getDirectory());
 		dialogSettings.put(INCLUDE_ATTACHMENT, getIncludeAttachments());
+	}
+	
+	private boolean validate(){
+		
+		if (super.getTableViewer().getCheckedElements().length == 0) {
+			MessageDialog.openInformation(getShell(),  Messages.MultiIntelligenceExportDialog_Title1, Messages.ExportIntelligenceHandler_NothingSelected);
+			return false;
+		}
+		
+		File dir = new File(txtFile.getText());
+		if (!dir.exists()) {
+			if (!MessageDialog.openQuestion(getShell(), Messages.MultiIntelligenceExportDialog_Title1, MessageFormat.format(Messages.ExportIntelligenceHandler_DirectoryNotExist, new Object[]{dir.getAbsolutePath()}))) {
+				return false;
+			}
+			if (!SmartUtils.createDirectory(dir)){
+				SmartPatrolPlugIn.displayLog(Messages.MultiIntelligenceExportDialog_CouldNotCreateDirectoryError, null);
+				return false;
+			}
+		}else if (!dir.isDirectory()){
+			SmartPatrolPlugIn.displayLog(MessageFormat.format(Messages.MultiIntelligenceExportDialog_InvalidDirectoryError, new Object[]{dir.toString()}),null);
+			return false;
+		}
+		return true;
 	}
 
 	@Override
