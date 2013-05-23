29d5c3daa3c22fee998007918b061a3a95d6dafd
Emily Gouge <egouge@refractions.net>
2013-05-23 21:09:08 +0000

    fixing analyst permissions #596  #595
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@1277 37136b1e-4cb1-4992-8394-573b48a9bdce

--- java/org.wcs.smart.intelligence/src/org/wcs/smart/intelligence/ui/IntelligenceFilterDialog.java
+++ java/org.wcs.smart.intelligence/src/org/wcs/smart/intelligence/ui/IntelligenceFilterDialog.java
@@ -59,6 +59,7 @@ public class IntelligenceFilterDialog extends SmartFilterDialog {
 	protected Control createDialogArea(Composite parent) {
 		final Composite filter = (Composite)super.createDialogArea(parent);
 		setMessage(Messages.IntelligenceFilterDialog_Message);
+		setTitle("Intelligence Filter");
 		
 		Composite composite = new Composite((Composite) filter, SWT.NONE);
 		composite.setLayout(new GridLayout(1, false));
