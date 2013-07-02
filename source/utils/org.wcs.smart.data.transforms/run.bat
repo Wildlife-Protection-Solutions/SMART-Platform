8c177488d36ed99f8663b1bffd6cf8e2fd98530c
Emily Gouge <egouge@refractions.net>
2013-07-02 19:35:46 +0000

    addition of observation deleter
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@1488 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/utils/org.wcs.smart.data.transforms/run.bat
+++ source/utils/org.wcs.smart.data.transforms/run.bat
@@ -1,2 +1,3 @@
 java -cp %CP1%%CP2% org.wcs.smart.data.transforms.ObservationMerger %1 %2
 java -cp %CP1%%CP2% org.wcs.smart.data.transforms.ObservationWaypointMerger %1 %2
+java -cp %CP1%%CP2% org.wcs.smart.data.transforms.ObservationDeleter %1 %2
