36028a3ecbe0c9c9be58c5e384fec4a8a9fd2da1
Evgeniy Litvin <elitvin@refractions.net>
2013-07-04 16:53:21 +0000

    #11 Cybertracker Database Tables
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@1507 37136b1e-4cb1-4992-8394-573b48a9bdce

--- extensions/org.wcs.smart.cybertracker/src/org/wcs/smart/cybertracker/CyberTrackerPlugIn.java
+++ extensions/org.wcs.smart.cybertracker/src/org/wcs/smart/cybertracker/CyberTrackerPlugIn.java
@@ -1,10 +1,24 @@
 package org.wcs.smart.cybertracker;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.jdbc.Work;
 import org.osgi.framework.BundleContext;
+import org.wcs.smart.SmartPlugIn;
+import org.wcs.smart.cybertracker.internal.Messages;
+import org.wcs.smart.hibernate.HibernateManager;
+import org.wcs.smart.hibernate.SmartDB.DbUser;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -34,6 +48,7 @@ public class CyberTrackerPlugIn extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		buildTables();
 	}
 
 	/*
@@ -80,4 +95,80 @@ public class CyberTrackerPlugIn extends AbstractUIPlugin {
 		});
 	}
 	
+	/**
+	 * Ensures that required tables are present in database and add them is case they are not present
+	 */
+	private void buildTables() {
+		Job j = new Job(Messages.CyberTrackerPlugIn_InitJob_Title) {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				Session session = HibernateManager.openSession();
+				//check is required table exists
+				try {
+					session.beginTransaction();
+					String sql = "select count(*) from SYS.SYSTABLES tbl inner join SYS.SYSSCHEMAS sch on tbl.SCHEMAID = sch.SCHEMAID AND sch.SCHEMANAME = 'SMART' WHERE tbl.TABLETYPE = 'T' AND tbl.TABLENAME = 'CYBERTRACKER_PROPERTIES'"; //$NON-NLS-1$
+					SQLQuery q = session.createSQLQuery(sql);
+					Integer result = (Integer) q.uniqueResult();
+					if (result > 0)
+						return Status.OK_STATUS; //required table exists
+				} catch (Exception e) {
+			        getDefault().getLog().log(new Status(IStatus.ERROR, PLUGIN_ID, IStatus.OK, "Failed to obtain information about CyberTracker plugin tables.", e)); //$NON-NLS-1$
+					e.printStackTrace();
+				} finally {
+					if (session.getTransaction().isActive()) {
+						session.getTransaction().rollback();
+					}
+					session.close();
+				}
+				
+				// need to login as admin user to create tables
+				HibernateManager.setUserName(DbUser.ADMIN.getUserName(), DbUser.ADMIN.getPassword());
+				session = HibernateManager.openSession();
+				try {
+					session.beginTransaction();
+					session.doWork(new Work() {
+						@Override
+						public void execute(Connection c) throws SQLException {
+							String createSql = "CREATE TABLE smart.cybertracker_properties ("+ //$NON-NLS-1$
+									"uuid CHAR(16) for bit data NOT NULL, "+ //$NON-NLS-1$
+									"ca_uuid CHAR(16) for bit data  NOT NULL, "+ //$NON-NLS-1$
+									"application_name VARCHAR(256), "+ //$NON-NLS-1$
+									"kiosk_mode BOOLEAN, "+ //$NON-NLS-1$
+									"waypoint_timer INTEGER, "+ //$NON-NLS-1$
+									"gps_time_zone INTEGER, "+ //$NON-NLS-1$
+									"PRIMARY KEY (UUID))"; //$NON-NLS-1$
+
+							String alterSql = "ALTER TABLE smart.cybertracker_properties "+ //$NON-NLS-1$
+									"ADD CONSTRAINT cybertracker_properties_ca_uuid_fk FOREIGN KEY (CA_UUID) "+ //$NON-NLS-1$
+									"REFERENCES smart.conservation_area(UUID) "+ //$NON-NLS-1$
+									"ON UPDATE RESTRICT "+ //$NON-NLS-1$
+									"ON DELETE RESTRICT"; //$NON-NLS-1$
+
+							c.createStatement().execute(createSql);
+							c.createStatement().execute(alterSql);
+							
+							c.createStatement().execute("GRANT ALL PRIVILEGES ON smart.cybertracker_properties to data_entry"); //$NON-NLS-1$
+							c.createStatement().execute("GRANT ALL PRIVILEGES ON smart.cybertracker_properties to manager"); //$NON-NLS-1$
+							c.createStatement().execute("GRANT ALL PRIVILEGES ON smart.cybertracker_properties to analyst"); //$NON-NLS-1$
+						}
+					});
+					session.getTransaction().commit();
+				} catch (Exception ex) {
+			        getDefault().getLog().log(new Status(IStatus.ERROR, PLUGIN_ID, IStatus.OK, "Failed to create CyberTracker plugin tables.", ex)); //$NON-NLS-1$
+				} finally {
+					if (session.getTransaction().isActive()) {
+						session.getTransaction().rollback();
+					}
+					if (session.isOpen()) {
+						session.close();
+					}
+					// disconnect from admin user
+					HibernateManager.endSessionFactory(true);
+				}
+				return Status.OK_STATUS;
+			}
+		};
+//		j.setRule(SmartPlugIn.PLUGIN_START_MUTEX);
+		j.schedule();
+	}
 }
