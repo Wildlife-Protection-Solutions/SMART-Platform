67dfcb548070c9e8b56ff335baa8a12f03ada178
Emily Gouge <egouge@refractions.net>
2013-07-19 17:02:26 +0000

    position observation deleter
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@1587 37136b1e-4cb1-4992-8394-573b48a9bdce

--- utils/org.wcs.smart.data.transforms/run.bat
+++ utils/org.wcs.smart.data.transforms/run.bat
@@ -2,3 +2,4 @@ java -cp %CP1%%CP2% org.wcs.smart.data.transforms.EmptyObservationFinder %1 %2
 java -cp %CP1%%CP2% org.wcs.smart.data.transforms.ObservationMerger %1 %2
 java -cp %CP1%%CP2% org.wcs.smart.data.transforms.ObservationWaypointMerger %1 %2
 java -cp %CP1%%CP2% org.wcs.smart.data.transforms.ObservationDeleter %1 %2
+java -cp %CP1%%CP2% org.wcs.smart.data.transforms.PositionObservationDeleter %1 %2
