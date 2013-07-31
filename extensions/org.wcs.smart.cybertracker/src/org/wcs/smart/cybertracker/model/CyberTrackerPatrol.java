eaaf0177d374c9a44f02c637f603c4ec0b9740f7
Evgeniy Litvin <elitvin@refractions.net>
2013-07-31 19:24:09 +0000

    CyberTracker: Remove import logic from model class
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@1646 37136b1e-4cb1-4992-8394-573b48a9bdce

--- extensions/org.wcs.smart.cybertracker/src/org/wcs/smart/cybertracker/model/CyberTrackerPatrol.java
+++ extensions/org.wcs.smart.cybertracker/src/org/wcs/smart/cybertracker/model/CyberTrackerPatrol.java
@@ -21,31 +21,19 @@
  */
 package org.wcs.smart.cybertracker.model;
 
-import java.sql.Time;
-import java.text.DateFormat;
-import java.text.MessageFormat;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.hibernate.Session;
 import org.wcs.smart.ca.Employee;
 import org.wcs.smart.ca.Station;
-import org.wcs.smart.cybertracker.CyberTrackerHibernateManager;
-import org.wcs.smart.cybertracker.export.PatrolScreensUtil;
-import org.wcs.smart.cybertracker.importer.SmartImporter;
-import org.wcs.smart.cybertracker.internal.Messages;
 import org.wcs.smart.cybertracker.model.data.Data.Elements.E;
 import org.wcs.smart.cybertracker.model.data.Data.Sightings.S;
-import org.wcs.smart.cybertracker.model.data.Data.Sightings.S.A;
-import org.wcs.smart.hibernate.HibernateManager;
 import org.wcs.smart.patrol.model.PatrolMandate;
 import org.wcs.smart.patrol.model.PatrolTransportType;
 import org.wcs.smart.patrol.model.PatrolType;
-import org.wcs.smart.patrol.model.PatrolType.Type;
 import org.wcs.smart.patrol.model.Team;
 
 /**
@@ -103,147 +91,9 @@ public class CyberTrackerPatrol {
 	public CyberTrackerPatrol(Map<String, E> elementsMap, List<S> patrolData) {
 		this.elementsMap = elementsMap;
 		this.patrolData = patrolData;
-		Session session = HibernateManager.openSession();
-		session.beginTransaction();
-		try {
-			initMetaData(session);
-		} finally {
-			session.getTransaction().rollback();
-			session.close();
-		}
 	}
 
-	private void initMetaData(Session session) {
-		if (patrolData.isEmpty())
-			return;
-		S s = patrolData.get(0); //init metadata from the first sight (as all other sighs MUST have the same metadata by design)
-		Date date = null;
-		Time time = null;
-		DateFormat formatter = SmartImporter.createCyberTrackerDateFormatter();
-		
-		for (A a : s.getA()) {
-			String i = a.getI();
-			String n = a.getN();
-			String v = a.getV();
-			
-			if (ICyberTrackerConstants.DATE.equals(i)) {
-				try {
-					date = formatter.parse(v);
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
-			} else if (ICyberTrackerConstants.TIME.equals(i)) {
-				time = Time.valueOf(v);
-			} else if (PatrolScreensUtil.RESULT_PATROL_ID.equals(n)) {
-				setId(v);
-			} else if (PatrolScreensUtil.RESULT_PATROL_TYPE.equals(n)) {
-				E e = getElementsMap().get(v);
-				String tag0 = e != null ? e.getTag0() : null;
-				if (tag0 != null) {
-					setPatrolType(Type.valueOf(e.getTag0()));
-				}
-			} else if (PatrolScreensUtil.RESULT_TRANSPORT.equals(n)) {
-				E e = getElementsMap().get(v);
-				PatrolTransportType transportType = fetchFromTag0(PatrolTransportType.class, e, session);
-				if (transportType == null)
-					addProblem(PatrolMeta.TRANSPORT, MessageFormat.format(Messages.CyberTrackerPatrol_Error_Transport, e.getN()));
-				setCtTransport(e.getN());
-				setPatrolTransportType(transportType);
-			} else if (PatrolScreensUtil.RESULT_ARMED.equals(n)) {
-				E e = getElementsMap().get(v);
-				String tag0 = e != null ? e.getTag0() : null;
-				if (tag0 != null) {
-					setArmed("true".equals(tag0.toLowerCase())); //$NON-NLS-1$
-				}				
-			} else if (PatrolScreensUtil.RESULT_TEAM.equals(n)) {
-				E e = getElementsMap().get(v);
-				Team t = fetchFromTag0(Team.class, e, session);
-				if (t == null && e.getTag0() != null)
-					addProblem(PatrolMeta.TEAM, MessageFormat.format(Messages.CyberTrackerPatrol_Warn_Team, e.getN()));
-				setCtTeam(e.getN());
-				setTeam(t);
-			} else if (PatrolScreensUtil.RESULT_STATION.equals(n)) {
-				E e = getElementsMap().get(v);
-				Station st = fetchFromTag0(Station.class, e, session);
-				if (st == null && e.getTag0() != null)
-					addProblem(PatrolMeta.STATION, MessageFormat.format(Messages.CyberTrackerPatrol_Warn_Station, e.getN()));
-				setCtStation(e.getN());
-				setStation(st);
-			} else if (PatrolScreensUtil.RESULT_MANDATE.equals(n)) {
-				E e = getElementsMap().get(v);
-				PatrolMandate m = fetchFromTag0(PatrolMandate.class, e, session);
-				if (m == null && e.getTag0() != null)
-					addProblem(PatrolMeta.MANDATE, MessageFormat.format(Messages.CyberTrackerPatrol_Warn_Mandate, e.getN()));
-				setMandate(m);
-			} else if (PatrolScreensUtil.RESULT_OBJECTIVE.equals(n)) {
-				setObjective(v);
-			} else if (PatrolScreensUtil.RESULT_COMMENTS.equals(n)) {
-				setComment(v);
-			} else if (PatrolScreensUtil.RESULT_LEADER.equals(n)) {
-				E e = getElementsMap().get(v);
-				Employee emp = fetchFromTag0(Employee.class, e, session);
-				if (emp == null && e.getTag0() != null)
-					addProblem(PatrolMeta.LEADER, MessageFormat.format(Messages.CyberTrackerPatrol_Warn_Leader, e.getN()));
-				setCtLeader(e.getN());
-				setLeader(emp);
-			} else if (PatrolScreensUtil.RESULT_PILOT.equals(n)) {
-				E e = getElementsMap().get(v);
-				Employee emp = fetchFromTag0(Employee.class, e, session);
-				if (emp == null && e.getTag0() != null)
-					addProblem(PatrolMeta.PILOT, MessageFormat.format(Messages.CyberTrackerPatrol_Warn_Pilot, e.getN()));
-				setCtPilot(e.getN());
-				setPilot(emp);
-			} else if (isMemberRecord(a)) {
-				E e = getElementsMap().get(i);
-				Employee emp = fetchFromTag0(Employee.class, e, session);
-				if (emp == null && e.getTag0() != null)
-					addProblem(PatrolMeta.MEMBERS, MessageFormat.format(Messages.CyberTrackerPatrol_Warn_Member, e.getN()));
-				getCtMembers().add(e.getN());
-				if (emp != null) {
-					getMembers().add(emp);
-				}
-			}
-		}
-		setStartDate(SmartImporter.combine(date, time));
-		date = null;
-		time = null;
-		
-		s = patrolData.get(patrolData.size()-1); //need to find end date
-		for (A a : s.getA()) {
-			String i = a.getI();
-			if (ICyberTrackerConstants.DATE.equals(i)) {
-				try {
-					date = formatter.parse(a.getV());
-					if (time != null)
-						break;
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
-			} else if (ICyberTrackerConstants.TIME.equals(i)) {
-				time = Time.valueOf(a.getV());
-				if (date != null)
-					break;
-			}
-		}
-		setEndDate(SmartImporter.combine(date, time));
-	}
-
-	private boolean isMemberRecord(A a) {
-		if (!ICyberTrackerConstants.STR_TRUE.equals(a.getV()))
-			return false;
-		//TODO: check if this is really member (use tag1?)
-		return true;
-	}
-
-	private <T> T fetchFromTag0(Class<T> clazz, E e, Session session) {
-		String tag0 = e != null ? e.getTag0() : null;
-		if (tag0 != null) {
-			return CyberTrackerHibernateManager.fetchByUuid(clazz, tag0, session);
-		}
-		return null;
-	}
-
-	protected void addProblem(PatrolMeta area, String problem) {
+	public void addProblem(PatrolMeta area, String problem) {
 		if (!problems.containsKey(area))
 			problems.put(area, new ArrayList<String>());
 		problems.get(area).add(problem);
