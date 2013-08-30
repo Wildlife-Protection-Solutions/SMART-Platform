c8b688194875836ee93a9cccc9306e24c0b2f4d2
Emily Gouge <egouge@refractions.net>
2013-08-30 20:28:42 +0000

    db updates for tickets 644, 691, 692, 696
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@1729 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ java/org.wcs.smart.util/src/main/java/org/wcs/smart/db/KeyGenerator.java
@@ -0,0 +1,138 @@
+package org.wcs.smart.db;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.regex.Pattern;
+
+public class KeyGenerator {
+	private static final String INVALID_START_CHARS_KEY_PATTERN = "[^a-z]+"; //$NON-NLS-1$
+	private static final String VALID_DM_KEY_PATTERN = "[a-z]{1}[a-z0-9_]*"; //$NON-NLS-1$
+	
+	public void generateKeys(Connection c, String tableName) throws Exception{
+		
+		String sql = "SELECT a.uuid, b.value, a.ca_uuid from " + tableName + 
+			" a LEFT JOIN ( smart.i18n_label b JOIN smart.LANGUAGE c ON b.language_uuid = c.uuid and c.isdefault ) " +
+			"on a.uuid = b.element_uuid";
+		
+
+		ResultSet rs = c.createStatement().executeQuery(sql);
+		
+		ArrayList<Object[]> keys = new ArrayList<Object[]>();
+		HashMap<String,HashSet<String>> usedKeys = new HashMap<String,HashSet<String>>();
+		
+		
+		while(rs.next()){
+			byte[] uuid = rs.getBytes(1);
+			String name = rs.getString(2);
+			byte[] cauuid = rs.getBytes(3);
+			
+			HashSet<String> existingKeys = usedKeys.get(Arrays.toString(cauuid));
+			if (existingKeys == null){
+				existingKeys = new HashSet<String>();
+				usedKeys.put(Arrays.toString(cauuid), existingKeys);
+			}
+			if (name == null){
+				name = tableName;
+			}
+			
+			String key = generateKey(name, existingKeys);
+			
+			existingKeys.add(key);
+			keys.add(new Object[]{uuid, key});
+		}
+
+		
+		PreparedStatement ps = c.prepareStatement("UPDATE " + tableName + " set keyid = ? where uuid = ?");
+		for (Object[] updates : keys){
+			ps.setString(1, (String)updates[1]);
+			ps.setBytes(2, (byte[])updates[0]);
+			ps.execute();
+		}
+		
+		c.commit();
+	}
+	
+	private static String generateKey(String name, HashSet<String> usedkeys){
+
+		String raw = name.toLowerCase().replaceAll("[^a-z0-9_]", ""); //$NON-NLS-1$ //$NON-NLS-2$
+		if (raw.isEmpty()){
+			raw = "key";
+		}
+		
+		//remove _ and 1 
+		while(raw.length() > 0 && Pattern.matches(INVALID_START_CHARS_KEY_PATTERN, raw.subSequence(0, 1))){
+			raw = raw.replaceFirst(INVALID_START_CHARS_KEY_PATTERN, "");
+		}
+		if(raw.isEmpty()){
+			raw = "key";
+		}
+		
+		String temp = raw;
+		int i = 1;
+		while(usedkeys.contains(temp)){
+			temp = raw + i;
+			i++;
+		}
+		raw = temp;
+		return raw;
+	}
+	
+	private static Connection getConnection(File databaseFile) throws Exception{
+		String driver = "org.apache.derby.jdbc.EmbeddedDriver"; 
+		Class.forName(driver).newInstance();
+		
+		String queryString = "jdbc:derby:" + databaseFile.getAbsolutePath() + ";user=smart_admin;password=smart_derby";
+		
+		Connection c = DriverManager.getConnection(queryString);
+		c.setAutoCommit(false);
+		return c;
+	}
+	
+	public static void main(String args[]) throws Exception{
+		File dbFile = new File("C:\\data\\SMART\\Source\\Version1\\trunk\\source\\java\\org.wcs.smart\\data\\database\\smartdb");
+		Connection c = getConnection(dbFile);
+		
+		KeyGenerator kg = new KeyGenerator();
+		kg.generateKeys(c, "smart.patrol_mandate");
+		kg.generateKeys(c, "smart.team");
+		kg.generateKeys(c, "smart.patrol_transport");
+		
+	}
+	
+	private static void test(){
+		String[] keys = new String[]{ "1this is Me I am.#@$%@$#%!$#_@%^ASDF @!$#%",
+				"1234",
+				"I Am . Big",
+				"@#$%&",
+				"_an underscore",
+				"min",
+				"max",
+				"and",
+				"i8 this and that"
+		};
+		for (String key : keys){
+			String newKey = KeyGenerator.generateKey(key, new HashSet<String>());
+			System.out.println(key + " : " + newKey);
+		}
+		
+		System.out.println(" ------------------------------------------------ ");
+		HashSet<String> ukeys = new HashSet<String>();
+		ukeys.add("abc");
+		ukeys.add("abc1");
+		
+		keys = new String[]{ "abc",
+				"1abc",
+		};
+		for (String key : keys){
+			String newKey = KeyGenerator.generateKey(key, ukeys);
+			System.out.println(key + " : " + newKey);
+		}
+	}
+}
---
+++ java/org.wcs.smart.util/src/main/resources/org/wcs/smart/db/updates/version_2.0.0.sql
@@ -0,0 +1,29 @@
+alter table smart.saved_maps drop column employee_uuid;
+
+insert into smart.ca_projection(uuid, ca_uuid, name, definition, is_default)
+values (x'00000000000000000000000000000001',x'00000000000000000000000000000000','WGS 84 [EPSG: 4326]', 'GEOGCS["WGS 84", DATUM["World Geodetic System 1984", SPHEROID["WGS 84", 6378137.0, 298.257223563, AUTHORITY["EPSG","7030"]], AUTHORITY["EPSG","6326"]], PRIMEM["Greenwich", 0.0, AUTHORITY["EPSG","8901"]], UNIT["degree", 0.017453292519943295], AXIS["Geodetic longitude", EAST], AXIS["Geodetic latitude", NORTH], AUTHORITY["EPSG","4326"]]', true)
+
+
+-- ADD keys for team, mandate, and transport for cross-ca analysis
+alter table smart.team add column keyid varchar(128);
+alter table smart.patrol_mandate add column keyid varchar(128);
+alter table smart.patrol_transport add column keyid varchar(128);
+
+-- this will not do; keys need to be unique (per conservation area) 
+-- only contain certain characters
+-- and not start with numbers
+--see KeyGenerator - the update script will need to run KeyGenerator for these tables
+--update smart.team set keyid = (select lower(a.value) from smart.i18n_label a, smart.LANGUAGE c 
+--where smart.team.uuid = a.element_uuid and a.language_uuid = c.uuid and c.isdefault);
+--
+--update smart.patrol_mandate set keyid = (select lower(a.value) from smart.i18n_label a, smart.LANGUAGE c 
+--where smart.patrol_mandate.uuid = a.element_uuid and a.language_uuid = c.uuid and c.isdefault);
+--
+--update smart.patrol_transport set keyid = (select lower(a.value) from smart.i18n_label a, smart.LANGUAGE c 
+--where smart.patrol_transport.uuid = a.element_uuid and a.language_uuid = c.uuid and c.isdefault);
+
+
+
+
+
+update smart.db_version set version = '2.0.0';
