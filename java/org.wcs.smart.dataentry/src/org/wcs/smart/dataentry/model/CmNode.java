7030d3ae4c874ea7d83457cedb8f62eb488903e5
Evgeniy Litvin <elitvin@refractions.net>
2013-09-03 21:52:32 +0000

    Model classes stub
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@1737 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ java/org.wcs.smart.dataentry/src/org/wcs/smart/dataentry/model/CmNode.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.dataentry.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.wcs.smart.ca.NamedItem;
+import org.wcs.smart.ca.datamodel.Category;
+
+/**
+ * @author elitvin
+ * @since 2.0.0
+ */
+public class CmNode extends NamedItem {
+
+	private ConfigurableModel model; 
+	private Category category; 
+	private CmNode parent; 
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="cm_uuid", referencedColumnName="uuid")
+	public ConfigurableModel getModel() {
+        return model;
+    }
+    public void setModel(ConfigurableModel model) {
+        this.model = model;
+    }
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="category_uuid", referencedColumnName="uuid")
+	public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    
+	/**
+	 * 
+	 * @return parent node;  <code>null</code> if root node
+	 */
+	@ManyToOne(fetch = FetchType.LAZY, cascade={CascadeType.ALL})
+	@JoinColumn(name="parent_node_uuid", referencedColumnName="uuid")
+	public CmNode getParent() {
+		return this.parent;
+	}
+	public void setParent(CmNode parent) {
+		this.parent = parent;
+	}
+
+	
+    
+}
