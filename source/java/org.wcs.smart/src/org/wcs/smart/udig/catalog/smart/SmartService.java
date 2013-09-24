fca3527b06873baeb807315b434a714b7c18d4c5
Emily Gouge <egouge@refractions.net>
2013-09-24 20:52:11 +0000

    update service status related to #764
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@1812 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart/src/org/wcs/smart/udig/catalog/smart/SmartService.java
+++ source/java/org.wcs.smart/src/org/wcs/smart/udig/catalog/smart/SmartService.java
@@ -77,6 +77,9 @@ public class SmartService extends IService {
 	 */
 	@Override
 	public Status getStatus() {
+		if (SmartDB.isMultipleAnalysis()){
+			return Status.RESTRICTED_ACCESS;
+		}
 		return Status.CONNECTED;
 	}
 
