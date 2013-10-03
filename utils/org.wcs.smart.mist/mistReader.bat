bccd472030f9995411eed4b3dbdfd116c7fc017c
Emily Gouge <egouge@refractions.net>
2013-10-03 20:15:34 +0000

    utility program to read basic MIST data
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@1854 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ utils/org.wcs.smart.mist/mistReader.bat
@@ -0,0 +1,13 @@
+@echo off
+REM -------------------------------------------------------------
+REM Script to read a MUST DB and output the stations, employees, agencies&ranks and patrol types
+REM Usage from DOS: Read_Mist.bat <DB Filename/Location>
+
+echo "Enter Database Name:"
+set /p DBName=
+
+SET LAUNCHDIR=%~dp0
+SET FIREBIRDDIR=%LAUNCHDIR%%FIREBIRDDIR%
+
+set PATH=%PATH%;%FIREBIRDDIR%
+"jre\bin\java.exe" -cp %CP1%%CP2% org.wcs.smart.mist.dataReader.DataReader %DBName%
