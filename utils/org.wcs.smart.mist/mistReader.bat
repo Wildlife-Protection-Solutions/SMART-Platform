75814454ea5fa086a6d6a3d8cfa9e2769d4f364e
Jeff Lounsbury <jeffloun@refractions.net>
2013-10-04 20:34:05 +0000

    updated MistReader Utililty
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@1866 37136b1e-4cb1-4992-8394-573b48a9bdce

--- utils/org.wcs.smart.mist/mistReader.bat
+++ utils/org.wcs.smart.mist/mistReader.bat
@@ -6,8 +6,12 @@ REM Usage from DOS: Read_Mist.bat <DB Filename/Location>
 echo "Enter Database Name:"
 set /p DBName=
 
+echo "Enter Language Code(en, fr, th...):"
+set /p Lang=
+
+
 SET LAUNCHDIR=%~dp0
 SET FIREBIRDDIR=%LAUNCHDIR%%FIREBIRDDIR%
 
 set PATH=%PATH%;%FIREBIRDDIR%
-"jre\bin\java.exe" -cp %CP1%%CP2% org.wcs.smart.mist.dataReader.DataReader %DBName%
+"jre\bin\java.exe" -cp %CP1%%CP2% org.wcs.smart.mist.dataReader.DataReader %DBName% %Lang%
