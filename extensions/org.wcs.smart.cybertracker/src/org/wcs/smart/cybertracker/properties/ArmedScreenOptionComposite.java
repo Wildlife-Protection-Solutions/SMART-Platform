2adf5675262e4d6b2c4d55d7c9d9fb662c81f064
Evgeniy Litvin <elitvin@refractions.net>
2013-10-07 21:40:28 +0000

    Armed/Mandete config info panels
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@1874 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ extensions/org.wcs.smart.cybertracker/src/org/wcs/smart/cybertracker/properties/ArmedScreenOptionComposite.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.cybertracker.properties;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.wcs.smart.cybertracker.internal.Messages;
+import org.wcs.smart.cybertracker.model.CyberTrackerPatrolOption;
+
+/**
+ * Screen armed option configuration panel.
+ * 
+ * @author elitvin
+ * @since 2.0.0
+ */
+public class ArmedScreenOptionComposite extends Composite {
+
+	private CyberTrackerPatrolOption model;
+	
+	private Button btnDisplayPage;
+	private Button btnArmed;
+
+	public ArmedScreenOptionComposite(Composite parent, CyberTrackerPatrolOption model) {
+		super(parent, SWT.NONE);
+		this.model = model;
+
+		createControls();
+	}
+
+	protected void createControls() {
+		this.setLayout(new GridLayout(1, false));
+		this.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		
+		Group group = new Group(this,  SWT.NONE);
+		group.setText(model.getType().getGuiLabel());
+		group.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		group.setLayout(new GridLayout(2, false));
+
+		Label label = new Label(group, SWT.NONE);
+		label.setText(Messages.ScreenOptionComposite_DisplayPage);
+		btnDisplayPage = new Button(group, SWT.CHECK);
+		btnDisplayPage.setSelection(model.isVisible());
+		btnDisplayPage.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				btnArmed.setEnabled(!btnDisplayPage.getSelection());
+			}
+		});
+		
+		label = new Label(group, SWT.NONE);
+		label.setText(Messages.ScreenOptionComposite_DefaultValue);
+		btnArmed = new Button(group, SWT.CHECK);
+		btnArmed.setText("Armed?");
+		btnArmed.setEnabled(!model.isVisible());
+		btnArmed.setSelection(Boolean.TRUE.equals(model.getBooleanValue()));
+		btnArmed.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				model.setBooleanValue(btnArmed.getSelection());
+			}
+		});
+		
+	}
+	
+}
