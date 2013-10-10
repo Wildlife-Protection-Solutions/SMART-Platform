ebe6de6acaa02e78fad5efe955293195bcfd9ac9
Evgeniy Litvin <elitvin@refractions.net>
2013-10-10 16:54:33 +0000

    ScreenOption configuration for Objective/Comment
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@1898 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ java/org.wcs.smart.patrol/src/org/wcs/smart/patrol/meta/TextScreenOptionComposite.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.patrol.meta;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.wcs.smart.patrol.model.ScreenOption;
+
+/**
+ * Screen option with text control for default value.
+ * 
+ * @author elitvin
+ * @since 2.0.0
+ */
+public class TextScreenOptionComposite extends ScreenOptionComposite {
+
+	/**
+	 * @param parent
+	 */
+	public TextScreenOptionComposite(Composite parent, ScreenOption model) {
+		super(parent);
+		TextScreenOptionGroup grp = new TextScreenOptionGroup(this, model);
+		grp.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+	}
+
+	private class TextScreenOptionGroup extends ScreenOptionGroup {
+
+		private Text text;
+
+		public TextScreenOptionGroup(Composite parent, ScreenOption option) {
+			super(parent, option);
+		}
+
+		@Override
+		protected void createDefaultControl(Group group) {
+			new Label(group, SWT.NONE);
+			text = new Text(group, SWT.BORDER | SWT.MULTI | SWT.WRAP | SWT.V_SCROLL);
+			GridData gd = new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1);
+//			gd.widthHint = 150;
+//			gd.heightHint = 80;
+			text.setLayoutData(gd);
+			text.setEnabled(!getModel().isVisible());
+			text.addModifyListener(new ModifyListener() {
+				@Override
+				public void modifyText(ModifyEvent e) {
+					getModel().setStringValue(text.getText());
+					fireScreenOptionListeners();
+				}
+			});
+		}
+
+		@Override
+		protected void onBtnDisplayPageClick() {
+			boolean visible = getBtnDisplayPage().getSelection();
+			getModel().setVisible(visible);
+			text.setEnabled(!visible);
+			fireScreenOptionListeners();
+		}
+
+	}
+}
