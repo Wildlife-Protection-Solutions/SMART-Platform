a5f546c5d7dd60d84649970d6041192a8cb9b354
Emily Gouge <egouge@refractions.net>
2013-10-30 22:29:47 +0000

    update
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@2017 37136b1e-4cb1-4992-8394-573b48a9bdce

--- utils/org.wcs.smart.upgrade.v200/src/org/wcs/smart/upgrade/v200/smart-tables-dataentry.sql
+++ utils/org.wcs.smart.upgrade.v200/src/org/wcs/smart/upgrade/v200/smart-tables-dataentry.sql
@@ -39,11 +39,21 @@ CREATE TABLE smart.cm_attribute_option
 CREATE TABLE smart.cm_attribute_list
 (
 	UUID CHAR(16) for bit data NOT NULL,
-	CM_ATTRIBUTE_UUID CHAR(16) for bit data  NOT NULL,
+	CM_UUID CHAR(16) for bit data  NOT NULL,
 	LIST_ELEMENT_UUID CHAR(16) for bit data  NOT NULL,
 	IS_ACTIVE BOOLEAN  NOT NULL,
 	PRIMARY KEY (UUID)
 );
+
+CREATE TABLE smart.cm_attribute_tree_node
+(
+	UUID CHAR(16) for bit data NOT NULL,
+	CM_UUID CHAR(16) for bit data  NOT NULL,
+	DM_TREE_NODE_UUID CHAR(16) for bit data  NOT NULL,
+	IS_ACTIVE BOOLEAN  NOT NULL,
+	PRIMARY KEY (UUID)
+);
+
 
 ALTER TABLE smart.configurable_model
 	ADD CONSTRAINT configurable_model_ca_uuid_fk FOREIGN KEY (CA_UUID)
@@ -88,8 +98,8 @@ ALTER TABLE smart.cm_attribute_option
 ;
 
 ALTER TABLE smart.cm_attribute_list
-	ADD CONSTRAINT cm_attribute_list_cm_attribute_uuid_fk FOREIGN KEY (CM_ATTRIBUTE_UUID)
-	REFERENCES smart.cm_attribute(UUID)
+	ADD CONSTRAINT cm_attribute_list_cm_uuid_fk FOREIGN KEY (CM_UUID)
+	REFERENCES smart.configurable_model(UUID)
 	ON UPDATE RESTRICT
 	ON DELETE CASCADE
 ;
@@ -97,6 +107,20 @@ ALTER TABLE smart.cm_attribute_list
 ALTER TABLE smart.cm_attribute_list
 	ADD CONSTRAINT cm_attribute_list_list_element_uuid_fk FOREIGN KEY (LIST_ELEMENT_UUID)
 	REFERENCES smart.dm_attribute_list(UUID)
+	ON UPDATE RESTRICT
+	ON DELETE CASCADE
+;
+
+ALTER TABLE smart.cm_attribute_tree_node
+	ADD CONSTRAINT cm_attribute_tree_node_tree_node_uuid_fk FOREIGN KEY (DM_TREE_NODE_UUID)
+	REFERENCES smart.dm_attribute_tree(UUID)
+	ON UPDATE RESTRICT
+	ON DELETE CASCADE
+;
+
+ALTER TABLE smart.cm_attribute_tree_node
+	ADD CONSTRAINT cm_attribute_tree_node_cm_uuid_fk FOREIGN KEY (CM_UUID)
+	REFERENCES smart.configurable_model(UUID)
 	ON UPDATE RESTRICT
 	ON DELETE CASCADE
 ;
