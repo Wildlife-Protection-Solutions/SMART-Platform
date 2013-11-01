4baf521fad5d5a8778c8b49f7a514b1b7c901a3a
Emily Gouge <egouge@refractions.net>
2013-11-01 20:21:03 +0000

    ui fixes for long names
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@2026 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart.patrol/src/org/wcs/smart/patrol/internal/ui/PatrolTransportComposite.java
+++ source/java/org.wcs.smart.patrol/src/org/wcs/smart/patrol/internal/ui/PatrolTransportComposite.java
@@ -75,8 +75,10 @@ public class PatrolTransportComposite extends PatrolLegItemComposite{
 		lbl.setText(Messages.PatrolTransportComposite_TransportType_Lable);
 		lbl.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
 		
-		patrolTypeViewer = new ComboViewer(center, SWT.READ_ONLY);
+		patrolTypeViewer = new ComboViewer(center, SWT.READ_ONLY | SWT.DROP_DOWN);
 		patrolTypeViewer.getControl().setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		((GridData)patrolTypeViewer.getControl().getLayoutData()).widthHint = 100;
+		
 		patrolTypeViewer.setContentProvider(ArrayContentProvider.getInstance());
 		patrolTypeViewer.setLabelProvider(new LabelProvider(){
 			public String getText(Object element) {
