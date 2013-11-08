ced8f8847fafeb6b299487ca6e680793715b8356
Jeff Lounsbury <jeffloun@refractions.net>
2013-11-08 20:19:44 +0000

    dmm updates
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@2058 37136b1e-4cb1-4992-8394-573b48a9bdce

--- utils/org.wcs.smart.mist/src/org/wcs/smart/datamodelmatcher/ui/DataModelMatcher.java
+++ utils/org.wcs.smart.mist/src/org/wcs/smart/datamodelmatcher/ui/DataModelMatcher.java
@@ -1,5 +1,8 @@
 package org.wcs.smart.datamodelmatcher.ui;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
@@ -8,9 +11,13 @@ public class DataModelMatcher {
 	  public static void main(String[] args) {
 			Display display = new Display();
 			Shell shell = new Shell(display);
+			GridLayout layout = new GridLayout(1, true);
+			shell.setLayout(layout);
+		    
+		    GridData gridData = new GridData(SWT.LEFT,SWT.TOP, false, false);
+		    shell.setLayoutData(gridData);
 			
 			shell.setText("Data Model Matcher");
-			
 			DataModelMatcherDialog main = new DataModelMatcherDialog(shell);
 
 			shell.pack();
