466c5d6e892da910046f6bfb4409e179b21812ad
Emily Gouge <egouge@refractions.net>
2013-11-22 23:58:37 +0000

    addition of field data menu addition of plugin for ui for idenpdendent incidents
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@2104 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ java/org.wcs.smart.independentincident/src/org/wcs/smart/incident/ui/IndIncidentListView.java
@@ -0,0 +1,27 @@
+package org.wcs.smart.incident.ui;
+
+import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class IndIncidentListView extends ViewPart {
+
+	private ListViewer list;
+	
+	public IndIncidentListView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
