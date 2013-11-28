787c8c16b9950278d31c7a2fb448643aa6e378bb
Evgeniy Litvin <elitvin@refractions.net>
2013-11-28 22:36:47 +0000

    Intelligence points data set for pdf export
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@2118 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ java/org.wcs.smart.intelligence/src/org/wcs/smart/intelligence/report/oda/IntelligencePointsQuery.java
@@ -0,0 +1,381 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.intelligence.report.oda;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.HashMap;
+
+import org.eclipse.datatools.connectivity.oda.IParameterMetaData;
+import org.eclipse.datatools.connectivity.oda.IQuery;
+import org.eclipse.datatools.connectivity.oda.IResultSet;
+import org.eclipse.datatools.connectivity.oda.IResultSetMetaData;
+import org.eclipse.datatools.connectivity.oda.OdaException;
+import org.eclipse.datatools.connectivity.oda.SortSpec;
+import org.eclipse.datatools.connectivity.oda.spec.QuerySpecification;
+
+/**
+ * SMART Intelligence Location points target query.
+ * 
+ * @author elitvin
+ * @since 3.0.0
+ */
+public class IntelligencePointsQuery implements IQuery {
+
+	public static final String ID = "org.wcs.smart.intelligence.report.oda.SmartIntelligencePoints"; //$NON-NLS-1$
+
+	private int m_maxRows;
+
+	//dataset parameters
+	private HashMap<Integer, Object> parameters = null;
+
+	//dataset metadata
+	private IntelligencePointsParameterMetaData pMetadata = null;
+	
+	/**
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#prepare(java.lang.String)
+	 * <p></p>
+	 */
+	public void prepare(String queryText) throws OdaException {
+		parameters = new HashMap<Integer, Object>();
+	}
+
+	/**
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#setAppContext(java.lang
+	 * .Object)
+	 */
+	public void setAppContext(Object context) throws OdaException {
+		// do nothing; assumes no support for pass-through context
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#close()
+	 */
+	public void close() throws OdaException {
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#getMetaData()
+	 */
+	public IResultSetMetaData getMetaData() throws OdaException {
+		return new IntelligencePointsResultSetMetadata();
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#executeQuery()
+	 */
+	public IResultSet executeQuery() throws OdaException {
+		String[] uuids = ((String)parameters.get(1)).split(","); //$NON-NLS-1$
+		return new IntelligencePointsResultSet(uuids, (IntelligencePointsResultSetMetadata)getMetaData());
+	}
+
+	/**
+	 * Properties not supported.
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#setProperty(java.lang.String
+	 * , java.lang.String)
+	 */
+	public void setProperty(String name, String value) throws OdaException {
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setMaxRows(int)
+	 */
+	public void setMaxRows(int max) throws OdaException {
+		m_maxRows = max;
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#getMaxRows()
+	 */
+	public int getMaxRows() throws OdaException {
+		return m_maxRows;
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#clearInParameters()
+	 */
+	public void clearInParameters() throws OdaException {
+		parameters.clear();
+	}
+
+	/**
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#setInt(java.lang.String,
+	 * int)
+	 */
+	public void setInt(String parameterName, int value) throws OdaException {
+		setObject(parameterName, value);
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setInt(int, int)
+	 */
+	public void setInt(int parameterId, int value) throws OdaException {
+		setObject(parameterId, value);
+	}
+
+	/**
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#setDouble(java.lang.String,
+	 * double)
+	 */
+	public void setDouble(String parameterName, double value)
+			throws OdaException {
+		setObject(parameterName, value);
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setDouble(int, double)
+	 */
+	public void setDouble(int parameterId, double value) throws OdaException {
+		setObject(parameterId, value);
+	}
+
+	/**
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#setBigDecimal(java.lang
+	 * .String, java.math.BigDecimal)
+	 */
+	public void setBigDecimal(String parameterName, BigDecimal value)
+			throws OdaException {
+		setObject(parameterName, value);
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setBigDecimal(int,
+	 * java.math.BigDecimal)
+	 */
+	public void setBigDecimal(int parameterId, BigDecimal value)
+			throws OdaException {
+		setObject(parameterId, value);
+	}
+
+	/**
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#setString(java.lang.String,
+	 * java.lang.String)
+	 */
+	public void setString(String parameterName, String value)
+			throws OdaException {
+		setObject(parameterName, value);
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setString(int,
+	 * java.lang.String)
+	 */
+	public void setString(int parameterId, String value) throws OdaException {
+		setObject(parameterId, value);
+	}
+
+	/**
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#setDate(java.lang.String,
+	 * java.sql.Date)
+	 */
+	public void setDate(String parameterName, Date value) throws OdaException {
+		setObject(parameterName, value);
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setDate(int,
+	 * java.sql.Date)
+	 */
+	public void setDate(int parameterId, Date value) throws OdaException {
+		setObject(parameterId, value);
+	}
+
+	/**
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#setTime(java.lang.String,
+	 * java.sql.Time)
+	 */
+	public void setTime(String parameterName, Time value) throws OdaException {
+		setObject(parameterName, value);
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setTime(int,
+	 * java.sql.Time)
+	 */
+	public void setTime(int parameterId, Time value) throws OdaException {
+		setObject(parameterId, value);
+	}
+
+	/**
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#setTimestamp(java.lang.
+	 * String, java.sql.Timestamp)
+	 */
+	public void setTimestamp(String parameterName, Timestamp value)
+			throws OdaException {
+		setObject(parameterName, value);
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setTimestamp(int,
+	 * java.sql.Timestamp)
+	 */
+	public void setTimestamp(int parameterId, Timestamp value)
+			throws OdaException {
+		setObject(parameterId, value);
+	}
+
+	/**
+	 * 
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#setBoolean(java.lang.String
+	 * , boolean)
+	 */
+	public void setBoolean(String parameterName, boolean value)
+			throws OdaException {
+		setObject(parameterName, value);
+	}
+
+	/**
+	 * 
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setBoolean(int,
+	 * boolean)
+	 */
+	public void setBoolean(int parameterId, boolean value) throws OdaException {
+		setObject(parameterId, value);
+	}
+
+	/**
+	 * 
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#setObject(java.lang.String,
+	 * java.lang.Object)
+	 */
+	public void setObject(String parameterName, Object value) throws OdaException {
+		setObject(getParameterMetaDataLocal().getParameterIndex(parameterName), value);
+	}
+
+	/**
+	 * 
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setObject(int,
+	 * java.lang.Object)
+	 */
+	public void setObject(int parameterId, Object value) throws OdaException {
+		parameters.put(parameterId, value);
+	}
+
+	/**
+	 * 
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#setNull(java.lang.String)
+	 */
+	public void setNull(String parameterName) throws OdaException {
+		setObject(parameterName, null);
+	}
+
+	/**
+	 * 
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#setNull(int)
+	 */
+	public void setNull(int parameterId) throws OdaException {
+		setObject(parameterId, null);
+	}
+
+	/**
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#findInParameter(java.lang
+	 * .String)
+	 */
+	public int findInParameter(String parameterName) throws OdaException {
+		return getParameterMetaDataLocal().getParameterIndex(parameterName);
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#getParameterMetaData()
+	 */
+	public IParameterMetaData getParameterMetaData() throws OdaException {
+		if (pMetadata == null) {
+			pMetadata = new IntelligencePointsParameterMetaData();
+		}
+		return pMetadata;
+	}
+
+	/**
+	 * @return
+	 * @throws OdaException
+	 */
+	private IntelligencePointsParameterMetaData getParameterMetaDataLocal()
+			throws OdaException {
+		return (IntelligencePointsParameterMetaData) getParameterMetaData();
+	}
+
+	/**
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#setSortSpec(org.eclipse
+	 * .datatools.connectivity.oda.SortSpec)
+	 */
+	public void setSortSpec(SortSpec sortBy) throws OdaException {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#getSortSpec()
+	 */
+	public SortSpec getSortSpec() throws OdaException {
+		return null;
+	}
+
+	/**
+	 * 
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#setSpecification(org.eclipse
+	 * .datatools.connectivity.oda.spec.QuerySpecification)
+	 */
+	public void setSpecification(QuerySpecification querySpec)
+			throws OdaException, UnsupportedOperationException {
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#getSpecification()
+	 */
+	public QuerySpecification getSpecification() {
+		// assumes no support
+		return null;
+	}
+
+	/**
+	 * @see
+	 * org.eclipse.datatools.connectivity.oda.IQuery#getEffectiveQueryText()
+	 */
+	public String getEffectiveQueryText() {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IQuery#cancel()
+	 */
+	public void cancel() throws OdaException, UnsupportedOperationException {
+		throw new UnsupportedOperationException();
+	}
+	
+}
---
+++ java/org.wcs.smart.intelligence/src/org/wcs/smart/intelligence/report/oda/IntelligencePointsResultSetMetadata.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.intelligence.report.oda;
+
+import org.eclipse.datatools.connectivity.oda.IResultSetMetaData;
+import org.eclipse.datatools.connectivity.oda.OdaException;
+
+/**
+ * SMART intelligence location pointst result set metadata.
+ * 
+ * @author elitvin
+ * @since 3.0.0
+ */
+public class IntelligencePointsResultSetMetadata implements IResultSetMetaData {
+
+	public static final String ID = "org.wcs.smart.intelligence.report.oda.SmartIntelligencePoints"; //$NON-NLS-1$
+	
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IResultSetMetaData#getColumnCount()
+	 */
+	@Override
+	public int getColumnCount() throws OdaException {
+		return 2;
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IResultSetMetaData#getColumnDisplayLength(int)
+	 */
+	@Override
+	public int getColumnDisplayLength(int arg0) throws OdaException {
+		return -1;
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IResultSetMetaData#getColumnLabel(int)
+	 */
+	@Override
+	public String getColumnLabel(int index) throws OdaException {
+		switch(index){
+		case 1: return "X";
+		case 2: return "Y";
+		}
+		return null;
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IResultSetMetaData#getColumnName(int)
+	 */
+	@Override
+	public String getColumnName(int index) throws OdaException {
+		switch(index){
+		case 1: return "X";
+		case 2: return "Y";
+		}
+		return null;
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IResultSetMetaData#getColumnType(int)
+	 */
+	@Override
+	public int getColumnType(int index) throws OdaException {
+		switch(index){
+		case 1: return java.sql.Types.DOUBLE;
+		case 2: return java.sql.Types.DOUBLE;
+		}
+		return -1;
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IResultSetMetaData#getColumnTypeName(int)
+	 */
+	@Override
+	public String getColumnTypeName(int index) throws OdaException {
+		 int nativeTypeCode = getColumnType( index );
+	     return SmartIntelligenceDriver.getNativeDataTypeName( nativeTypeCode, ID );
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IResultSetMetaData#getPrecision(int)
+	 */
+	@Override
+	public int getPrecision(int arg0) throws OdaException {
+		return -1;
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IResultSetMetaData#getScale(int)
+	 */
+	@Override
+	public int getScale(int arg0) throws OdaException {
+		return -1;
+	}
+
+	/**
+	 * @see org.eclipse.datatools.connectivity.oda.IResultSetMetaData#isNullable(int)
+	 */
+	@Override
+	public int isNullable(int arg0) throws OdaException {
+		return columnNullableUnknown;
+	}
+
+}
