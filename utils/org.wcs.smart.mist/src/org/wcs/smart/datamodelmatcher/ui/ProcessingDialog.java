bd2823e68ba4c3a31e0af5f12f125971535abd5e
Jeff Lounsbury <jeffloun@refractions.net>
2013-12-17 22:34:19 +0000

    DMM - various modifications and adding the license info into each file.
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@2182 37136b1e-4cb1-4992-8394-573b48a9bdce

--- utils/org.wcs.smart.mist/src/org/wcs/smart/datamodelmatcher/ui/ProcessingDialog.java
+++ utils/org.wcs.smart.mist/src/org/wcs/smart/datamodelmatcher/ui/ProcessingDialog.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package org.wcs.smart.datamodelmatcher.ui;
 
 
@@ -32,8 +54,8 @@ public class ProcessingDialog extends Dialog{
     GridLayout layout = new GridLayout(1, false);
 	shell.setLayout(layout);
 
-	Label wait0 = new Label(shell, SWT.NONE);
-    wait0.setText("----------------------------------");
+	//Label wait0 = new Label(shell, SWT.NONE);
+    //wait0.setText("----------------------------------");
 
 	Label wait1 = new Label(shell, SWT.NONE);
     wait1.setText("Please Wait, Loading MIST Data.");
@@ -41,8 +63,8 @@ public class ProcessingDialog extends Dialog{
     ProgressBar pb1 = new ProgressBar(shell, SWT.INDETERMINATE);
     pb1.setSelection(100);
     
-	Label wait2 = new Label(shell, SWT.NONE);
-    wait2.setText("----------------------------------");
+	//Label wait2 = new Label(shell, SWT.NONE);
+    //wait2.setText("----------------------------------");
 
     
 
