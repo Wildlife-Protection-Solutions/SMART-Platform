5c00e5c65e60d5029730786368a00375797a709b
Evgeniy Litvin <elitvin@refractions.net>
2014-02-07 00:36:42 +0000

    Common plug-in install/uninstall actions that allow to distinguish upgrade/fresh install/removal
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@2428 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ java/org.wcs.smart/src/org/wcs/smart/common/updatesite/InstallProvisioningAction.java
@@ -0,0 +1,51 @@
+package org.wcs.smart.common.updatesite;
+
+import java.util.Map;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.equinox.internal.p2.engine.InstallableUnitOperand;
+import org.eclipse.equinox.p2.engine.spi.ProvisioningAction;
+import org.eclipse.equinox.p2.metadata.IInstallableUnit;
+
+/**
+ * Action that is called when some plug-in is being installed.
+ * Class contains logic that allows to distinguish if this is plug-in fresh install or upgrade.
+ * 
+ * @author elitvin
+ * @since 3.0.0
+ */
+@SuppressWarnings("restriction")
+public abstract class InstallProvisioningAction extends ProvisioningAction {
+
+	@Override
+	public IStatus execute(Map<String, Object> parameters) {
+		IInstallableUnit iu = (IInstallableUnit) parameters.get("iu"); //$NON-NLS-1$
+		IInstallableUnit upgradeFrom = null;
+		Object operand = parameters.get("operand"); //$NON-NLS-1$
+		try {
+			if (operand instanceof InstallableUnitOperand)
+				upgradeFrom = ((InstallableUnitOperand) operand).first();
+		}
+		catch (Throwable e) {
+			// Ignore class not found in case InstallableUnitOperand is missing
+		}
+		if (upgradeFrom != null) {
+			performUpgrade(iu, upgradeFrom);
+		} else {
+			performNewInstall(iu);
+		}
+		return Status.OK_STATUS;
+	}
+
+	protected abstract void performUpgrade(IInstallableUnit iu, IInstallableUnit oldIu);
+	
+	protected abstract void performNewInstall(IInstallableUnit iu);
+
+	@Override
+	public IStatus undo(Map<String, Object> parameters) {
+		return Status.OK_STATUS;
+	}
+
+
+}
