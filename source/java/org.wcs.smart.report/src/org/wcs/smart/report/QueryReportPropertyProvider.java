ed08375529ee2628c977a4d763aeab2bfe7133a0
Emily Gouge <egouge@refractions.net>
2014-02-14 19:41:14 +0000

    import clean and findbug fixes
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@2487 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart.report/src/org/wcs/smart/report/QueryReportPropertyProvider.java
+++ source/java/org.wcs.smart.report/src/org/wcs/smart/report/QueryReportPropertyProvider.java
@@ -29,7 +29,6 @@ import org.wcs.smart.hibernate.HibernateManager;
 import org.wcs.smart.query.model.IQueryType;
 import org.wcs.smart.query.model.Query;
 import org.wcs.smart.query.ui.AbstractQueryPropertyProvider;
-
 import org.wcs.smart.report.internal.Messages;
 import org.wcs.smart.report.model.ReportQuery;
 
