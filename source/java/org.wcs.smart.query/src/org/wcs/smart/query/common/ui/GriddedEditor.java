3676b5ad4ba08620a0167d0c0d8bb67e1c5b975f
Emily Gouge <egouge@refractions.net>
2014-02-21 18:40:39 +0000

    fix #902
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@2520 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart.query/src/org/wcs/smart/query/common/ui/GriddedEditor.java
+++ source/java/org.wcs.smart.query/src/org/wcs/smart/query/common/ui/GriddedEditor.java
@@ -49,6 +49,7 @@ import org.wcs.smart.ca.IAreaModifiedListener;
 import org.wcs.smart.hibernate.HibernateManager;
 import org.wcs.smart.query.QueryHibernateManager;
 import org.wcs.smart.query.QueryPlugIn;
+import org.wcs.smart.query.common.model.Grid;
 import org.wcs.smart.query.common.model.GriddedQuery;
 import org.wcs.smart.query.event.IQueryListener;
 import org.wcs.smart.query.event.QueryAreaModifiedListener;
@@ -172,8 +173,8 @@ public abstract class GriddedEditor extends MultiPageEditorPart implements MapPa
 					resultPage.updateAndShowTable(results, mymonitor);
 				} catch (Exception ex) {
 					String message = "Could not execute query." + "\n\n"; //$NON-NLS-1$ //$NON-NLS-2$
-					if (ex.getCause() != null){
-						message += ex.getCause().getLocalizedMessage();
+					if (ex.getCause() != null && ex.getCause().getCause()== Grid.GRID_TO_BIG_EXCEPTION){
+						message += ex.getCause().getCause().getLocalizedMessage();
 					}else{
 						message += ex.getLocalizedMessage();
 					}
