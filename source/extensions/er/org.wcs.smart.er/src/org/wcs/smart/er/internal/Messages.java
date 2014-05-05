98232b1e957a2f1a6490796a831ba38b6b9732a6
Evgeniy Litvin <elitvin@refractions.net>
2014-05-05 20:47:21 +0000

    DB upgrade support
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@2887 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/extensions/er/org.wcs.smart.er/src/org/wcs/smart/er/internal/Messages.java
+++ source/extensions/er/org.wcs.smart.er/src/org/wcs/smart/er/internal/Messages.java
@@ -5,6 +5,7 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.wcs.smart.er.internal.messages"; //$NON-NLS-1$
 	public static String AddERJob_Title;
+	public static String ERDatabaseUpgrader_UpgradeTask;
 	public static String RemoveERJob_Title;
 	static {
 		// initialize resource bundle
