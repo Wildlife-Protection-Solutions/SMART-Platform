788f882e1600c19b29fcf3a5afbe397b67f054e3
Evgeniy Litvin <elitvin@refractions.net>
2014-05-05 20:41:42 +0000

    P2 stubs
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@2886 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ source/extensions/er/org.wcs.smart.er/src/org/wcs/smart/er/internal/Messages.java
@@ -0,0 +1,16 @@
+package org.wcs.smart.er.internal;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.wcs.smart.er.internal.messages"; //$NON-NLS-1$
+	public static String AddERJob_Title;
+	public static String RemoveERJob_Title;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
