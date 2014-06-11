0be10db8228a3e27112c414b375bf1209b99c1d7
Evgeniy Litvin <elitvin@refractions.net>
2014-06-11 19:51:42 +0000

    mapping file merger
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@2940 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ utils/org.wcs.smart.ct2smart/src/org/wcs/smart/ct2smart/run/MappingsMerger.java
@@ -0,0 +1,58 @@
+package org.wcs.smart.ct2smart.run;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.JAXBException;
+
+import org.wcs.smart.ct2smart.matcher.FileUtil;
+import org.wcs.smart.ct2smart.matcher.model.Ct2Attribute;
+import org.wcs.smart.ct2smart.matcher.model.Ct2AttributeValue;
+import org.wcs.smart.ct2smart.matcher.model.Ct2Smart;
+import org.wcs.smart.ct2smart.patrol.Ct2SmartLookup;
+
+public class MappingsMerger {
+
+	public static void main(String[] args) throws JAXBException, IOException {
+		Ct2Smart from = FileUtil.loadCt2Smart(new File("match_super_x.xml"));
+		Ct2Smart to = FileUtil.loadCt2Smart(new File("match_short.xml"));
+		MappingsMerger merger = new MappingsMerger();
+		merger.merge(from, to);
+		FileUtil.write(new File("match_short_merged.xml"), to);
+	}
+	
+	public void merge(Ct2Smart from, Ct2Smart to) {
+		Ct2SmartLookup lookup = new Ct2SmartLookup(from);
+		for (Ct2Attribute aTo : to.getCt2Attribute()) {
+			Ct2Attribute aFrom = lookup.findAttribute(aTo.getI());
+			if (aFrom != null) {
+				merge(aFrom, aTo);
+			}
+		}
+	}
+	
+	public void merge(Ct2Attribute from, Ct2Attribute to) {
+		//NOTE: N and I must be the same by design!!!
+		to.setCategoryKey(from.getCategoryKey());
+		to.setMapTo(from.getMapTo());
+		to.setType(from.getType());
+		
+		to.getExtraAttribute().clear();
+		to.getExtraAttribute().addAll(from.getExtraAttribute());
+		
+		Map<String, Ct2AttributeValue> vLookup = new HashMap<String, Ct2AttributeValue>();
+		for (Ct2AttributeValue vFrom : from.getCt2AttributeValue()) {
+			vLookup.put(vFrom.getI(), vFrom);
+		}
+
+		for (Ct2AttributeValue vTo : to.getCt2AttributeValue()) {
+			Ct2AttributeValue vFrom = vLookup.get(vTo.getI());
+			if (vFrom != null) {
+				//NOTE: N and I must be the same by design!!!
+				vTo.setMapTo(vFrom.getMapTo());
+			}
+		}
+	}
+}
