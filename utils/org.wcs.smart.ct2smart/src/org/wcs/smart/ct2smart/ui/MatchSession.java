38c122b9288f77d367e5124ed5dfbed4b3d51595
Evgeniy Litvin <elitvin@refractions.net>
2014-06-20 18:54:46 +0000

    Current stubs
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@2958 37136b1e-4cb1-4992-8394-573b48a9bdce

--- utils/org.wcs.smart.ct2smart/src/org/wcs/smart/ct2smart/ui/MatchSession.java
+++ utils/org.wcs.smart.ct2smart/src/org/wcs/smart/ct2smart/ui/MatchSession.java
@@ -1,14 +1,18 @@
 package org.wcs.smart.ct2smart.ui;
 
 import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.wcs.smart.ca.datamodel.DataModel;
 import org.wcs.smart.ct2smart.matcher.model.Ct2Smart;
+import org.wcs.smart.internal.ca.datamodel.xml.generate.AttributeType;
+import org.wcs.smart.internal.ca.datamodel.xml.generate.DataModel;
 
 public class MatchSession {
 	
 	private Ct2Smart ct2Smart;
 	private Connection connection;
+	private Map<String, AttributeType> key2Attribute;
 	private DataModel dataModel;
 	
 	public Ct2Smart getCt2Smart() {
@@ -28,6 +32,19 @@ public class MatchSession {
 	}
 	public void setDataModel(DataModel dataModel) {
 		this.dataModel = dataModel;
+		key2Attribute = createKey2AttributeMap(dataModel);
+	}
+	
+	public Map<String, AttributeType> getKey2Attribute() {
+		return key2Attribute;
+	}
+	
+	private Map<String, AttributeType> createKey2AttributeMap(DataModel dm) {
+		Map<String, AttributeType> map = new HashMap<String, AttributeType>();
+		for (AttributeType a : dm.getAttributes().getAttributes()) {
+			map.put(a.getKey(), a);
+		}
+		return map;
 	}
 
 	
