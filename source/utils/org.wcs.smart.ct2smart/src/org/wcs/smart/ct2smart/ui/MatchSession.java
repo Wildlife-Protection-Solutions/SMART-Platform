c088310153dedf408bbb6b18623b03a11ea06627
Evgeniy Litvin <elitvin@refractions.net>
2014-06-23 22:19:26 +0000

    GUI
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@2961 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/utils/org.wcs.smart.ct2smart/src/org/wcs/smart/ct2smart/ui/MatchSession.java
+++ source/utils/org.wcs.smart.ct2smart/src/org/wcs/smart/ct2smart/ui/MatchSession.java
@@ -2,10 +2,12 @@ package org.wcs.smart.ct2smart.ui;
 
 import java.sql.Connection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.wcs.smart.ct2smart.matcher.model.Ct2Smart;
 import org.wcs.smart.internal.ca.datamodel.xml.generate.AttributeType;
+import org.wcs.smart.internal.ca.datamodel.xml.generate.CategoryType;
 import org.wcs.smart.internal.ca.datamodel.xml.generate.DataModel;
 
 public class MatchSession {
@@ -13,6 +15,7 @@ public class MatchSession {
 	private Ct2Smart ct2Smart;
 	private Connection connection;
 	private Map<String, AttributeType> key2Attribute;
+	private Map<String, CategoryType> key2Category;
 	private DataModel dataModel;
 	
 	public Ct2Smart getCt2Smart() {
@@ -33,10 +36,15 @@ public class MatchSession {
 	public void setDataModel(DataModel dataModel) {
 		this.dataModel = dataModel;
 		key2Attribute = createKey2AttributeMap(dataModel);
+		key2Category = createKey2CategoryMap(dataModel);
 	}
 	
 	public Map<String, AttributeType> getKey2Attribute() {
 		return key2Attribute;
+	}
+	
+	public Map<String, CategoryType> getKey2Category() {
+		return key2Category;
 	}
 	
 	private Map<String, AttributeType> createKey2AttributeMap(DataModel dm) {
@@ -47,5 +55,18 @@ public class MatchSession {
 		return map;
 	}
 
+	private Map<String, CategoryType> createKey2CategoryMap(DataModel dm) {
+		Map<String, CategoryType> map = new HashMap<String, CategoryType>();
+		addChildCategories(map, dm.getCategories().getCategories(), ""); //$NON-NLS-1$
+		return map;
+	}
+
+	private void addChildCategories(Map<String, CategoryType> map, List<CategoryType> cList, String parentKey) {
+		for (CategoryType c : cList) {
+			String key = parentKey + c.getKey() + "."; //$NON-NLS-1$
+			map.put(key, c);
+			addChildCategories(map, c.getCategories(), key);
+		}
+	}
 	
 }
---
+++ source/utils/org.wcs.smart.ct2smart/src/org/wcs/smart/ct2smart/ui/support/SmartCategoryTableEditor.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.ct2smart.ui.support;
+
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ColumnViewer;
+import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TextCellEditor;
+import org.wcs.smart.ct2smart.matcher.model.Ct2Attribute;
+import org.wcs.smart.ct2smart.util.Ct2AttributeTypeUtil;
+
+/**
+ * @author elitvin
+ * @since 3.0.0
+ */
+public class SmartCategoryTableEditor extends EditingSupport {
+	
+	private CellEditor editor;
+
+	public SmartCategoryTableEditor(TableViewer viewer) {
+		super(viewer);
+		editor = new DmTreeCellEditor(viewer.getTable());
+	}
+
+	@Override
+	protected boolean canEdit(Object arg0) {
+		if (arg0 instanceof Ct2Attribute) {
+			Ct2Attribute a = (Ct2Attribute) arg0;
+			return Ct2AttributeTypeUtil.canMap(a.getType());
+		}
+		return false;
+	}
+
+	@Override
+	protected CellEditor getCellEditor(Object arg0) {
+		return editor;
+	}
+
+	@Override
+	protected Object getValue(Object arg0) {
+		// TODO Auto-generated method stub
+		return "asd";
+	}
+
+	@Override
+	protected void setValue(Object arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
