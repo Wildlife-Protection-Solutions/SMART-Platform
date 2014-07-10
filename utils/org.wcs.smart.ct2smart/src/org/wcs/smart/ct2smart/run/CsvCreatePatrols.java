3c9da4e7b6a6b62ea1cf54089836a7f9954ac222
Evgeniy Litvin <elitvin@refractions.net>
2014-07-10 23:00:53 +0000

    CsvPatrolExtractor
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@3032 37136b1e-4cb1-4992-8394-573b48a9bdce

--- utils/org.wcs.smart.ct2smart/src/org/wcs/smart/ct2smart/run/CsvCreatePatrols.java
+++ utils/org.wcs.smart.ct2smart/src/org/wcs/smart/ct2smart/run/CsvCreatePatrols.java
@@ -8,26 +8,27 @@ import java.util.List;
 import org.wcs.smart.ct2smart.dao.ConnectionUtil;
 import org.wcs.smart.ct2smart.matcher.FileUtil;
 import org.wcs.smart.ct2smart.matcher.model.Ct2Smart;
+import org.wcs.smart.ct2smart.patrol.CsvPatrolExtractor;
 import org.wcs.smart.ct2smart.patrol.PatrolBuilder;
-import org.wcs.smart.ct2smart.patrol.PatrolExtractor;
 import org.wcs.smart.ct2smart.xml.parser.TagS;
 import org.wcs.smart.ct2smart.xml.parser.TagT;
 import org.wcs.smart.patrol.xml.model.PatrolType;
 
-@Deprecated
-public class CreatePatrols {
+public class CsvCreatePatrols {
 
 	public static void main(String[] args) throws Exception {
 		long start = System.currentTimeMillis();
 		
-		PatrolExtractor extractor = new PatrolExtractor();
+		Connection c = ConnectionUtil.getConnection();
+
+		CsvPatrolExtractor extractor = new CsvPatrolExtractor(c);
 		Ct2Smart ct2Smart = FileUtil.loadCt2Smart(new File("match_super_x.xml"));
 		PatrolBuilder builder = new PatrolBuilder(ct2Smart);
 		
-		Connection c = ConnectionUtil.getConnection();
 		String[] uniqueId = new String[] {"Date", "Unit_ID", "DeviceId"};
+		String[] columnNames = extractor.getCsvColumns(uniqueId);
 		String[] uniqueValues = new String[uniqueId.length];
-		ResultSet rs = extractor.getUniqueGroups(c, uniqueId);
+		ResultSet rs = extractor.getUniqueGroups(columnNames);
 		while (rs.next()) {
 			StringBuilder sb = new StringBuilder();
 			for (int i = 0; i < uniqueValues.length; i++) {
@@ -35,8 +36,8 @@ public class CreatePatrols {
 				sb.append(uniqueValues[i]).append("  ");
 			}
 			System.out.println("Extracting patrol for: " + sb);
-			List<TagS> sList = extractor.extractS(c, uniqueId, uniqueValues);
-			List<TagT> tList = extractor.extractT(c, uniqueValues[2], uniqueValues[0]);
+			List<TagS> sList = extractor.extractS(columnNames, uniqueValues);
+			List<TagT> tList = extractor.extractT(uniqueValues[2], uniqueValues[0]);
 			PatrolType p = builder.createPatrol(sList, tList);
 			p.setId(uniqueValues[1] + "-patrol-" + uniqueValues[0].replace('/', '-'));
 			
@@ -46,5 +47,5 @@ public class CreatePatrols {
 		
 		System.out.println("Done in "+ (double)(System.currentTimeMillis()-start)/1000 +" seconds!!!");
 	}
-
+	
 }
--- utils/org.wcs.smart.ct2smart/src/org/wcs/smart/ct2smart/xml/parser/TagE.java
+++ utils/org.wcs.smart.ct2smart/src/org/wcs/smart/ct2smart/xml/parser/TagE.java
@@ -1,9 +1,25 @@
 package org.wcs.smart.ct2smart.xml.parser;
 
 
-public interface TagE {
+public class TagE {
 
 	public static final String I = "I"; //$NON-NLS-1$
 	public static final String N = "N"; //$NON-NLS-1$
+	
+	private String i;
+	private String n;
+	
+	public String getI() {
+		return i;
+	}
+	public void setI(String i) {
+		this.i = i;
+	}
+	public String getN() {
+		return n;
+	}
+	public void setN(String n) {
+		this.n = n;
+	}
 	
 }
