a9b09f9253cd9a6aea709d593130603245fade9d
Emily Gouge <egouge@refractions.net>
2014-08-19 20:36:13 +0000

    design changes for sampling unit attributes
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@3128 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ extensions/er/org.wcs.smart.er/src/org/wcs/smart/er/model/SamplingUnitAttributeValue.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.er.model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+
+/**
+ * Link between a sampling unit attribute, sampling unit and
+ * associated attribute value.
+ * 
+ * @author Emily
+ *
+ */
+@Entity
+@Table(name="smart.sampling_unit_attribute_value")
+public class SamplingUnitAttributeValue {
+
+	private SamplingUnitAttributeValuePk id = new SamplingUnitAttributeValuePk();
+	
+	private String stringValue;
+	
+	private double doubleValue;
+
+	public SamplingUnitAttributeValue(){
+		
+	}
+	
+	@EmbeddedId
+	public SamplingUnitAttributeValuePk getId() {
+		return this.id;
+	}
+
+	public void setId(SamplingUnitAttributeValuePk id) {
+		this.id = id;
+	}
+	
+	/**
+	 * 
+	 * @return the associate sampling unit
+	 */
+	@Transient
+	public SamplingUnit getSamplingUnit(){
+		return id.getSamplingUnit();
+	}
+	
+	public void setSamplingUnit(SamplingUnit unit){
+		id.setSamplingUnit(unit);
+	}
+	
+	/**
+	 * 
+	 * @return the associate sampling unit
+	 * 
+	 */
+	@Transient
+	public SamplingUnitAttribute getSamplingUnitAttribute(){
+		return id.getSamplingUnitAttribute();
+	}
+	
+	public void setSamplingUnitAttribute(SamplingUnitAttribute attribute){
+		id.setSamplingUnitAttribute(attribute);
+	}
+	
+	@Column(name = "string_value")
+	public String getStringValue(){
+		return this.stringValue;
+	}
+	
+	public void setStringValue(String value){
+		this.stringValue = value;
+	}
+	
+	@Column(name = "double_value")
+	public Double getDoubleValue(){
+		return this.doubleValue;
+	}
+	
+	public void setDoubleValue(Double value){
+		this.doubleValue = value;
+	}
+	
+	@Embeddable
+	private static class SamplingUnitAttributeValuePk implements Serializable {
+		private static final long serialVersionUID = 1L;
+
+		private SamplingUnit samplingUnit;	
+		
+		private SamplingUnitAttribute attribute;
+
+		public SamplingUnitAttributeValuePk() {
+		}
+
+		/**
+		 * 
+		 * @return the associate sampling unit
+		 */
+		@ManyToOne(fetch = FetchType.LAZY, cascade = { CascadeType.ALL })
+		@JoinColumn(name = "su_uuid", referencedColumnName = "uuid")
+		public SamplingUnit getSamplingUnit(){
+			return this.samplingUnit;
+		}
+		
+		public void setSamplingUnit(SamplingUnit unit){
+			this.samplingUnit = unit;
+		}
+		
+		/**
+		 * 
+		 * @return the associate sampling unit
+		 */
+		@ManyToOne(fetch = FetchType.LAZY, cascade = { CascadeType.ALL })
+		@JoinColumn(name = "su_attribute_uuid", referencedColumnName = "uuid")
+		public SamplingUnitAttribute getSamplingUnitAttribute(){
+			return this.attribute;
+		}
+		
+		public void setSamplingUnitAttribute(SamplingUnitAttribute attribute){
+			this.attribute = attribute;
+		}
+
+		@Override
+		public boolean equals(Object key) {
+			if (!(key instanceof SamplingUnitAttributeValuePk)) {
+				return false;
+			}
+			SamplingUnitAttributeValuePk p = (SamplingUnitAttributeValuePk) key;
+
+			if (p.samplingUnit == null || this.samplingUnit == null
+					|| p.attribute == null || this.attribute == null) {
+
+				if (p.samplingUnit == null && this.samplingUnit == null
+						&& p.attribute == null && this.attribute == null) {
+					return true;
+				}
+				return false;
+			}
+
+			return p.samplingUnit.equals(this.samplingUnit)
+					&& p.attribute.equals(this.attribute);
+		}
+
+		@Override
+		public int hashCode() {
+			int code = 0;
+			if (samplingUnit != null) {
+				code += samplingUnit.hashCode();
+			}
+			code *= 31;
+			if (attribute != null) {
+				code += attribute.hashCode();
+			}
+			return code;
+		}
+	}
+}
+
---
+++ extensions/er/org.wcs.smart.er/src/org/wcs/smart/er/model/SurveyDesignSamplingUnitAttribute.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.er.model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/**
+ * Link between a survey design and associated sampling unit attributes.
+ * @author Emily
+ *
+ */
+@Entity
+@Table(name="smart.survey_design_sampling_unit")
+public class SurveyDesignSamplingUnitAttribute {
+	
+	private SurveyDesignSamplingUnitAttributePk id = 
+			new SurveyDesignSamplingUnitAttributePk();
+
+
+	public SurveyDesignSamplingUnitAttribute() {
+
+	}
+
+	@EmbeddedId
+	public SurveyDesignSamplingUnitAttributePk getId() {
+		return this.id;
+	}
+
+	public void setId(SurveyDesignSamplingUnitAttributePk id) {
+		this.id = id;
+	}
+
+	@Transient
+	public SurveyDesign getSurveyDesign() {
+		return id.getSurveyDesign();
+	}
+
+	public void setSurveyDesign(SurveyDesign surveyDesign) {
+		id.setSurveyDesign(surveyDesign);
+	}
+
+	@Transient
+	public SamplingUnitAttribute getSamplingUnitAttribute() {
+		return id.getSamplingUnitAttribute();
+	}
+
+	public void setSamplingUnitAttribute(SamplingUnitAttribute attribute) {
+		id.setSamplingUnitAttribute(attribute);
+	}
+
+	/**
+	 * @param o
+	 * @return
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof MissionPropertyValue) {
+			return this.id.equals(((SurveyDesignSamplingUnitAttribute) o).id);
+		}
+		return false;
+	}
+
+	/**
+	 * @return
+	 */
+	@Override
+	public int hashCode() {
+		return id.hashCode();
+	}
+
+	@Embeddable
+	private static class SurveyDesignSamplingUnitAttributePk implements Serializable {
+		/**
+	 * 
+	 */
+		private static final long serialVersionUID = 1L;
+
+
+		private SamplingUnitAttribute attribute;
+		private SurveyDesign design;
+
+		public SurveyDesignSamplingUnitAttributePk() {
+		}
+
+		@ManyToOne(fetch = FetchType.LAZY, cascade = { CascadeType.ALL })
+		@JoinColumn(name = "survey_design_uuid", referencedColumnName = "uuid")
+		public SurveyDesign getSurveyDesign() {
+			return design;
+		}
+
+		public void setSurveyDesign(SurveyDesign design) {
+			this.design = design;
+		}
+
+		@ManyToOne(fetch = FetchType.LAZY)
+		@JoinColumn(name = "su_attribute_uuid", referencedColumnName = "uuid")
+		public SamplingUnitAttribute getSamplingUnitAttribute() {
+			return attribute;
+		}
+
+		public void setSamplingUnitAttribute(SamplingUnitAttribute attribute) {
+			this.attribute = attribute;
+		}
+
+		@Override
+		public boolean equals(Object key) {
+			if (!(key instanceof SurveyDesignSamplingUnitAttributePk)) {
+				return false;
+			}
+			SurveyDesignSamplingUnitAttributePk p = (SurveyDesignSamplingUnitAttributePk) key;
+
+			if (p.design == null || this.design == null
+					|| p.attribute == null || this.attribute == null) {
+
+				if (p.design == null && this.design == null
+						&& p.attribute == null && this.attribute == null) {
+					return true;
+				}
+				return false;
+			}
+
+			return p.design.equals(this.design)
+					&& p.attribute.equals(this.attribute);
+		}
+
+		@Override
+		public int hashCode() {
+			int code = 0;
+			if (design != null) {
+				code += design.hashCode();
+			}
+			code *= 31;
+			if (attribute != null) {
+				code += attribute.hashCode();
+			}
+			return code;
+		}
+	}
+}
