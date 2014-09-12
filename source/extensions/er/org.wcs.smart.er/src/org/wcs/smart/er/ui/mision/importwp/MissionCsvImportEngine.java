6ef0b16f783ba8448e0491fd97b32c336ff4c815
Evgeniy Litvin <elitvin@refractions.net>
2014-09-12 21:49:34 +0000

    Import waypoint/track stubs for mission
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@3225 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ source/extensions/er/org.wcs.smart.er/src/org/wcs/smart/er/ui/mision/importwp/MissionCsvImportEngine.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.er.ui.mision.importwp;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.wcs.smart.observation.common.importwp.GPSDataImport.ImportType;
+import org.wcs.smart.observation.common.importwp.ImportOptionsComposite.ImportOption;
+import org.wcs.smart.observation.common.importwp.csv.CsvImportEngine;
+import org.wcs.smart.observation.model.Waypoint;
+
+/**
+ * CsvImportEngine for mission
+ * 
+ * @author elitvin
+ * @since 3.0.0
+ */
+public class MissionCsvImportEngine extends CsvImportEngine {
+
+	/**
+	 * 
+	 */
+	public MissionCsvImportEngine() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.wcs.smart.observation.common.importwp.IImportEngine#updatePatrol(org.wcs.smart.observation.common.importwp.ImportOptionsComposite.ImportOption, org.wcs.smart.observation.common.importwp.GPSDataImport.ImportType, java.lang.Object, java.util.List, org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	@Override
+	public String updateSourceObject(ImportOption option, ImportType type,
+			Object object, List<Waypoint> data, IProgressMonitor monitor)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
