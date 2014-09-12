1c20c22fb36b3715f088cc6d677bb8e85f846048
Emily Gouge <egouge@refractions.net>
2014-09-12 19:03:41 +0000

    modifications to support table categories for SMART BIRT Report Tables
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@3221 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/extensions/org.wcs.smart.entity/src/org/wcs/smart/entity/report/EntityTable.java
+++ source/extensions/org.wcs.smart.entity/src/org/wcs/smart/entity/report/EntityTable.java
@@ -49,7 +49,8 @@ public class EntityTable extends SmartBirtTable {
 	private EntityType et;
 	
 	public EntityTable(EntityType et) {
-		super(MessageFormat.format(Messages.EntityTable_EntityTypeTableName, new Object[]{et.getName()}),
+		super(MessageFormat.format(Messages.EntityTable_LongName, new Object[]{et.getName()}),
+				et.getName(),
 				ENTITYKEY_PREFIX + ":" + et.getType().name() + ":" + et.getKeyId()); //$NON-NLS-1$ //$NON-NLS-2$
 		this.et = et;
 	}
