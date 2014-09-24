19d8c90389a0ff1b44218a4b373125f708b8141a
Evgeniy Litvin <elitvin@refractions.net>
2014-09-24 22:53:41 +0000

    Mission Track Point dialog
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@3281 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ extensions/er/org.wcs.smart.er/src/org/wcs/smart/er/ui/mision/editor/MissionTrackPointDialog.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.er.ui.mision.editor;
+
+import java.util.TimeZone;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.widgets.Shell;
+import org.wcs.smart.er.model.MissionTrack;
+import org.wcs.smart.ui.map.TrackPointDialog;
+
+import com.vividsolutions.jts.geom.LineString;
+
+/**
+ * Dialog for display tack points for a mission track.
+ * <p>The track points are displayed in a simple table.</p>
+ * 
+ * @author elitvin
+ * @since 3.0.0
+ */
+public class MissionTrackPointDialog extends TrackPointDialog {
+	
+	private MissionTrack track;
+	private MissionTrack editTrack;
+
+	/**
+	 * @param parentShell parent shell
+	 * @param t the track to display
+	 */
+	public MissionTrackPointDialog(Shell parentShell, MissionTrack t) {
+		super(parentShell);
+		this.track = t;
+		
+		editTrack = new MissionTrack();
+		editTrack.setLineString(t.getLineString());
+		editTrack.setUuid(track.getUuid());
+	}
+
+	@Override
+	protected TimeZone getTrackZTimezone() {
+		return MissionTrack.ZTIMEZONE;
+	}
+
+	@Override
+	protected byte[] getEditTrackUUid() {
+		return editTrack.getUuid();
+	}
+
+	@Override
+	protected LineString getEditTrackLineString() {
+		return editTrack.getLineString();
+	}
+
+	@Override
+	protected void setEditTrackLineString(LineString ls) {
+		editTrack.setLineString(ls);
+	}
+
+	@Override
+	protected void okPressed() {
+//		Patrol p = track.getPatrolLegDay().getPatrolLeg().getPatrol();
+//		PatrolLegDay pld = track.getPatrolLegDay();
+//		
+//		//save then close
+//		if (editTrack.getLineString() == null){
+//			//delete track
+//			track.getPatrolLegDay().setTrack(null);
+//			track.setPatrolLegDay(null);
+//		}else{
+//			track.setLineString(editTrack.getLineString());
+//		}
+//		
+//		//save and fire
+//		SavePatrolPartJob saveJob = new SavePatrolPartJob(p,pld); 		
+//		saveJob.schedule();
+//		try{
+//			saveJob.join();
+//			PatrolEventManager.getInstance().patrolChanged(PatrolEventManager.PATROL_TRACKS, pld);
+//			getButton(IDialogConstants.OK_ID).setEnabled(false);
+//			setModified(false);
+//		}catch (InterruptedException ex){
+//			throw new IllegalStateException("Save Job Interrupted", ex); //$NON-NLS-1$
+//		}
+	}
+	
+}
