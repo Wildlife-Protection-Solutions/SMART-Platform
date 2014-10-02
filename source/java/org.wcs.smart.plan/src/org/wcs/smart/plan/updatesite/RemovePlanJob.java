b0a383d18bc078249247322fb844a94a18ec13f3
Emily Gouge <egouge@refractions.net>
2014-10-02 22:02:05 +0000

    update removal job to delete filestore
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@3308 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart.plan/src/org/wcs/smart/plan/updatesite/RemovePlanJob.java
+++ source/java/org.wcs.smart.plan/src/org/wcs/smart/plan/updatesite/RemovePlanJob.java
@@ -21,11 +21,17 @@
  */
 package org.wcs.smart.plan.updatesite;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.hibernate.Session;
+import org.wcs.smart.ca.ConservationArea;
 import org.wcs.smart.hibernate.DerbyHibernateExtensions;
 import org.wcs.smart.hibernate.HibernateManager;
 import org.wcs.smart.plan.SmartPlanPlugIn;
@@ -51,9 +57,12 @@ public class RemovePlanJob extends Job {
 
 	@Override
 	protected IStatus run(IProgressMonitor monitor) {
+		List<ConservationArea> cas = null;
+		
 		final Session session = HibernateManager.openSession();
 		session.beginTransaction();
 		try {
+			cas = HibernateManager.getConservationAreas(session);
 			if (DerbyHibernateExtensions.tableExists(session, "plan")){ //$NON-NLS-1$
 				session.createSQLQuery("delete FROM smart.I18N_LABEL where ELEMENT_UUID in (select uuid from smart.plan)").executeUpdate(); //$NON-NLS-1$
 			}
@@ -78,6 +87,17 @@ public class RemovePlanJob extends Job {
 				session.close();
 			} catch (Exception ex) {
 				SmartPlanPlugIn.log(ex.getMessage(), ex);
+			}
+		}
+		if (cas != null){
+			//delete filestores
+			for (ConservationArea ca : cas){
+				try {
+					File folder = new File(ca.getFileDataStoreLocation() + File.separator + SmartPlanPlugIn.PLAN_DIR);
+					FileUtils.deleteDirectory(folder);
+				} catch (IOException ex) {
+					SmartPlanPlugIn.log(ex.getMessage(), ex);
+				}
 			}
 		}
 		
