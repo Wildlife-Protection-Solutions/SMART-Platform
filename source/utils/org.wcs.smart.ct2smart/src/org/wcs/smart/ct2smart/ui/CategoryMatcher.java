cd7cf91afe590aa0da5c8f92740717143f8319b5
Evgeniy Litvin <elitvin@refractions.net>
2014-11-11 00:46:31 +0000

    Fix "Extra attributes are not persisted for category mapping after adding new attribute to category set"
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@3538 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/utils/org.wcs.smart.ct2smart/src/org/wcs/smart/ct2smart/ui/CategoryMatcher.java
+++ source/utils/org.wcs.smart.ct2smart/src/org/wcs/smart/ct2smart/ui/CategoryMatcher.java
@@ -51,6 +51,8 @@ public class CategoryMatcher {
 				}
 				if (found) {
 					nc.setCategoryKey(oc.getCategoryKey());
+					nc.getExtraAttribute().clear();
+					nc.getExtraAttribute().addAll(oc.getExtraAttribute());
 					break;
 				}
 			}
