4cd9056a1baaa4b2862996e89388fd6978088354
Emily Gouge <egouge@refractions.net>
2015-03-25 22:26:40 +0000

    minor updates to tools
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@3855 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart.util/src/main/java/org/wcs/smart/PackageProduct.java
+++ source/java/org.wcs.smart.util/src/main/java/org/wcs/smart/PackageProduct.java
@@ -7,7 +7,7 @@ import org.apache.commons.io.FileUtils;
 public class PackageProduct {
 
 	public static final String VERSION = "3.2.0";
-	public static final String RC = "a3";
+	public static final String RC = "a6";
 	
 	public static final String GPS_BABEL = "C:\\data\\SMART\\Exports\\dependencies\\GPSBabel";
 	
@@ -26,7 +26,7 @@ public class PackageProduct {
 	public static final String UPDATESITE_DIRNAME = "updatesite";
 	
 //	public static final String UPDATE_SITE_DIR = "C:\\data\\SMART\\Source\\Version1\\trunk\\source\\java\\org.wcs.smart.updatesite";
-	public static final String UPDATE_SITE_DIR = "C:\\data\\SMART\\Source\\Eclipse4\\source\\java\\org.wcs.smart.updatesite";
+	public static final String UPDATE_SITE_DIR = "C:\\data\\SMART\\Source\\trunk\\source\\java\\org.wcs.smart.updatesite";
 	
 	public static final String[] UPDATE_SITE_FILES = new String[]{"features", "plugins", "artifacts.jar", "content.jar"};
 	
@@ -187,9 +187,9 @@ public class PackageProduct {
 	
 	public static void main(String[] args) throws Exception{
 
-//		packageUpdateSize();
+		//packageUpdateSize();
 		
-//		processWindows();
+//		processWindows();		
 		zipWindows();
 		copyToNetworkWindows();
 		
