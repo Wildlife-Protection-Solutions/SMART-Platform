8e677d7d142e4a0d96849cb01be3808a689f4532
Evgeniy Litvin <elitvin@refractions.net>
2015-04-15 11:46:22 +0000

    Extract common logic to abstract class
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@3893 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ utils/conversion/org.wcs.smart.conversion/src/org/wcs/smart/conversion/tool/AbstractBuilder.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.conversion.tool;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.wcs.smart.conversion.lookup.Ct2SmartLookup;
+import org.wcs.smart.conversion.lookup.DataModelLookup;
+import org.wcs.smart.conversion.lookup.Ct2SmartLookup.Ct2AttributeValuePair;
+import org.wcs.smart.conversion.model.MappedAttribute;
+import org.wcs.smart.conversion.model.MappedAttributeType;
+import org.wcs.smart.conversion.model.MappedCategory;
+import org.wcs.smart.conversion.tag.TagA;
+import org.wcs.smart.conversion.tag.TagS;
+import org.wcs.smart.conversion.util.Ct2AttributeTypeUtil;
+
+/**
+ * TODO Purpose of 
+ * <p>
+ * <ul>
+ * <li></li>
+ * </ul>
+ * </p>
+ * @author elitvin
+ * @since 3.2.0
+ */
+public abstract class AbstractBuilder {
+
+	private static final String LANGUAGE_CODE = "en"; //$NON-NLS-1$
+
+	private Ct2SmartLookup lookup;
+	private DataModelLookup dmLookup;
+	private TeamMembersParser membersParser = new TeamMembersParser();
+	private DateParser dateParser = new DateParser();
+
+	public AbstractBuilder(MatchSession session, DataModelLookup dmLookup) throws SQLException {
+		this.dmLookup = dmLookup;
+		lookup = new Ct2SmartLookup(session.getSmartMapping());
+	}
+
+	protected Ct2SmartLookup getLookup() {
+		return lookup;
+	}
+	
+	protected DataModelLookup getDmLookup() {
+		return dmLookup;
+	}
+	
+	public static String getLanguageCode() {
+		return LANGUAGE_CODE;
+	}
+	
+	public DateParser getDateParser() {
+		return dateParser;
+	}
+	
+	public TeamMembersParser getMembersParser() {
+		return membersParser;
+	}
+	
+	protected MappedCategory getDefaultCategory(TagS s) {
+		List<Ct2AttributeValuePair> data = new ArrayList<Ct2AttributeValuePair>();
+		for (TagA a : s) {
+			MappedAttribute cta = lookup.findAttribute(a.getI());
+			if (cta != null && MappedAttributeType.CATEGORY.equals(cta.getType())) {
+				Ct2AttributeValuePair pair = new Ct2AttributeValuePair();
+				pair.attribute = cta;
+				pair.value = a.getV();
+				data.add(pair);
+			}
+		}
+		MappedCategory c = lookup.findCategory(data);
+		if (c == null) {
+			String info = "";
+			for (Ct2AttributeValuePair pair : data) {
+				info += "\nattribute: " + Ct2AttributeTypeUtil.getN(pair.attribute) + " " + pair.attribute.getI();
+				MappedAttribute value = lookup.findAttribute(pair.value);
+				info += "  value: ";
+				info += value != null ? Ct2AttributeTypeUtil.getN(value) : "null";
+				info += " " + pair.value;
+			}
+			System.err.println("ERROR: No category defined for following items: " + info);
+		}
+		return c;
+	}
+
+	protected boolean isSame(Object o1, Object o2) {
+		if (o1 == null) 
+			return o2 == null;
+		return o1.equals(o2);
+	}
+
+}
