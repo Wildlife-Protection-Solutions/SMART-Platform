a8b504d080159eea62ccf649ae378892f2ccc4fa
Emily Gouge <egouge@refractions.net>
2015-04-22 17:08:09 +0000

    new waypoint/track bbox deleter
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@3908 37136b1e-4cb1-4992-8394-573b48a9bdce

--- utils/org.wcs.smart.data.transforms/src/org/wcs/smart/data/transforms/WaypointBboxDeleter.java
+++ utils/org.wcs.smart.data.transforms/src/org/wcs/smart/data/transforms/WaypointBboxDeleter.java
@@ -25,6 +25,9 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.crypto.dsig.TransformException;
+
+import org.geotools.referencing.GeodeticCalculator;
 import org.wcs.smart.patrol.xml.model.PatrolLegDayType;
 import org.wcs.smart.patrol.xml.model.PatrolLegType;
 import org.wcs.smart.patrol.xml.model.PatrolType;
@@ -32,73 +35,84 @@ import org.wcs.smart.patrol.xml.model.TrackType;
 import org.wcs.smart.patrol.xml.model.WaypointType;
 
 import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.io.WKBReader;
+import com.vividsolutions.jts.io.WKBWriter;
 
 /**
  * This function finds searches all waypoints and track point to ensure
- * they are inside a bounding box.  The bounding box must
+ * they are inside a bounding box.  Any waypoints or track points outside
+ * the bounding box are removed.  The bounding box must
  * be supplied as a parameter to the script in the format
  * "xmin ymin xmax ymax"
  * 
  * @author Emily
  *
  */
-public class WaypointBboxChecker implements IDataProcessor {
+public class WaypointBboxDeleter implements IDataProcessor {
 
 	private double xMin = 0;
 	private double yMin = 0;
 	private double xMax = 0;
 	private double yMax = 0;
 	
-	private void checkWaypointsTrack(PatrolType patrol) throws Exception{
+	private void deleteWaypointsTrack(PatrolType patrol) throws Exception{
 		
 		WKBReader reader = new WKBReader();
-		List<String> waypointErrors = new ArrayList<String>();
-		List<String> trackErrors = new ArrayList<String>();
+		WKBWriter writer = new WKBWriter();
+		GeometryFactory gf = new GeometryFactory();
+		
 		for (PatrolLegType leg : patrol.getLegs()){
 			for (PatrolLegDayType day : leg.getDays()){
+				List<WaypointType> toRemove = new ArrayList<WaypointType>();
 				for (WaypointType wp : day.getWaypoints()){
 					if (wp.getX() < xMin || 
 							wp.getX() > xMax || 
 							wp.getY() < yMin || 
 							wp.getY() > yMax){
-						waypointErrors.add(day.getDate() + " [" + wp.getId() + "]");
+						toRemove.add(wp);
+						//System.out.println("modified wp");
 					}
 				}
+				day.getWaypoints().removeAll(toRemove);
+				
 				TrackType tt = day.getTrack();
 				if (tt != null){
 					LineString ls = (LineString)reader.read(decodeHex(tt.getGeom()));
+					List<Coordinate> coords = new ArrayList<Coordinate>();
 					for (Coordinate c : ls.getCoordinates()){
+						coords.add(c);
+					}
+					List<Coordinate> toRemovec = new ArrayList<Coordinate>();
+					for (Coordinate c : coords){
 						if (c.x < xMin || c.x > xMax || c.y < yMin || c.y > yMax){
-							trackErrors.add(day.getDate().toString());
-							break;
+							toRemovec.add(c);
+						}
+					}
+					if (toRemovec.size() > 0){
+						//System.out.println("modified track");
+						coords.removeAll(toRemovec);
+						if (coords.size() > 1){
+							LineString newLs = gf.createLineString(coords.toArray(new Coordinate[coords.size()]));
+							tt.setGeom(encodeHex(writer.write(newLs)));
+							tt.setDistance(distanceInMeters(newLs));
+						}else{
+							System.out.println("Track for " + patrol.getId() + " (" + day.getDate() + ") will be removed as fewer than 1 points is inside the bbox.");	
+							day.setTrack(null);
 						}
 					}
 				}
 			}
 		}
-		
-		if (waypointErrors.size() > 0){
-			System.out.print("Waypoint Outside Bbox " + patrol.getId() + ":");
-			for (String error: waypointErrors){
-				System.out.print(error + " ");
-			}
-			System.out.println();
-		}
-		if (trackErrors.size() > 0){
-			System.out.print("Tracks Outsite Boox " + patrol.getId() + ":");
-			for (String error: trackErrors){
-				System.out.print(error + " ");
-			}
-			System.out.println();
-		}
+
 	}
 	
 	@Override
 	public void processFile(File in, File out) throws Exception{
 		PatrolType pt = DataUtils.readPatrol(in);
-		checkWaypointsTrack(pt);
+		deleteWaypointsTrack(pt);
+		DataUtils.writePatrol(out, pt);
 	}
 	
 	
@@ -107,7 +121,7 @@ public class WaypointBboxChecker implements IDataProcessor {
 			System.err.println("Bounding box not provided to WaypointBbox Checker");
 			return;
 		}
-		WaypointBboxChecker checker = new WaypointBboxChecker();
+		WaypointBboxDeleter checker = new WaypointBboxDeleter();
 		checker.xMin = Double.parseDouble(args[2]);
 		checker.yMin = Double.parseDouble(args[3]);
 		checker.xMax = Double.parseDouble(args[4]);
@@ -144,5 +158,47 @@ public class WaypointBboxChecker implements IDataProcessor {
 			throw new IllegalStateException("Invalid hex char.");
 		}
 		return digit;
+	}
+	
+	/**
+	 * Encodes a byte array has a hex string. This is adapted from the Apache
+	 * commons HEX class.
+	 * 
+	 * @param data
+	 *            input byte array
+	 * @return hex encoded string
+	 */
+	public static String encodeHex(byte[] data) {
+		if (data == null) return ""; //$NON-NLS-1$
+		char[] toDigits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+				'a', 'b', 'c', 'd', 'e', 'f' };
+		int l = data.length;
+		char[] out = new char[l << 1];
+		// two characters form the hex value.
+		for (int i = 0, j = 0; i < l; i++) {
+			out[j++] = toDigits[(0xF0 & data[i]) >>> 4];
+			out[j++] = toDigits[0x0F & data[i]];
+		}
+		return new String(out);
+
+	}
+	
+	/**
+	 * Computes the distance in  meters of the given linestring
+	 * in 4326 projections.
+	 * @param ls
+	 * @return
+	 * @throws TransformException
+	 */
+	public static double distanceInMeters(LineString ls) {
+		GeodeticCalculator cal = new GeodeticCalculator();
+		double distance = 0;
+		for (int i = 1; i < ls.getCoordinates().length; i ++){
+			cal.setStartingGeographicPoint(ls.getCoordinateN(i-1).x, ls.getCoordinateN(i-1).y);
+			cal.setDestinationGeographicPoint(ls.getCoordinateN(i).x, ls.getCoordinateN(i).y);
+			
+			distance +=cal.getOrthodromicDistance();
+		}
+		return distance;
 	}
 }
