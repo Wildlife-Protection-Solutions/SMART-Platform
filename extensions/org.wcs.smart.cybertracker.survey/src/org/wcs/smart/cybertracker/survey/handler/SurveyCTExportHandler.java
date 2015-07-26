c3be1b3aca4fb6836a1a4553f5ced751205a0bdb
Evgeniy Litvin <elitvin@refractions.net>
2015-07-26 18:01:28 +0000

    Initial code for survey cybertracker project.
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@4092 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ extensions/org.wcs.smart.cybertracker.survey/src/org/wcs/smart/cybertracker/survey/handler/SurveyCTExportHandler.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.cybertracker.survey.handler;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.tools.compat.parts.DIHandler;
+import org.eclipse.swt.widgets.Shell;
+import org.wcs.smart.cybertracker.survey.export.SurveyCTExportDialog;
+
+/**
+ * Handler for exporting data to CyberTracker application.
+ * 
+ * <p>
+ * Displays a dialog for users to select export options. All data manipulations are performed by this dialog.
+ * </p>
+ * 
+ * @author elitvin
+ * @since 4.0.0
+ */
+public class SurveyCTExportHandler {
+
+	@Execute
+	public void execute (Shell shell) {
+		SurveyCTExportDialog dialog = new SurveyCTExportDialog(shell);
+		dialog.open();
+	}
+	
+	public static class SurveyCTExportHandlerWrapper extends DIHandler<SurveyCTExportHandler>{
+		public SurveyCTExportHandlerWrapper() {
+			super (SurveyCTExportHandler.class);
+		}
+	}
+}
