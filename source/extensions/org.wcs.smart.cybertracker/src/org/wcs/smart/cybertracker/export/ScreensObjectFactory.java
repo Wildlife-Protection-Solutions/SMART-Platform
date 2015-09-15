ee4815d59f97b52f3e872600380114a5a2b861d3
Evgeniy Litvin <elitvin@refractions.net>
2015-09-15 16:50:14 +0000

    Improved "Save" screens for CT application
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@4223 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/extensions/org.wcs.smart.cybertracker/src/org/wcs/smart/cybertracker/export/ScreensObjectFactory.java
+++ source/extensions/org.wcs.smart.cybertracker/src/org/wcs/smart/cybertracker/export/ScreensObjectFactory.java
@@ -342,7 +342,7 @@ public class ScreensObjectFactory {
         <Name>???</Name>
         <DataClass>TctScreen</DataClass>
         <Data>
-            <NextId>14</NextId>
+            <NextId>15</NextId>
             <TemplateId>{8A40BEBA-3329-41EE-B03E-E41D85EB7DCE}</TemplateId>
             <Name>???</Name>
             <Controls>
@@ -358,7 +358,7 @@ public class ScreensObjectFactory {
 		node.setDataClass("TctScreen"); //$NON-NLS-1$
 
 		Node.Data data = new Node.Data();
-		data.setNextId(14);
+		data.setNextId(15);
 		data.setTemplateId("{8A40BEBA-3329-41EE-B03E-E41D85EB7DCE}"); //$NON-NLS-1$
 		data.setName(name);
 		Controls controls = new Controls();
