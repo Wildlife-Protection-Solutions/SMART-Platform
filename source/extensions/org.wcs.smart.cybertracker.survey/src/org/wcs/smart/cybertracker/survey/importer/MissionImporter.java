b0f0ca89e6ef8f792eaeb1adba533de2b8875071
Evgeniy Litvin <elitvin@refractions.net>
2015-09-19 12:46:16 +0000

    Avoid string constants
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@4237 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/extensions/org.wcs.smart.cybertracker.survey/src/org/wcs/smart/cybertracker/survey/importer/MissionImporter.java
+++ source/extensions/org.wcs.smart.cybertracker.survey/src/org/wcs/smart/cybertracker/survey/importer/MissionImporter.java
@@ -36,6 +36,7 @@ import org.eclipse.swt.widgets.Display;
 import org.hibernate.Session;
 import org.wcs.smart.SmartPlugIn;
 import org.wcs.smart.ca.Employee;
+import org.wcs.smart.cybertracker.export.ElementsUtil;
 import org.wcs.smart.cybertracker.export.ScreensUtil;
 import org.wcs.smart.cybertracker.importer.AbstractSmartImporter;
 import org.wcs.smart.cybertracker.importer.ImportWarningDialog;
@@ -235,7 +236,7 @@ public class MissionImporter extends AbstractSmartImporter {
 				wp.setX(Double.valueOf(a.getV()));
 			} else if (ScreensUtil.RESULT_NEW_WAYPOINT.equals(a.getN())) {
 				E e = eMap.get(a.getV());
-				newWp = "true".equals(e.getTag0()); //$NON-NLS-1$
+				newWp = ElementsUtil.BOOL_TRUE.equals(e.getTag0());
 			}
 		}
 
