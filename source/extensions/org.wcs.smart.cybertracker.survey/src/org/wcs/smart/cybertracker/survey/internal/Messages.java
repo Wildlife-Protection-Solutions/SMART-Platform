637db665c8d6db1610ef538c0068c9814bff1367
Evgeniy Litvin <elitvin@refractions.net>
2015-10-13 10:10:26 +0000

    CT Surveys localizations
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/branches/3.3.0@4347 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/extensions/org.wcs.smart.cybertracker.survey/src/org/wcs/smart/cybertracker/survey/internal/Messages.java
+++ source/extensions/org.wcs.smart.cybertracker.survey/src/org/wcs/smart/cybertracker/survey/internal/Messages.java
@@ -6,6 +6,23 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.wcs.smart.cybertracker.survey.internal.messages"; //$NON-NLS-1$
 	public static String SurveyCTExportDialog_LoadSurveyDesigns_Error;
 	public static String SurveyCTExportDialog_SurveyDesign;
+	public static String SurveyScreensUtil_BeginSurvey;
+	public static String SurveyScreensUtil_Comments;
+	public static String SurveyScreensUtil_EndSurvey;
+	public static String SurveyScreensUtil_EndSurveyMessage;
+	public static String SurveyScreensUtil_Error_InvalidMissionPropertyType;
+	public static String SurveyScreensUtil_Error_Leader;
+	public static String SurveyScreensUtil_ErrorDialog_Title;
+	public static String SurveyScreensUtil_Leader;
+	public static String SurveyScreensUtil_NewObservation;
+	public static String SurveyScreensUtil_NewSamplingUnit;
+	public static String SurveyScreensUtil_NoSamplingUnit;
+	public static String SurveyScreensUtil_PausedSurveyTitle;
+	public static String SurveyScreensUtil_PauseSurvey;
+	public static String SurveyScreensUtil_ResumeSurvey;
+	public static String SurveyScreensUtil_SamplingUnit;
+	public static String SurveyScreensUtil_StartSurvey;
+	public static String SurveyScreensUtil_StartSurveyTitle;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
