26deef65e93b25bbdb5763545372f81c8ba82b9c
Emily Gouge <egouge@refractions.net>
2015-10-28 23:08:14 +0000

    cleaning up local filestore
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@4454 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/extensions/connect/org.wcs.smart.connect/src/org/wcs/smart/connect/ui/server/DownloadChangeLogHandler.java
+++ source/extensions/connect/org.wcs.smart.connect/src/org/wcs/smart/connect/ui/server/DownloadChangeLogHandler.java
@@ -22,9 +22,10 @@
 package org.wcs.smart.connect.ui.server;
 
 import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 
 import javax.inject.Named;
 
@@ -33,10 +34,8 @@ import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.tools.compat.parts.DIHandler;
-import org.eclipse.e4.ui.model.application.ui.advanced.MArea;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
-import org.eclipse.e4.ui.workbench.IPresentationEngine;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -48,6 +47,7 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.internal.ErrorEditorPart;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.jdbc.Work;
 import org.wcs.smart.LogoutHandler;
 import org.wcs.smart.SmartPlugIn;
 import org.wcs.smart.ca.Employee;
