a82b29a2f3354280313d9538c03c3e666bc2683f
Emily Gouge <egouge@refractions.net>
2015-11-13 18:25:06 +0000

    bug fix for #1538
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@4505 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart.patrol.query/src/org/wcs/smart/patrol/query/map/udig/QueryServiceFactory.java
+++ source/java/org.wcs.smart.patrol.query/src/org/wcs/smart/patrol/query/map/udig/QueryServiceFactory.java
@@ -47,7 +47,6 @@ public class QueryServiceFactory {
 		
 		if (query instanceof SimpleQuery){
 			return new QueryService((SimpleQuery)query);
-//		}else if (query.getTypeKey().equals(PatrolGriddedQuery.class) ){
 		}else if (query instanceof PatrolGriddedQuery ){
 			return new RasterService((PatrolGriddedQuery)query);
 		}
