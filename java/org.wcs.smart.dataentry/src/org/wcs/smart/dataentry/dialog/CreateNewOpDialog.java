87b656c712f366998a8ccb682516beac95269d7f
Evgeniy Litvin <elitvin@refractions.net>
2016-01-04 16:47:34 +0000

    #1583 UI changes to select configurable model as a template for a new one
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@4643 37136b1e-4cb1-4992-8394-573b48a9bdce

--- java/org.wcs.smart.dataentry/src/org/wcs/smart/dataentry/dialog/CreateNewOpDialog.java
+++ java/org.wcs.smart.dataentry/src/org/wcs/smart/dataentry/dialog/CreateNewOpDialog.java
@@ -22,12 +22,21 @@
 package org.wcs.smart.dataentry.dialog;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -37,22 +46,32 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.hibernate.Session;
+import org.wcs.smart.SmartPlugIn;
+import org.wcs.smart.dataentry.DataentryHibernateManager;
 import org.wcs.smart.dataentry.internal.Messages;
 import org.wcs.smart.dataentry.model.ConfigurableModel;
 
 /**
  * Dialog for creating new configurable model.
  * 
- * @author Emily
+ * @author Emily, Evgeniy
  *
  */
 public class CreateNewOpDialog extends TitleAreaDialog {
+	
+	private enum CreateCmOption {
+		BLANK,
+		CM,
+		DM
+	}
 
-	private boolean blank = false;
-	private Button opBlank, opDm;
+	private CreateCmOption option = CreateCmOption.BLANK;
+	private Button opBlank, opCm, opDm;
+	private ComboViewer cbCm;
 	private Session currentSession;
 	private ConfigurableModel initModel;
 	private String name = null;
+	private ConfigurableModel cmTemplate = null;
 	private Text txtName;
 	
 	protected CreateNewOpDialog(Shell parentShell, Session currentSession) {
@@ -61,38 +80,102 @@ public class CreateNewOpDialog extends TitleAreaDialog {
 	}
 
 	protected void okPressed() {
-		blank = opBlank.getSelection();
 		name = txtName.getText();
-		super.okPressed();
+		IStructuredSelection selection = (IStructuredSelection)cbCm.getSelection();
+		cmTemplate = !selection.isEmpty() ? (ConfigurableModel) selection.getFirstElement() : null;
+		String error = validate();
+		setErrorMessage(error);
+		if (error == null) {
+			super.okPressed();
+		}
 	}
 	
+	private String validate() {
+		if (CreateCmOption.CM.equals(option) && cbCm.getSelection().isEmpty()) {
+			return Messages.CreateNewOpDialog_NoConfigurableModelSelected_Error;
+		}
+		return null;
+	}
+
 	protected Control createDialogArea(Composite parent) {
 		parent = (Composite) super.createDialogArea(parent);
 		
 		Composite panel = new Composite(parent, SWT.NONE);
-		panel.setLayout(new GridLayout(2, false));
+		panel.setLayout(new GridLayout(3, false));
 		panel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true));
 		((GridLayout)panel.getLayout()).marginWidth = 20;
 		Label lblName = new Label(panel, SWT.NONE);
 		lblName.setText(Messages.CreateNewOpDialog_NameLabel);
 		
 		txtName = new Text(panel, SWT.BORDER);
-		txtName.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		txtName.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 2, 1));
 		txtName.setText(Messages.ConfigurableModelPropertyDialog_ConfigurableModelDeafultName);
 		
 		Label lblOp = new Label(panel, SWT.NONE);
 		lblOp.setText(Messages.CreateNewOpDialog_TemplateLabel);
 		
 		opBlank = new Button(panel, SWT.RADIO);
+		opBlank.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false, 2, 1));
+		opBlank.setSelection(true);
 		opBlank.setText(Messages.CreateNewOpDialog_BlankCmOp);
 		opBlank.setToolTipText(Messages.CreateNewOpDialog_BlankCmToolTip);
-		opBlank.setSelection(true);
+		opBlank.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				optionChanged(CreateCmOption.BLANK);
+			}
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// nothing
+			}
+		});
+		
+		new Label(panel, SWT.NONE);
+
+		opCm = new Button(panel, SWT.RADIO);
+		opCm.setText(Messages.CreateNewOpDialog_CmTemplateOp);
+		opCm.setToolTipText(Messages.CreateNewOpDialog_CmTemplateToolTip);
+		opCm.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				optionChanged(CreateCmOption.CM);
+			}
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// nothing
+			}
+		});
+
+		cbCm = new ComboViewer(panel, SWT.READ_ONLY);
+		cbCm.getControl().setEnabled(false);
+		cbCm.getControl().setToolTipText(Messages.CreateNewOpDialog_CmTemplateToolTip);
+		cbCm.getControl().setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		cbCm.setContentProvider(ArrayContentProvider.getInstance());
+		cbCm.setLabelProvider(new ConfigurableModelLabelProvider());
+ 		cbCm.setInput(getCmList());
+		cbCm.addSelectionChangedListener(new ISelectionChangedListener() {
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				//changesMade();
+			}
+		});
 		
 		new Label(panel, SWT.NONE);
 		
 		opDm = new Button(panel, SWT.RADIO);
+		opDm.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, false, 2, 1));
 		opDm.setText(Messages.CreateNewOpDialog_DmTemplateOp);
 		opDm.setToolTipText(Messages.CreateNewOpDialog_DmTemplateToolTip);
+		opDm.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				optionChanged(CreateCmOption.DM);
+			}
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// nothing
+			}
+		});
 		
 		getShell().setText(Messages.CreateNewOpDialog_DialogTitle);
 		setTitle(Messages.CreateNewOpDialog_DialogTitle);
@@ -100,12 +183,40 @@ public class CreateNewOpDialog extends TitleAreaDialog {
 		
 		return parent;
 	}
+
+	private List<ConfigurableModel> getCmList() {
+		List<ConfigurableModel> modelList = new ArrayList<ConfigurableModel>();
+		try {
+			modelList = DataentryHibernateManager.getConfigurableModels(currentSession);
+		} catch (Exception ex) {
+			SmartPlugIn.displayLog(Messages.ConfigurableModelPropertyDialog_LoadModelsListError, ex);
+		}
+		return modelList;
+	}
 	
-	public ConfigurableModel getDefaultConfigurableModel() throws Exception{
-		if (blank){
+	protected void optionChanged(CreateCmOption op) {
+		option = op;
+		cbCm.getControl().setEnabled(CreateCmOption.CM.equals(option));
+	}
+	
+	public ConfigurableModel getDefaultConfigurableModel() throws Exception {
+		switch (option) {
+		case BLANK:
 			return ConfigurableModelFactory.createBlankModel(name);
-		}else{
-			//TODO: this should probably be run within a progress dialog
+		case CM:
+		{
+			ProgressMonitorDialog pmd = new ProgressMonitorDialog(getShell());
+			pmd.run(true, true, new IRunnableWithProgress() {
+				@Override
+				public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+					initModel = ConfigurableModelFactory.createConfigurableModelClone(cmTemplate, name, monitor);
+					
+				}
+			});
+			return initModel;
+		}
+		case DM:
+		{
 			ProgressMonitorDialog pmd = new ProgressMonitorDialog(getShell());
 			pmd.run(true, true, new IRunnableWithProgress() {
 				
@@ -118,6 +229,9 @@ public class CreateNewOpDialog extends TitleAreaDialog {
 			});
 			return initModel;
 		}
+		}
+		//this line should never be reached
+		throw new IllegalStateException("Unknown template option for creaing a configurable model: " + option); //$NON-NLS-1$
 	}
 	
 }
