264a2a40b180aed4b41284ddb8b9f0031348353c
Emily Gouge <egouge@refractions.net>
2016-01-27 19:54:20 +0000

    stubs / model classes for data queue processing
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@4723 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ extensions/connect/org.wcs.smart.connect.dataqueue/shared/org/wcs/smart/connect/dataqueue/model/DataQueueItem.java
@@ -0,0 +1,58 @@
+package org.wcs.smart.connect.dataqueue.model;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+import org.wcs.smart.ca.UuidItem;
+
+@Entity
+@Inheritance(strategy= InheritanceType.TABLE_PER_CLASS)
+public class DataQueueItem extends UuidItem{
+
+	public enum Type{
+		PATROL_XML,
+		INCIDENT_XML,
+		MISSION_XML,
+		INTELL_XML
+	}
+	
+	private Type type;
+	private UUID caUuid;
+	private String filename;
+	
+	@Column(name="type")
+	@Enumerated(EnumType.STRING)
+	public Type getType(){
+		return this.type;
+	}
+	
+	public void setType(Type type){
+		this.type = type;
+	}
+	
+	@Column(name="ca_uuid")
+	public UUID getConservationArea(){
+		return this.caUuid;
+	}
+	
+	public void setConservationArea(UUID caUuid){
+		this.caUuid = caUuid;
+	}
+	
+	@Column(name="filename")
+	public String getFilename(){
+		return this.filename;
+	}
+	
+	public void setFilename(String filename){
+		this.filename = filename;
+	}
+	
+	
+}
