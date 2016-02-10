2ccf940c3517449f8f8b5e2c6f7dc7168c7ce51f
Emily Gouge <egouge@refractions.net>
2016-02-10 18:05:20 +0000

    refactoring to support custom smart connect ca export
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@4741 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart/src/org/wcs/smart/ca/export/CaExporter.java
+++ source/java/org.wcs.smart/src/org/wcs/smart/ca/export/CaExporter.java
@@ -84,6 +84,22 @@ public class CaExporter {
 	 */
 	public void export(File destFile, IProgressMonitor monitor) throws Exception{
 		
+		File tempDir = SmartUtils.createTemporaryDirectory();
+		try{
+			exportToTempDirectory(tempDir, monitor);
+			zipTempDirectory(tempDir, destFile, monitor);
+			
+		}finally{
+			try{
+				FileUtils.deleteDirectory(tempDir);
+			}catch(Exception ex){
+				SmartPlugIn.log(Messages.CaExporter_Error_TempDirDelete + tempDir.getAbsolutePath(), ex);
+			}
+		}
+	}
+	
+	protected void exportToTempDirectory(File tempDir, IProgressMonitor monitor) throws Exception{
+
 		List<ICaDataExporter> exporters = getExportExtensions();
 		Collections.sort(exporters, new Comparator<ICaDataExporter>() {
 
@@ -94,42 +110,33 @@ public class CaExporter {
 			
 		});
 		
-		Session session = HibernateManager.openSession();
+		
 		ConservationArea ca = SmartDB.getCurrentConservationArea();
 		monitor.beginTask(Messages.CaExporter_ProgressExportCA, (exporters.size() + 2) * 10);
+		
+		Session session = HibernateManager.openSession();
 		try{
-			File tempDir = SmartUtils.createTemporaryDirectory();
-			try{
-				/* write a conservation area info file */
-				writeConservationAreaInfo(tempDir, ca);
-				if (monitor.isCanceled()) return;
-				monitor.worked(10);
+			/* write a conservation area info file */
+			writeConservationAreaInfo(tempDir, ca);
+			if (monitor.isCanceled()) return;
+			monitor.worked(10);
 			
-				/* run through the exporters exporting data */
-				
-				ICaDataExportEngine engine = new DerbyCaDataExportEngine(tempDir, ca, session);
-				for (ICaDataExporter exporter: exporters){
-					if (monitor.isCanceled()) return;
-					if (ca.getIsCcaa() && !exporter.supportsCcaa()) continue;
-					exporter.exportData(engine, new SubProgressMonitor(monitor, 10));
-					monitor.worked(10);
-				}
-			
-				/* zip up files */
+			/* run through the exporters exporting data */
+			ICaDataExportEngine engine = new DerbyCaDataExportEngine(tempDir, ca, session);
+			for (ICaDataExporter exporter: exporters){
 				if (monitor.isCanceled()) return;
-				ZipUtil.createZip(tempDir.listFiles(), destFile, new SubProgressMonitor(monitor,10));
+				if (ca.getIsCcaa() && !exporter.supportsCcaa()) continue;
+				exporter.exportData(engine, new SubProgressMonitor(monitor, 10));
 				monitor.worked(10);
-			}finally{
-				try{
-					FileUtils.deleteDirectory(tempDir);
-				}catch(Exception ex){
-					SmartPlugIn.log(Messages.CaExporter_Error_TempDirDelete + tempDir.getAbsolutePath(), ex);
-				}
 			}
 		}finally{
-			monitor.done();
 			session.close();
 		}
+	}
+	
+	protected void zipTempDirectory(File tempDir, File destFile, IProgressMonitor monitor) throws Exception{
+		if (monitor.isCanceled()) return;
+		ZipUtil.createZip(tempDir.listFiles(), destFile, new SubProgressMonitor(monitor,10));		
 	}
 	
 	/**
@@ -139,9 +146,8 @@ public class CaExporter {
 	 * @param ca
 	 * @throws IOException
 	 */
-	private void writeConservationAreaInfo(File directory, ConservationArea ca) throws IOException{
+	protected void writeConservationAreaInfo(File directory, ConservationArea ca) throws IOException{
 		try(FileWriter fw = new FileWriter(new File(directory, ICaDataExportEngine.CA_INFO_FILENAME))){
-//			fw.write(SmartUtils.encodeHex(ca.getUuid()));
 			fw.write(UuidUtils.uuidToString(ca.getUuid()));
 			fw.write(SharedUtils.LINE_SEPARATOR);
 			fw.write(ca.getId());
@@ -157,7 +163,7 @@ public class CaExporter {
 	/**
 	 * @return list of ca data exporter extension points
 	 */
-	private List<ICaDataExporter> getExportExtensions(){
+	protected List<ICaDataExporter> getExportExtensions(){
 		if (Platform.getExtensionRegistry() == null) return Collections.emptyList();
 		List<ICaDataExporter> items = new ArrayList<ICaDataExporter>();
 		IConfigurationElement[] config = Platform.getExtensionRegistry().getConfigurationElementsFor(EXPORT_EXTENSION_ID);
