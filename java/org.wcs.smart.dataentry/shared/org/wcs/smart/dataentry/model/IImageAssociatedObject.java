926f5ffb3baf2bd21121bb20b7d0cbf6a985d083
Evgeniy Litvin <elitvin@refractions.net>
2016-03-22 21:01:17 +0000

    #906 Logic to persist images for CmNode items
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@4880 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ java/org.wcs.smart.dataentry/shared/org/wcs/smart/dataentry/model/IImageAssociatedObject.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.dataentry.model;
+
+import java.io.File;
+
+/**
+ * Interface indicates that an object has an image associated with it.
+ * 
+ * @author elitvin
+ * @since 4.0.0
+ */
+public interface IImageAssociatedObject {
+	
+	public File getImageFile();
+	
+	public void setImageFile(File file);
+	
+	public String getImagePersistenceLocation();
+	
+}
--- java/org.wcs.smart.dataentry/src/org/wcs/smart/dataentry/dialog/ConfigurableModelEditDialog.java
+++ java/org.wcs.smart.dataentry/src/org/wcs/smart/dataentry/dialog/ConfigurableModelEditDialog.java
@@ -279,7 +279,7 @@ public class ConfigurableModelEditDialog extends TitleAreaDialog {
 				public void run(IProgressMonitor monitor) throws InvocationTargetException,
 						InterruptedException {
 					monitor.beginTask(Messages.ConfigurableModelEditDialog_SaveCmProgress, IProgressMonitor.UNKNOWN);
-					Session s = HibernateManager.openSession();
+					Session s = HibernateManager.openSession(new AssociatedImageInterceptor());
 					try{
 						s.beginTransaction();
 						//commit transaction
