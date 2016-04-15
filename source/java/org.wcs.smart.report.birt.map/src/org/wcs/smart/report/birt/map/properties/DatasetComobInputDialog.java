6ab69de4d8eda1899198862f308cb6c58142545f
Emily Gouge <egouge@refractions.net>
2016-04-15 23:39:25 +0000

    dependency updates and bug fixes
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@4935 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart.report.birt.map/src/org/wcs/smart/report/birt/map/properties/DatasetComobInputDialog.java
+++ source/java/org.wcs.smart.report.birt.map/src/org/wcs/smart/report/birt/map/properties/DatasetComobInputDialog.java
@@ -237,7 +237,7 @@ public class DatasetComobInputDialog extends Dialog {
 			@Override
 			public String getText(Object element){
 				if (element instanceof LayerDefinition){
-					return ((LayerDefinition)element).handle.getDisplayName() + " - " + ((LayerDefinition) element).info.getGeometryColumn() + " ( " + ((LayerDefinition) element).info.getLayerType() + ")";
+					return ((LayerDefinition)element).handle.getDisplayName() + " [" + ((LayerDefinition) element).info.getLayerType() + "]"; //$NON-NLS-1$ //$NON-NLS-2$
 				}
 				return super.getText(element);
 			}
