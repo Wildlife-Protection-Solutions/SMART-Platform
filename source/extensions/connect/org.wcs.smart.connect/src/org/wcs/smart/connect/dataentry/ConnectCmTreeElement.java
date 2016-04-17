493915b6520480bea94d499eeb9b8e8d5966590f
Evgeniy Litvin <elitvin@refractions.net>
2016-04-17 11:27:34 +0000

    #1748 Additional portion of logic for alerts configuration (add/edit/delete alert, UI improvements, fixes)
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@4938 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ source/extensions/connect/org.wcs.smart.connect/src/org/wcs/smart/connect/dataentry/ConnectCmTreeElement.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.connect.dataentry;
+
+import org.wcs.smart.ca.UuidItem;
+import org.wcs.smart.dataentry.model.CmAttribute;
+
+/**
+ * Class that allows to uniquely identify particular alert item which is specific for some attribute.
+ * 
+ * @author elitvin
+ * @since 4.0.0
+ */
+public class ConnectCmTreeElement {
+	
+	private CmAttribute attribute;
+	private UuidItem element;
+	
+	public ConnectCmTreeElement(CmAttribute attribute, UuidItem element) {
+		this.attribute = attribute;
+		this.element = element;
+	}
+
+	public CmAttribute getAttribute() {
+		return attribute;
+	}
+
+	public UuidItem getElement() {
+		return element;
+	}
+
+}
