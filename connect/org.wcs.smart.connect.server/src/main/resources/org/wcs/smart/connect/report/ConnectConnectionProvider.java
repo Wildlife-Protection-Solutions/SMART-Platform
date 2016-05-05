c0b4c773253de8d1d1bc48b9f1838217ea371610
Emily Gouge <egouge@refractions.net>
2016-05-05 22:13:31 +0000

    update for new connection provider
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@5022 37136b1e-4cb1-4992-8394-573b48a9bdce

--- connect/org.wcs.smart.connect.server/src/main/resources/org/wcs/smart/connect/report/ConnectConnectionProvider.java
+++ connect/org.wcs.smart.connect.server/src/main/resources/org/wcs/smart/connect/report/ConnectConnectionProvider.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.wcs.smart.connect.report;
 
 import java.util.Locale;
@@ -8,12 +29,18 @@ import org.hibernate.Session;
 import org.wcs.smart.connect.hibernate.HibernateManager;
 import org.wcs.smart.udig.catalog.smart.IDatabaseConnectionProvider;
 
+/**
+ * Connection provider 
+ * @author Emily
+ *
+ */
 public class ConnectConnectionProvider implements IDatabaseConnectionProvider {
 
 	private static final long serialVersionUID = 1L;
 
 	private ServletContext context;
 	private Locale l;
+	
 	public ConnectConnectionProvider(ServletContext context, Locale l){
 		this.context = context;
 		this.l = l;
@@ -27,6 +54,11 @@ public class ConnectConnectionProvider implements IDatabaseConnectionProvider {
 	@Override
 	public Locale getLocale() {
 		return l;
+	}
+
+	@Override
+	public void finishSession(Session session) {
+		session.close();
 	}
 
 }
