a07eeeeb073fe6489d4b13f7f6c57099d6659c94
Evgeniy Litvin <elitvin@refractions.net>
2016-05-05 17:01:00 +0000

    #1402 Logic to add list/tree attributes/items/nodes to CM after DM modified
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk/source@5017 37136b1e-4cb1-4992-8394-573b48a9bdce

--- java/org.wcs.smart.dataentry/src/org/wcs/smart/dataentry/DataModelItemListener.java
+++ java/org.wcs.smart.dataentry/src/org/wcs/smart/dataentry/DataModelItemListener.java
@@ -22,7 +22,9 @@
 package org.wcs.smart.dataentry;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import org.hibernate.Query;
@@ -34,12 +36,14 @@ import org.wcs.smart.ca.datamodel.AttributeTreeNode;
 import org.wcs.smart.ca.datamodel.Category;
 import org.wcs.smart.ca.datamodel.CategoryAttribute;
 import org.wcs.smart.ca.datamodel.IDataModelItemListener;
+import org.wcs.smart.ca.datamodel.Attribute.AttributeType;
 import org.wcs.smart.dataentry.internal.CmAttributeOptionFactory;
 import org.wcs.smart.dataentry.model.CmAttribute;
 import org.wcs.smart.dataentry.model.CmAttributeListItem;
 import org.wcs.smart.dataentry.model.CmAttributeOption;
 import org.wcs.smart.dataentry.model.CmAttributeTreeNode;
 import org.wcs.smart.dataentry.model.CmNode;
+import org.wcs.smart.dataentry.model.ConfigurableModel;
 
 /**
  * DataModel listener for removing data model items from configured models.
@@ -177,24 +181,31 @@ public class DataModelItemListener implements IDataModelItemListener {
 
 	@Override
 	public void addItem(Session currentSession, Object itemToAdd) {
-		if (itemToAdd instanceof CategoryAttribute){
-			//for each node the links to the category we need to add a new attribute node
-			CategoryAttribute ca = (CategoryAttribute)itemToAdd;
-			if (!ca.getIsActive()){
-				return;
-			}
-			addAttributeToCategory(currentSession, ca.getCategory(), ca.getAttribute());
-			List<Category> kids = new ArrayList<Category>();
-			kids.addAll(ca.getCategory().getChildren());
-			while(kids.size() > 0){
-				Category c = kids.remove(0);
-				addAttributeToCategory(currentSession, c, ca.getAttribute());
-				kids.addAll(c.getChildren());
-			}
+		if (itemToAdd instanceof CategoryAttribute) {
+			addCategoryAttribute(currentSession, (CategoryAttribute)itemToAdd);
+		} else if (itemToAdd instanceof AttributeListItem) {
+			addAttributeListItem(currentSession, (AttributeListItem)itemToAdd);
 		}
 	}
+	
+	private void addCategoryAttribute(Session currentSession, CategoryAttribute ca) {
+		//for each node the links to the category we need to add a new attribute node
+		if (!ca.getIsActive()){
+			return;
+		}
+		addAttributeToCategory(currentSession, ca.getCategory(), ca.getAttribute());
+		List<Category> kids = new ArrayList<Category>();
+		kids.addAll(ca.getCategory().getChildren());
+		while(kids.size() > 0){
+			Category c = kids.remove(0);
+			addAttributeToCategory(currentSession, c, ca.getAttribute());
+			kids.addAll(c.getChildren());
+		}
+	}
+	
 	private void addAttributeToCategory(Session currentSession, Category category, Attribute attribute){
 		List<CmNode> nodes = currentSession.createCriteria(CmNode.class).add(Restrictions.eq("category", category)).list(); //$NON-NLS-1$
+		Set<ConfigurableModel> changedModels = new HashSet<>();
 		for (CmNode node : nodes){
 			//ensure attribute doesn't already exist
 			boolean add = true;
@@ -212,11 +223,88 @@ public class DataModelItemListener implements IDataModelItemListener {
 				newAttribute.setNode(node);
 				newAttribute.setOrder(node.getCmAttributes().size());
 				newAttribute.setCmAttributeOptions(CmAttributeOptionFactory.buildDefaultOptions(newAttribute, attribute.getType()));
-				node.getCmAttributes().add(newAttribute);
 				
+				ConfigurableModel cm = node.getModel();
+				if (AttributeType.TREE.equals(attribute.getType()) || AttributeType.LIST.equals(attribute.getType())) {
+					CmAttributeOption option = CmAttributeOptionFactory.createCustomCofigOption(newAttribute);
+					option.setBooleanValue(true);
+					newAttribute.getCmAttributeOptions().put(CmAttributeOption.ID_CUSTOM_CONFIG, option);
+					if (!changedModels.contains(cm)) {
+						//create default configurations if needed
+						changedModels.add(cm);
+						if (AttributeType.TREE.equals(attribute.getType())) {
+							ensureDefaultTreeExists(currentSession, cm, attribute);
+						} else if (AttributeType.LIST.equals(attribute.getType())) {
+							ensureDefaultListExists(currentSession, cm, attribute);
+						}
+					}
+					//create custom configurations
+					if (AttributeType.TREE.equals(attribute.getType())) {
+						List<CmAttributeTreeNode> customTree = CmCustomTreesUtil.buildCustomTree(cm, newAttribute, attribute);
+						newAttribute.setTree(customTree);
+					} else if (AttributeType.LIST.equals(attribute.getType())) {
+						List<CmAttributeListItem> customList = CmCustomListsUtil.buildCustomList(cm, newAttribute, attribute);
+						newAttribute.setList(customList);
+					}
+				}
+
+				node.getCmAttributes().add(newAttribute);
 				currentSession.saveOrUpdate(newAttribute);
 			}
 		}
+	}
+
+	private void ensureDefaultTreeExists(Session s, ConfigurableModel m, Attribute a) {
+		Set<Attribute> existingTrees = CmDefaultTreesUtil.getPresentedTreeAttributes(m);
+		if (!existingTrees.contains(a)) {
+			List<CmAttributeTreeNode> defTree = CmDefaultTreesUtil.buildDefaultTree(m, a);
+			m.getDefaultTrees().addAll(defTree);
+			s.saveOrUpdate(m);
+		}
+	}
+
+	private void ensureDefaultListExists(Session s, ConfigurableModel m, Attribute a) {
+		Set<Attribute> existingLists = CmDefaultListsUtil.getPresentedListAttributes(m);
+		if (!existingLists.contains(a)) {
+			List<CmAttributeListItem> defList = CmDefaultListsUtil.buildDefaultList(m, a);
+			m.getDefaultLists().addAll(defList);
+			s.saveOrUpdate(m);
+		}
+	}
+	
+	private void addAttributeListItem(Session currentSession, AttributeListItem dmListItem) {
+		List<CmAttribute> attrList = currentSession.createCriteria(CmAttribute.class).add(Restrictions.eq("attribute", dmListItem.getAttribute())).list(); //$NON-NLS-1$
+		Set<CmAttribute> attributes = new HashSet<>(attrList); //this will remove duplicates
+		Set<ConfigurableModel> processedCMs = new HashSet<>();
+		for (CmAttribute a : attributes) {
+			ConfigurableModel cm = a.getNode().getModel();
+			if (!processedCMs.contains(cm)) {
+				//need to add new CmAttributeListItem to default lists configuration
+				processedCMs.add(cm);
+				CmAttributeListItem li = createCmAttributeListItem(dmListItem, cm);
+				li.setDmAttribute(dmListItem.getAttribute());
+				currentSession.saveOrUpdate(li);
+			}
+			if (a.isUseCustomConfig()) {
+				//need to add new CmAttributeListItem to custom list configuration
+				CmAttributeListItem li = createCmAttributeListItem(dmListItem, cm);
+				li.setAttribute(a);
+				currentSession.saveOrUpdate(li);
+			}
+		}
+	}
+	
+	/**
+	 * Notice that returned item is not fully filled.
+	 * DM or CM attribute must be additionally assigned to it
+	 */
+	private CmAttributeListItem createCmAttributeListItem(AttributeListItem dmListItem, ConfigurableModel model) {
+		CmAttributeListItem cmListItem = new CmAttributeListItem();
+		cmListItem.setConfigurableModel(model);
+		cmListItem.setListItem(dmListItem);
+		cmListItem.setIsActive(false); //by design
+		cmListItem.setListOrder(dmListItem.getListOrder());
+		return cmListItem;
 	}
 
 	/**
