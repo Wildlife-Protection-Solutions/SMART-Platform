0d2c23a776d7ac803ab49d5f6ed83f710e36d0fd
Evgeniy Litvin <elitvin@refractions.net>
2016-08-04 13:15:33 +0000

    #1643 SMART CSV import tool only runs on 64-bit
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@5175 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/utils/conversion/org.wcs.smart.conversion.csv/run.bat
+++ source/utils/conversion/org.wcs.smart.conversion.csv/run.bat
@@ -1,1 +1,14 @@
+@echo off
+setlocal
+
+set is64=0
+for /f "tokens=3" %%g in ('java -version 2^>^&1 ^| findstr /i "64-Bit"') do (
+    set is64=1
+)
+if %is64%==0 (
+    @echo Application requires 64-Bit version of java. Please install the desired version.
+    pause
+)
+endlocal
+@echo on
 javaw -cp %CP1%;%CP2% org.wcs.smart.conversion.csv.Csv2SmartMatcher > csv2smart.log
