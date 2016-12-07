45c5356052591b7bc622d8ea3b405e216bfff754
Emily Gouge <egouge@refractions.net>
2016-12-07 17:14:26 +0000

    bug fixes to upgrade scripts
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@5452 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart/src/org/wcs/smart/upgrade/v500/Upgrader410To500.java
+++ source/java/org.wcs.smart/src/org/wcs/smart/upgrade/v500/Upgrader410To500.java
@@ -56,7 +56,9 @@ public class Upgrader410To500 implements IDatabaseUpgrader {
 				"ALTER TABLE smart.employee ADD COLUMN usertemp VARCHAR(5000)",
 				"UPDATE smart.employee set usertemp = case when smartuserlevel = 0 THEN 'ADMIN' when smartuserlevel = 1 THEN 'MANAGER' WHEN smartuserlevel = 2  THEN 'ANALYST' when smartuserlevel=3 THEN 'DATAENTRY' ELSE null END",
 				"ALTER TABLE smart.employee DROP COLUMN smartuserlevel",
-				"RENAME COLUMN smart.employee.usertemp TO smartuserlevel",
+				"ALTER TABLE smart.employee ADD COLUMN smartuserlevel VARCHAR(5000)",
+				"UPDATE smart.employee SET smartuserlevel = usertemp",
+				"ALTER TABLE smart.employee DROP COLUMN usertemp",
 
 		};
 
