7cf2ff516036886e9387d87c3af5dcc319c3af5c
Jeff Lounsbury <jeffloun@refractions.net>
2017-05-26 18:47:37 +0000

    added some missing 5.0 sql to the new script
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/branches/5.0.0@5837 37136b1e-4cb1-4992-8394-573b48a9bdce

--- connect/org.wcs.smart.connect.server/database/upgrade_5_0_0_to_5_0_1.sql
+++ connect/org.wcs.smart.connect.server/database/upgrade_5_0_0_to_5_0_1.sql
@@ -2,4 +2,7 @@
 DROP SCHEMA If exists query_temp CASCADE;
 CREATE SCHEMA query_temp;
 
+alter table connect.shared_links drop column expires_after;
+alter table connect.shared_links ALTER COLUMN ca_uuid DROP NOT null;
+
 update connect.connect_version set version = '5.0.1';
