0a0beff8b7fcd1001e1dbaac4718b08ca217e6f1
Emily Gouge <egouge@refractions.net>
2017-05-26 17:06:17 +0000

    upgrade db version
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/branches/5.0.0@5834 37136b1e-4cb1-4992-8394-573b48a9bdce

--- connect/org.wcs.smart.connect.server/database/upgrade_5_0_0_to_5_0_1.sql
+++ connect/org.wcs.smart.connect.server/database/upgrade_5_0_0_to_5_0_1.sql
@@ -1,3 +1,5 @@
 --schema creation was accidentally left out of -> 5.0.0 sql script. Drop it if it already exists and add it.
 DROP SCHEMA If exists query_temp CASCADE;
 CREATE SCHEMA query_temp;
+
+update connect.connect_version set version = '5.0.1';
