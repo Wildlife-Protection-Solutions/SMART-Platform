35beee0a7ea48008857792e7c299206d765a6449
Emily Gouge <egouge@refractions.net>
2017-07-13 17:45:51 +0000

    #1727 (reinstall ca from connect option)
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@5939 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ source/extensions/connect/org.wcs.smart.connect/src/org/wcs/smart/connect/ui/server/DownloadReplaceCaHandler.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.connect.ui.server;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Named;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.tools.compat.parts.DIHandler;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.wcs.smart.connect.ConnectPlugIn;
+import org.wcs.smart.connect.SmartConnect;
+import org.wcs.smart.connect.api.model.ConservationAreaProxy;
+import org.wcs.smart.connect.internal.Messages;
+import org.wcs.smart.connect.internal.server.DownloadCaEngine;
+import org.wcs.smart.hibernate.SmartDB;
+
+/**
+ * Download and replace the current conservation area with data from connect.
+ * @author Emily
+ *
+ */
+public class DownloadReplaceCaHandler {
+
+	@Execute
+	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell activeShell){
+		
+		if (!MessageDialog.openConfirm(activeShell, "Download and Install", "Are you sure you want to replace your Conservation Area data with the data from Connect?  You will lose all local changes.")){
+			return;
+		}
+		
+		ConnectDialog dialog = new ConnectDialog(activeShell){
+			@Override
+			protected Control createDialogArea(Composite parent) {
+				setTitle("Download and Install");
+				setMessage("Replace the Conservation Area data with data from Connect");
+				getShell().setText("Download and Install");
+				
+				return super.createDialogArea(parent);
+			}	
+		};
+		if (dialog.open() != Window.OK){
+			return;
+		}
+		
+		SmartConnect connect = dialog.getConnection();
+		ConservationAreaProxy proxy = new ConservationAreaProxy();
+		proxy.setUuid(SmartDB.getCurrentConservationArea().getUuid());
+		
+		final DownloadCaEngine installer = new DownloadCaEngine(proxy, connect);
+		
+		final List<Exception> errors = new ArrayList<Exception>();
+		try{
+			ProgressMonitorDialog monitor = new ProgressMonitorDialog(activeShell);
+			monitor.run(true, true, new IRunnableWithProgress() {
+				@Override
+				public void run(IProgressMonitor monitor) throws InvocationTargetException,
+						InterruptedException {
+					try{
+						if (!installer.downloadImport(monitor)){
+							errors.add(new Exception(Messages.DownloadConnectWizard_Cancelled));
+						}
+						
+					}catch (Exception ex){
+						errors.add(ex);
+					}
+				}
+			});
+		}catch (Exception ex){
+			errors.add(ex);
+		}
+		if (errors.isEmpty()){
+			PlatformUI.getWorkbench().restart();
+			return;
+		}
+		ConnectPlugIn.displayLog(Messages.DownloadConnectWizard_DownloadError + "\n\n" + errors.get(0).getMessage(), errors.get(0)); //$NON-NLS-1$
+		
+	}
+	
+	
+	public static class DownloadReplaceCaHandlerWrapper extends DIHandler<DownloadReplaceCaHandler>{
+		public DownloadReplaceCaHandlerWrapper() {
+			super(DownloadReplaceCaHandler.class);
+		}
+	}
+	
+}
