50fa686bb2684ff6e8bb1f0b3605f63c66edec4e
Emily Gouge <egouge@refractions.net>
2018-02-23 00:47:01 +0000

    modifications to support encrypted files on connect in reports
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/branches/emily_encryption@6393 37136b1e-4cb1-4992-8394-573b48a9bdce

--- connect/org.wcs.smart.connect.server/src/main/resources/org/wcs/smart/connect/query/engine/i2/IntelConnection.java
+++ connect/org.wcs.smart.connect.server/src/main/resources/org/wcs/smart/connect/query/engine/i2/IntelConnection.java
@@ -21,12 +21,19 @@
  */
 package org.wcs.smart.connect.query.engine.i2;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Collections;
 
+import org.eclipse.birt.report.engine.api.HTMLRenderContext;
+import org.eclipse.birt.report.engine.api.HTMLRenderOption;
 import org.hibernate.Session;
+import org.wcs.smart.SmartContext;
 import org.wcs.smart.birt.BirtConstants;
 import org.wcs.smart.ca.ConservationArea;
+import org.wcs.smart.cipher.EncryptUtils;
+import org.wcs.smart.common.attachment.ISmartAttachment;
 import org.wcs.smart.connect.report.query.ServerSmartConnection;
 import org.wcs.smart.connect.security.AdvIntelAction;
 import org.wcs.smart.connect.security.SecurityManager;
@@ -49,6 +56,37 @@ public class IntelConnection extends AbstractIntelBirtConnection {
 	}
 
 	@Override
+	public String decryptAttachment(ISmartAttachment attachment) {
+		if (attachment == null) return null;
+		
+		Path imageOutDir = getImageOutputDirectory();
+		Path imageFile = null;
+		try {
+			if (imageOutDir == null) {
+				imageFile = EncryptUtils.decryptAttachment(attachment);
+			}else {
+				imageFile = imageOutDir.resolve(attachment.getFilename());
+				imageFile = EncryptUtils.uniqueFile(imageFile);
+				EncryptUtils.decryptAttachment(attachment, imageFile);
+			}
+			attachmentFiles.add(imageFile);
+			
+			if (imageFile.startsWith(Paths.get(SmartContext.INSTANCE.getFilestoreLocation()))) {
+				return imageFile.toAbsolutePath().toUri().toString();
+			}else {
+				if (isHtml()) {
+					return getImageUrl() + "/" + imageOutDir.relativize(imageFile).toString();
+				}else {
+					return imageFile.toAbsolutePath().toUri().toString();	
+				}
+			}
+		} catch (Exception e) {
+			//we are going to eat this; likely an issue decrypting the attachment 
+		}
+		return null;
+	}
+	
+	@Override
 	public Collection<ConservationArea> getConservationAreas() {
 		Object x = appContext.get(ServerSmartConnection.CCAA_FILTER_KEY);
 		if (x == null) return null;
@@ -58,6 +96,47 @@ public class IntelConnection extends AbstractIntelBirtConnection {
 		return (Collection<ConservationArea>)x;
 	}
 
+	/**
+	 * 
+	 * @return the image output directory supplied in the app context.  Will
+	 * return null if not specified
+	 */
+	@Override
+	protected Path getImageOutputDirectory() {
+		Object ops = appContext.get("HTML_RENDER_CONTEXT");
+		if (ops == null) return null;
+		HTMLRenderContext context = (HTMLRenderContext)ops;
+		if (context.getImageDirectory() == null) return null;
+		return Paths.get( context.getImageDirectory() );		
+	}
+	
+	/**
+	 * 
+	 * @return the image output directory supplied in the app context.  Will
+	 * return null if not specified
+	 */
+	
+	protected String getImageUrl() {
+		Object ops = appContext.get("HTML_RENDER_CONTEXT");
+		if (ops == null) return null;
+		HTMLRenderContext context = (HTMLRenderContext)ops;
+		if (context.getBaseImageURL() == null) return null;
+		return context.getBaseImageURL();		
+	}
+	/**
+	 * If the output type is html
+	 * @return
+	 */
+	@Override
+	protected boolean isHtml() {
+		if (appContext == null) return false;
+		Object ctx = appContext.get("HTML_RENDER_CONTEXT");
+		if (ctx == null) return false;
+		if (!(ctx instanceof HTMLRenderContext)) return false;
+		HTMLRenderContext cc = (HTMLRenderContext)ctx;
+		return cc.getRenderOption().getOutputFormat().equalsIgnoreCase(HTMLRenderOption.HTML);
+	}
+	
 	@Override
 	public boolean hasPermission(Permission permission) {
 		if (appContext == null) return true;
