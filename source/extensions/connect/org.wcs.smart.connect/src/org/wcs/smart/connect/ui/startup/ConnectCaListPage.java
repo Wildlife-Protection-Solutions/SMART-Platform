51ec95b26d2aff0740b7160b873886b851267668
Emily Gouge <egouge@refractions.net>
2018-08-03 20:20:19 +0000

    2523
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@6942 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/extensions/connect/org.wcs.smart.connect/src/org/wcs/smart/connect/ui/startup/ConnectCaListPage.java
+++ source/extensions/connect/org.wcs.smart.connect/src/org/wcs/smart/connect/ui/startup/ConnectCaListPage.java
@@ -70,7 +70,7 @@ public class ConnectCaListPage extends WizardPage implements ISelectionChangedLi
 		Label l = new Label(outer, SWT.NONE);
 		l.setText(Messages.ConnectCaListPage_CaLabel);
 	
-		cmbList = new ListViewer(outer);
+		cmbList = new ListViewer(outer, SWT.SINGLE | SWT.BORDER);
 		cmbList.getControl().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		((GridData)cmbList.getList().getLayoutData()).heightHint = 100;
 		cmbList.setContentProvider(ArrayContentProvider.getInstance());
