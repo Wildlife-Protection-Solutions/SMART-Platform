7b7514ca04ff7530b62c29286b8067ff3520b1a9
Emily Gouge <egouge@refractions.net>
2018-10-17 19:58:43 +0000

    typo
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@7098 37136b1e-4cb1-4992-8394-573b48a9bdce

--- connect/org.wcs.smart.connect.server/database/upgrade_6_1_0_to_6_2_0.sql
+++ connect/org.wcs.smart.connect.server/database/upgrade_6_1_0_to_6_2_0.sql
@@ -72,4 +72,4 @@ update connect.connect_plugin_version set version = '6.2.0' where plugin_id = 'o
 update connect.ca_plugin_version set version = '6.2.0' where plugin_id = 'org.wcs.smart';
 
 update connect.connect_version set version = '6.2.0';		
---dont via upgrade script; update connect.connect_version set filestore_version = '6.2.0';
+--done via connect java upgrade script; update connect.connect_version set filestore_version = '6.2.0';
