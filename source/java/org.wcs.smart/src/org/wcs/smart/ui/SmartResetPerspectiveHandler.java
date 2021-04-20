728e908d11682bc513061cc6950b7b4423fa02e2
Emily Gouge <egouge@refractions.net>
2021-04-20 16:59:49 +0000

    re: #3158
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@8216 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/org.wcs.smart/src/org/wcs/smart/ui/SmartResetPerspectiveHandler.java
+++ source/java/org.wcs.smart/src/org/wcs/smart/ui/SmartResetPerspectiveHandler.java
@@ -1,18 +1,55 @@
- 
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.wcs.smart.ui;
 
-import javax.inject.Inject;
+import java.util.Collections;
 
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.NotHandledException;
+import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.e4.core.commands.ExpressionContext;
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.ui.internal.handlers.ResetPerspectiveHandler;
 
+/**
+ * Reset perspective 
+ * 
+ * @author Emily
+ *
+ */
 public class SmartResetPerspectiveHandler {
+	
 	@Execute
-	public void execute(ExecutionEvent event) {
+	public void execute(IEclipseContext context, @Optional Event trigger, @Optional IEvaluationContext staticContext)
+			throws ExecutionException, NotHandledException {
+		IEvaluationContext appContext = staticContext;
+		if (appContext == null) {
+			appContext = new ExpressionContext(context);
+		}
+		ExecutionEvent event = new ExecutionEvent(null, Collections.emptyMap(), trigger, appContext);
 		(new ResetPerspectiveHandler()).execute(event);
-
 	}
-		
 }
