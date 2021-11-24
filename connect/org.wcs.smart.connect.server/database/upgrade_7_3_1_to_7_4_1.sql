9673f0e65b447fd46dfef52fe03c9eb1751bdafb
Emily Gouge <egouge@refractions.net>
2021-11-24 19:34:25 +0000

    bug fix
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/branches/smart7.4.0@8565 37136b1e-4cb1-4992-8394-573b48a9bdce

--- connect/org.wcs.smart.connect.server/database/upgrade_7_3_1_to_7_4_1.sql
+++ connect/org.wcs.smart.connect.server/database/upgrade_7_3_1_to_7_4_1.sql
@@ -6,5 +6,5 @@ ALTER TABLE smart.employee_team_member ADD FOREIGN KEY (employee_uuid) REFERENCE
 SET session_replication_role = DEFAULT;
 
 update connect.connect_plugin_version set version = '7.4.1' where plugin_id = 'org.wcs.smart';
-update connect.ca_plugin_version set version = '7.4.1' where plugin_id = 'org.wcs.smart.';
+update connect.ca_plugin_version set version = '7.4.1' where plugin_id = 'org.wcs.smart';
 update connect.connect_version set version = '7.4.1', last_updated = now();
