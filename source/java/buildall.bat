f3e9cc826c3c76ba100cb165c96aa52d235dacf6
Emily Gouge <egouge@refractions.net>
2022-05-20 23:26:58 +0000

    smart 751 build configuration
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/branches/smart7.4.0@8806 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/buildall.bat
+++ source/java/buildall.bat
@@ -8,7 +8,7 @@ echo %outputlocation%
 MKDIR %outputlocation%
 
 REM run maven to build packages
-call mvn clean install -Pallplatforms,product,update,core,plugins,utils,migrationtools,languagepacks
+call mvn clean install -Pallplatforms,product,update,core,plugins,utils,languagepacks
 REM call mvn install -Pallplatforms,migrationtools
 
 del %outputlocation%\smartapp-win32.win32.x86_64.zip
@@ -26,11 +26,11 @@ copy .\org.wcs.smart.updatesite\target\org.wcs.smart.updatesite-%version%-SNAPSH
 del %outputlocation%\org.wcs.smart.utils.updatesite-%version%-SNAPSHOT.zip
 copy .\org.wcs.smart.utils.updatesite\target\org.wcs.smart.utils.updatesite-%version%-SNAPSHOT.zip %outputlocation%
 
-del %outputlocation%\org.wcs.smart.migrationtools.updatesite-%version%-SNAPSHOT.zip
-copy .\org.wcs.smart.migrationtools.updatesite\target\org.wcs.smart.migrationtools.updatesite-%version%-SNAPSHOT.zip %outputlocation%
+REM del %outputlocation%\org.wcs.smart.migrationtools.updatesite-%version%-SNAPSHOT.zip
+REM copy .\org.wcs.smart.migrationtools.updatesite\target\org.wcs.smart.migrationtools.updatesite-%version%-SNAPSHOT.zip %outputlocation%
 
 move %outputlocation%\org.wcs.smart.updatesite-%version%-SNAPSHOT.zip %outputlocation%\site_%version%.zip
-move %outputlocation%\org.wcs.smart.migrationtools.updatesite-%version%-SNAPSHOT.zip %outputlocation%\migrationtools_%version%.zip
+REM move %outputlocation%\org.wcs.smart.migrationtools.updatesite-%version%-SNAPSHOT.zip %outputlocation%\migrationtools_%version%.zip
 move %outputlocation%\org.wcs.smart.utils.updatesite-%version%-SNAPSHOT.zip %outputlocation%\smartutils_%version%.zip
 
 @RD /s /q %outputlocation%\smartapp-win32.win32.x86_64
@@ -40,6 +40,6 @@ powershell Expand-Archive %outputlocation%\smartapp-win32.win32.x86_64.zip %outp
 del %outputlocation%\smartapp-win32.win32.x86_64\eclipsec.exe
 mkdir %outputlocation%\smartapp-win32.win32.x86_64\updatesite
 copy %outputlocation%\site_%version%.zip %outputlocation%\smartapp-win32.win32.x86_64\updatesite
-copy %outputlocation%\migrationtools_%version%.zip %outputlocation%\smartapp-win32.win32.x86_64\updatesite
+copy %outputlocation%\smartutils_%version%.zip %outputlocation%\smartapp-win32.win32.x86_64\updatesite
 
 REM complete - start smart and install plugins/restore backup
