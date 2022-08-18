d90c2f256b0d36ae2b71d1562f8909415b26a4a3
Emily Gouge <egouge@refractions.net>
2022-08-18 19:02:32 +0000

    Merged revision(s) 8893 from branches/smart7.5.3: re: #3466 ........
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@8895 37136b1e-4cb1-4992-8394-573b48a9bdce

--- connect/org.wcs.smart.connect.server/database/upgrade_7_5_1_to_7_5_3.sql
+++ connect/org.wcs.smart.connect.server/database/upgrade_7_5_1_to_7_5_3.sql
@@ -3,6 +3,31 @@ alter table smart.data_link drop constraint "data_link_provider_id_key";
 alter table smart.data_link add constraint data_link_provider_id_unq unique(provider_id, data_type);
 alter table smart.data_link add constraint data_link_smart_id_unq unique(smart_id);
 
+
+
+CREATE OR REPLACE FUNCTION connect.trg_changelog_after() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+BEGIN
+    PERFORM pg_advisory_unlock_shared(a.lock_key) FROM connect.ca_info a WHERE a.ca_uuid = NEW.ca_uuid;
+RETURN NEW; END$$;
+
+
+CREATE OR REPLACE FUNCTION connect.trg_changelog_before() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+  canlock boolean;
+BEGIN
+    --check if we should log this ca
+    IF (NOT connect.dolog(NEW.ca_uuid)) THEN RETURN NULL; END IF;
+    SELECT pg_try_advisory_lock_shared(a.lock_key) into canlock FROM connect.ca_info a WHERE a.ca_uuid = NEW.ca_uuid;
+    IF (canlock) THEN return NEW; ELSE RAISE EXCEPTION 'Database Locked to Editing'; END IF;
+END$$;
+
+
+
 --update versions
 update connect.connect_plugin_version set version = '7.5.3' where plugin_id = 'org.wcs.smart';
 update connect.ca_plugin_version set version = '7.5.3' where plugin_id = 'org.wcs.smart';
