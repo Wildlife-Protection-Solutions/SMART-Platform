8ddc8d21b242e369cebc430f0a7b58c1aa5365aa
Emily Gouge <egouge@refractions.net>
2022-09-15 20:53:56 +0000

    updates to support db updates required for er plugin for 754
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/branches/smart7.5.4@8933 37136b1e-4cb1-4992-8394-573b48a9bdce

--- connect/org.wcs.smart.connect.server/database/upgrade_7_5_3_to_7_5_4.sql
+++ connect/org.wcs.smart.connect.server/database/upgrade_7_5_3_to_7_5_4.sql
@@ -4,4 +4,8 @@ ALTER TABLE smart.survey_waypoint ADD CONSTRAINT survey_waypoint_wp_uuid_fk FORE
 --update versions
 update connect.connect_plugin_version set version = '5.0' where plugin_id = 'org.wcs.smart.er';
 update connect.ca_plugin_version set version = '5.0' where plugin_id = 'org.wcs.smart.er';
+
+update connect.connect_plugin_version set version = '7.5.4' where plugin_id = 'org.wcs.smart';
+update connect.ca_plugin_version set version = '7.5.4' where plugin_id = 'org.wcs.smart';
+
 update connect.connect_version set version = '7.5.4', last_updated = now();
