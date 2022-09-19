6078678e9b09ba7bc538616c85a88530cbc46a9e
Emily Gouge <egouge@refractions.net>
2022-09-19 20:19:18 +0000

    Merged revision(s) 8940 from branches/smart7.5.4: fix 754 upgrade script ........
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@8941 37136b1e-4cb1-4992-8394-573b48a9bdce

--- connect/org.wcs.smart.connect.server/database/upgrade_7_5_3_to_7_5_4.sql
+++ connect/org.wcs.smart.connect.server/database/upgrade_7_5_3_to_7_5_4.sql
@@ -4,4 +4,6 @@ ALTER TABLE smart.survey_waypoint ADD CONSTRAINT survey_waypoint_wp_uuid_fk FORE
 --update versions
 update connect.connect_plugin_version set version = '5.0' where plugin_id = 'org.wcs.smart.er';
 update connect.ca_plugin_version set version = '5.0' where plugin_id = 'org.wcs.smart.er';
-update connect.connect_version set version = '7.5.4', last_updated = now();
+--don't update the version as we are not deploying a new jar file
+--update connect.connect_version set version = '7.5.4', last_updated = now();
+update connect.connect_version set last_updated = now();
