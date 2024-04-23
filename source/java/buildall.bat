b6766e5d29048febffa5b4f648e0aa07706e2152
Emily Gouge <egouge@refractions.net>
2024-04-23 01:03:41 +0000

    more km translation fixes; including birt report issue that affects other languages as well
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/branches/smart7.5.4@9312 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/buildall.bat
+++ source/java/buildall.bat
@@ -8,8 +8,8 @@ echo %outputlocation%
 MKDIR %outputlocation%
 
 REM run maven to build packages
-REM set JAVA_HOME=C:\Java\jdk-11.0.2
-REM set PATH=C:\Java\jdk-11.0.2\bin;%PATH%
+set JAVA_HOME=C:\Java\jdk-11.0.2
+set PATH=C:\Java\jdk-11.0.2\bin;%PATH%
 call mvn clean install -Pallplatforms,product,update,core,plugins,utils,languagepacks
 rem call mvn clean install -Pallplatforms,update,core,plugins,utils,languagepacks
 REM call mvn install -Pallplatforms,migrationtools
