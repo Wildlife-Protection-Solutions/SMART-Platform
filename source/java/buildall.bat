de0d8bc7317760a552968612559d81d7333d0f0a
Emily Gouge <egouge@refractions.net>
2024-05-10 19:49:42 +0000

    build updates to include translations
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@9345 37136b1e-4cb1-4992-8394-573b48a9bdce

--- source/java/buildall.bat
+++ source/java/buildall.bat
@@ -87,11 +87,16 @@ copy .\org.wcs.smart.updatesite\target\org.wcs.smart.updatesite-%version%-SNAPSH
 del %outputlocation%\org.wcs.smart.utils.updatesite-%version%-SNAPSHOT.zip
 copy .\org.wcs.smart.utils.updatesite\target\org.wcs.smart.utils.updatesite-%version%-SNAPSHOT.zip %outputlocation%
 
-del %outputlocation%\org.wcs.smart.migrationtools.updatesite-%version%-SNAPSHOT.zip
-copy .\org.wcs.smart.migrationtools.updatesite\target\org.wcs.smart.migrationtools.updatesite-%version%-SNAPSHOT.zip %outputlocation%
+REM del %outputlocation%\org.wcs.smart.migrationtools.updatesite-%version%-SNAPSHOT.zip
+REM copy .\org.wcs.smart.migrationtools.updatesite\target\org.wcs.smart.migrationtools.updatesite-%version%-SNAPSHOT.zip %outputlocation%
+
+del %outputlocation%\org.wcs.smart.translations.updatesite-%version%-SNAPSHOT.zip
+copy .\org.wcs.smart.translations.updatesite\target\org.wcs.smart.translations.updatesite-%version%-SNAPSHOT.zip %outputlocation%
+
 
 move %outputlocation%\org.wcs.smart.updatesite-%version%-SNAPSHOT.zip %outputlocation%\site_%version%.zip
-move %outputlocation%\org.wcs.smart.migrationtools.updatesite-%version%-SNAPSHOT.zip %outputlocation%\migrationtools_%version%.zip
+REM move %outputlocation%\org.wcs.smart.migrationtools.updatesite-%version%-SNAPSHOT.zip %outputlocation%\migrationtools_%version%.zip
+move %outputlocation%\org.wcs.smart.translations.updatesite-%version%-SNAPSHOT.zip %outputlocation%\languagepacks_%version%.zip
 move %outputlocation%\org.wcs.smart.utils.updatesite-%version%-SNAPSHOT.zip %outputlocation%\smartutils_%version%.zip
 
 @RD /s /q %outputlocation%\smartapp-win32.win32.x86_64
@@ -101,6 +106,8 @@ powershell Expand-Archive %outputlocation%\smartapp-win32.win32.x86_64.zip %outp
 del %outputlocation%\smartapp-win32.win32.x86_64\SMARTc.exe
 mkdir %outputlocation%\smartapp-win32.win32.x86_64\updatesite
 copy %outputlocation%\site_%version%.zip %outputlocation%\smartapp-win32.win32.x86_64\updatesite
-copy %outputlocation%\migrationtools_%version%.zip %outputlocation%\smartapp-win32.win32.x86_64\updatesite
+copy %outputlocation%\languagepacks_%version%.zip %outputlocation%\smartapp-win32.win32.x86_64\updatesite
+copy %outputlocation%\smartutils_%version%.zip %outputlocation%\smartapp-win32.win32.x86_64\updatesite
+REM copy %outputlocation%\migrationtools_%version%.zip %outputlocation%\smartapp-win32.win32.x86_64\updatesite
 
 REM complete - start smart and install plugins/restore backup
