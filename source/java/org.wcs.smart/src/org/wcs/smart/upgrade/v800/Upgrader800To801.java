a3b1cf28a1214550c10cc86938994b4ab5ee0654
Emily Gouge <egouge@refractions.net>
2024-06-25 22:39:03 +0000

    Merged revision(s) 9388 from branches/smart8.0.X: re: #3763 missed upgrade file ........
    git-svn-id: https://subversion.assembla.com/svn/smart-cs/trunk@9389 37136b1e-4cb1-4992-8394-573b48a9bdce

---
+++ source/java/org.wcs.smart/src/org/wcs/smart/upgrade/v800/Upgrader800To801.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2012 Wildlife Conservation Society
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.wcs.smart.upgrade.v800;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.MessageFormat;
+import java.util.HashMap;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.hibernate.Session;
+import org.hibernate.jdbc.Work;
+import org.wcs.smart.SmartPlugIn;
+import org.wcs.smart.ca.ConservationArea;
+import org.wcs.smart.hibernate.HibernateManager;
+import org.wcs.smart.internal.Messages;
+import org.wcs.smart.upgrade.AbstractInteralDatabaseUpgrader;
+import org.wcs.smart.upgrade.UpgradeEngine;
+
+/**
+ * 8.0.0 to 8.0.1 upgrader
+ * 
+ * @author Emily
+ *
+ */
+public class Upgrader800To801 extends AbstractInteralDatabaseUpgrader { 
+	
+	private Exception thrownException = null;
+
+	private HashMap<ConservationArea, String> caTimeZoneMapping;
+	
+	
+	public HashMap<ConservationArea, String> getCaTimeZoneMapping(){
+		return this.caTimeZoneMapping;
+	}
+	
+	@Override
+	public void upgrade(final IProgressMonitor monitor) throws Exception {
+		monitor.subTask(MessageFormat.format(Messages.Upgrader700To741_UpgradeMsg, 
+				UpgradeEngine.UpgradeFromVersion.V801.fromVersion, 
+				UpgradeEngine.UpgradeFromVersion.V801.toVersion));  
+		
+		
+		try(Session s = HibernateManager.openSession()){
+			
+			
+			s.doWork(new Work() {
+				@Override
+				public void execute(Connection c) throws SQLException {
+					s.beginTransaction();
+					try {
+						c.setAutoCommit(false);
+						upgrade(c, monitor);
+						c.setAutoCommit(true);
+						s.getTransaction().commit();
+					} catch (final Exception e) {
+						thrownException = new Exception(MessageFormat.format(Messages.Upgrader700To741_UpgradeErrorMsage, UpgradeEngine.UpgradeFromVersion.V800.fromVersion, UpgradeEngine.UpgradeFromVersion.V800.toVersion), e); 
+					}
+				}
+			});
+		}
+		if (thrownException != null)
+			throw thrownException;
+
+		monitor.done();
+	}
+
+	private void upgrade(Connection c, IProgressMonitor monitor)
+			throws Exception {
+
+		String[] sql = new String[] {		
+			"ALTER TABLE smart.cm_node DROP COLUMN use_single_gps_point",  //$NON-NLS-1$
+		};
+		
+		for (String s : sql) {
+			SmartPlugIn.logInfo(s);
+			c.createStatement().execute(s);
+		}
+		
+		/* VERSION UDATE */
+		String ssql = "update smart.db_version set version = '" + UpgradeEngine.UpgradeFromVersion.V801.toVersion + "' where plugin_id = 'org.wcs.smart'"; //$NON-NLS-1$ //$NON-NLS-2$
+		c.createStatement().execute(ssql);
+	}
+
+}
